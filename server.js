9ca970a7d9d1d2d9318fe24e8baeb06d229f5326
Pat Collins <padjo@mac.com>
2018-01-26 23:28:03 +0000

    minor error preventing server from starting up

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const bodyParser = require('body-parser');
 
 const server = express();
 
-server.use(bodyParser.JSON());
+server.use(bodyParser.json());
 
 // endpoints here 
+
+server.get('/', (req, res)=>{
+    res.send('all good');
+});
 
 server.listen(3000, function(){console.log('server running on port 3000')});
