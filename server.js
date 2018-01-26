2db53e0f1f94b9406bb77f9b358e3b48a5959ee0
Patrick Sandoval <patsand24csu@yahoo.com>
2018-01-26 10:56:42 -0800

    seeds migrated properly and first two project routes are working

--- server.js
+++ server.js
@@ -10,3 +10,31 @@ const server = express();
 server.use(bodyParser.json());
 
 // project routes
+
+server.post('/projects', (req, res) => {
+    const project = req.body;
+
+    knex.insert(project)
+        .into('projects')
+        .then(function(ids) {
+            res.status(201).json({ ids });
+        })
+        .catch(function() {
+            res.status(500).json({ errorMessage: 'could not insert into table' });
+        });
+});
+
+server.get('/projects', (req, res) => {
+    const projects = knex('projects')
+        .then(function(projects) {
+            res.status(200).json(projects);
+        })
+        .catch(function() {
+            res.status(500).json({ errorMessage: 'could not insert into table' });
+        });
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`server running at ${port}`);
+})
