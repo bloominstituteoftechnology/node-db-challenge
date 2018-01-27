c035e5ed427154c43ce64ddcec4f7b824dff968d
KickMe <kickme@alifs-mbp.gateway.sonic.net>
2018-01-26 22:54:34 -0800

    changes made

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const bodyParser = require('body-parser');
+
+const server = express();
+
+server.use(bodyParser.json());
+
+// endpoints here
+
+const port = 3000;
+server.listen(port, function() {
+    console.log(`Server listening on ${port}`);
+});
+
