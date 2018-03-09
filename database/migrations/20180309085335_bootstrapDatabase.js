334af037a600a3ea184c8de099dba49659086ef5
Samuel Cha <sunchipssam@gmail.com>
2018-03-09 09:25:19 -0800

    Created actions table

--- database/migrations/20180309085335_bootstrapDatabase.js
+++ database/migrations/20180309085335_bootstrapDatabase.js
@@ -1,10 +1,15 @@
 exports.up = function(knex, Promise) {
-  return createProjectsTable(knex).catch(err => reject(err));
+  return createProjectsTable(knex)
+    .then(createActionsTable)
+    .catch(err => console.log(err));
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema
-    .dropTableIfExists('projects')
+    .dropTableIfExists('actions')
+    .then(_ => {
+      return knex.schema.dropTableIfExists('projects');
+    })
     .catch(err => console.log(err));
 };
 
@@ -13,9 +18,24 @@ const createProjectsTable = knex => {
     knex.schema
       .createTable('projects', project => {
         project.increments();
-        project.string('name');
-        project.string('description');
+        project.string('name', 64);
+        project.string('description', 128);
         project.boolean('completed');
+
+        resolve(knex);
+      })
+      .catch(err => reject(err));
+  });
+};
+
+const createActionsTable = knex => {
+  return new Promise((resolve, reject) => {
+    knex.schema
+      .createTable('actions', action => {
+        action.increments();
+        action.string('description', 64);
+        action.string('notes', 128);
+        action.boolean('completed');
 
         resolve(knex);
       })
