6bcbaa341888f7c74417e94619c3ae932c1717bd
Simon <Simon_Freelance@hotmail.com>
2018-08-03 11:37:39 -0400

    Initial setup of routes, server, and status codes

---
+++ api/actionRouter.js
@@ -0,0 +1,9 @@
+const codes = require('./../status-codes/statusCodes');
+
+const express = require('express');
+const db = require('../');
+
+const router = express.Router();
+
+
+module.exports = router;
