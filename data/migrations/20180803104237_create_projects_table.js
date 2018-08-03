cd736f7b900083bfd21c5838b0f6fa9425278faa
Allen Hai <allenhai03@gmail.com>
2018-08-03 11:20:55 -0500

    add default values and required columns to migrations

--- data/migrations/20180803104237_create_projects_table.js
+++ data/migrations/20180803104237_create_projects_table.js
@@ -1,12 +1,13 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('projects', t => {
     t.increments('id')
-    t.string('name')
-    t.text('description')
-    t.boolean('completed')
+    t.string('name').notNullable()
+    t.text('description').notNullable()
+    t.boolean('completed').notNullable().defaultTo(false)
+    t.timestamp('createdAt').notNullable().defaultTo(knex.fn.now())
   })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('projects') 
+  return knex.schema.dropTableIfExists('projects') 
 };
--- data/migrations/20180803104251_create_actions_table.js
+++ data/migrations/20180803104251_create_actions_table.js
@@ -1,13 +1,14 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('actions', t => {
     t.increments('id')
-    t.integer('projectId')
-    t.text('description')
-    t.text('notes')
-    t.boolean('completed')
+    t.integer('projectId').notNullable()
+    t.text('description').notNullable()
+    t.text('notes').nullable()
+    t.boolean('completed').notNullable().defaultTo(false)
+    t.timestamp('createdAt').notNullable().defaultTo(knex.fn.now())
   })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('actions') 
+  return knex.schema.dropTableIfExists('actions') 
 };
