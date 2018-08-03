c5a6efac3f0e177b3ff10c8f211543869e66fc95
DirupT <iamcooled@gmail.com>
2018-08-03 11:05:54 -0500

    added delete by id to project

--- api/projects/projectRoutes.js
+++ api/projects/projectRoutes.js
@@ -3,6 +3,7 @@ const projectDb = require('../../data/helpers/projectDb');
 
 const router = express.Router();
 
+
 router.get('/', async (req, res) => {
     try {
         const response = await projectDb.get();
@@ -26,6 +27,15 @@ router.put('/:id', async (req, res) => {
     if (!name || !description) return res.status(404).json({ error: 'Please provide name and description' });
     try {
         const response = await projectDb.update(req.params.id, { name, description, completed });
+        return res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const response = await projectDb.remove(req.params.id);
         return res.status(200).json(response);
     } catch (err) {
         return res.status(500).json(err);
