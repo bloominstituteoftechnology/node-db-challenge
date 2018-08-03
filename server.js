7f06615f447753946142c0d97a7eab737a0b26df
Sukhada Gholba <sukhadagholba@gmail.com>
2018-08-03 09:55:48 -0700

    configured server file to address POST request for projects and actions to add new projects and actions to the database

--- server.js
+++ server.js
@@ -96,6 +96,63 @@ server.get('/api/actions/:id', (req, res) => {
 });
 
 
+server.post('/api/projects', (req, res) => {
+
+        const {name, description, completed} = req.body;
+        const project = {name, description, completed};
+        console.log(req.body);
+
+        if (!name || !description || completed ==="") {
+                res.status(400).json({message: "Please provide name, description and completed status for the project."});
+        }
+
+        else{
+        db.insert(project)
+	.into('projects')
+        .then(ids => {
+                const id= ids[0];
+		const message ="Successfully added a new post";
+                res.status(200).json({message, id, ...project});
+        })
+	
+        .catch(error => {
+        res.status(500).json({ message: "There was an error while saving the project to the database" });
+        })
+}
+});
+
+
+server.post('/api/actions', (req, res) => {
+
+        if(isNaN(req.body.project_id)){
+        res.status(404).json({ error: "Project_id should be a number" });
+        }
+
+        else {
+
+        const {project_id, notes, description, completed} = req.body;
+        const action = {project_id, notes, description, completed};
+
+        if (!project_id || !description || completed ==="" || !notes) {
+                res.status(400).json({message: "Please don't leave project_id, description and completed fields empty."});
+        }
+
+        else{
+        db.insert(action)
+	.into('actions')
+        .then(ids => {
+                const id= ids[0];
+                const message ="Successfully added a new action";
+                res.status(200).json({message, id, ...action});
+        })
+
+        .catch(error => {
+        res.status(500).json({ message: "There was an error while saving the action to the database" });
+        })
+}
+}
+});
+	
 
 
 
