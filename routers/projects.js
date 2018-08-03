fe8a1f2fa810997e256136915c93cfcccd0bd41d
Kevin <brackkevin@gmail.com>
2018-08-03 11:32:06 -0500

    GET and POST endpoints for projects made in projects router

--- routers/projects.js
+++ routers/projects.js
@@ -4,8 +4,29 @@ const db = require("../data/db");
 
 projectsRouter.get("/", (req, res) => {
   db("projects")
-    .then(post => {
-      res.status(200).json(post);
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(err => res.status(500).json(err.message));
+});
+
+projectsRouter.get("/:id", (req, res) => {
+  const id = req.params.id;
+  db("projects")
+    .where("id", Number(id))
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(err => res.status(500).json(err.message));
+});
+
+projectsRouter.post("/", (req, res) => {
+  const project = req.body;
+  db.insert(project)
+    .into("projects")
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ id, ...post });
     })
     .catch(err => res.status(500).json(err.message));
 });
