9e1612db88f92bce8dab76bbfd94f6a9737710de
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-08-03 13:05:20 -0500

    initialized project

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const knex = require('knex');
+const cors = require('cors');
+
+const projects = require('./data/helpers/projectHelpers.js');
+const actions = require('./data/helpers/actionHelpers.js');
+
+const port = 5555;
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send('Up and running...')
+  })
+
+server.listen(port, () => console.log('API running...'))
