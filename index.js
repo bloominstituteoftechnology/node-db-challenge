1735d4635c797db91f442a6f544ff336952895c2
Wdifulvio523 <w.difulvio523@gmail.com>
2018-08-03 12:00:57 -0400

    Creating endpoints and testing CRUD operations for database

--- index.js
+++ index.js
@@ -18,8 +18,76 @@ server.get('/projects', (req, res) =>{
     .then(projects => {
         res.status(200).json(projects);
     })
+    .catch(err => {
+        res.status(500).json({ error: "The projects could not be retrieved." });
+      });
+})
+
+//Post New Project
+server.post('/projects', (req, res) => {
+    const { name, description } = req.body;
+    if (!name || !description) {
+        res.status(400).json({
+          errorMessage:
+            "Please provide name and description for the project."
+        });
+        return;
+      }
+    db()
+    .insert({ name, description })
+    .into('projects')
+    .then(project => {
+        res.status(201).json({ name, description })
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: "There was an error while saving the project to the database"
+          });
+      });
+})
+
+
+
+
+
+
+
+
+
+// ACTIONS------------------------------------------------------------------------------
+//Get All Actions
+server.get('/actions', (req, res) =>{
+    db('actions')
+    .then(actions => {
+        res.status(200).json(actions);
+    })
     .catch(err => res.status(500).json(err))
 })
+
+
+//Post New Action
+server.post('/actions', (req, res) => {
+    const { project_id, description, notes } = req.body;
+    if (!project_id || !description) {
+        res.status(400).json({
+          errorMessage:
+            "Please provide project id and description for the action."
+        });
+        return;
+      }
+    db()
+    .insert({ project_id, description, notes })
+    .into('actions')
+    .then(project => {
+        res.status(201).json({ project_id, description, notes})
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: "There was an error while saving the action to the database"
+          });
+      });
+})
+
 
 
 const port = 3300;
