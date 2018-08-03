531b6d687ddb7874da049f89ca39866f14257d24
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-08-03 14:32:35 -0400

    actionRoutes, MVP

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -1,9 +1,80 @@
 const express = require('express');
+const db = require('../data/helpers/actionDB');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Actions');
+// GET Action
+
+router.get('/', async (req, res) => {
+    try {
+        const response = await db.get();
+        res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
+
+// GET :id Action
+
+router.get('/:id', async(req, res) => {
+    const id = req.params.id;
+    try {
+        const response = await db.get(id);
+        if (response.length===0) {
+            return res.status(404, 'Failed to retrieve action information')
+        }
+        res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
+
+// POST Action
+
+router.post('/', async (req, res) => {
+    if (!(req.body.notes && req.body.description && req.body.project_id)) {
+        return res.status(400, 'Failed to save actions to database.')
+    }
+    try {
+        const response = await db.insert(req.body);
+        res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
+
+// PUT Action
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    if ((!req.body.description && !req.body.notes && !req.body.project_id)) {
+        return res.status(400, 'Failed to update actions.')
+    } 
+    try {
+        const response = await db.update(id, req.body);
+        if (response===null) {
+            return res.status(404, 'Failed to update action.')
+        }
+        res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
+
+// DELETE Action
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const action = await db.get(id);
+        if (action.length===0) {
+            return res.status(404, 'Failed to remove action.')
+        }
+        await db.remove(id);
+        res.status(200).json(action);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
 })
 
 module.exports = router;
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -5,33 +5,33 @@ const router = express.Router();
 
 // GET Project
 
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res) => {
     try {
         const response = await db.getProjects();
         res.status(200).json(response);
-    } catch (error) {
-        next(sendError(500, 'Failed to retrieve projects information.', error.message))
+    } catch (err) {
+        return res.status(500).json(err);
     }
 })
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', async (req, res) => {
     const id = Number(req.params.id);
     try {
         const response = await db.findProjectById(id);
         if (!response) {
-            return next(sendError(404, 'Failed to retrieve project information.'))
+            return res.status(400, 'Failed to retrieve project information.')
         } 
         res.status(200).json(response);
-    } catch (error) {
-        next(sendError(500, 'Failed to retrieve project information.', error.message))
+    } catch (err) {
+        return res.status(500).json(err);
     }
 })
 
 //POST Project 
 
-router.post('/', async (req, res, next) => {
+router.post('/', async (req, res) => {
     if (!(req.body.name && req.body.description)) {
-        return next(sendError(400, "Failed to save project to database."))
+        return res.status(400, "Failed to save project to database.")
     }
     try {
         const response = await db.addProject(req.body);
@@ -41,49 +41,49 @@ router.post('/', async (req, res, next) => {
             completed: false
         }
         res.status(200).json(newProject);
-    } catch (error) {
-        next(sendError(500, "Failed to save project to database.", error.message))
+    } catch (err) {
+        return res.status(500).json(err);
     }
 })
 
 // UPDATE Project
 
-router.put('/:id', async (req, res, next) => {
+router.put('/:id', async (req, res) => {
     const id = Number(req.params.id);
     if ((!req.body.name && !req.body.description)) {
-        return next(sendError(400, "Failed to update project."))
+        return res.status(400, "Failed to update project.")
     } 
 
     try {
         const response = await db.updateProject(id, req.body);
         if (!response){
-            return next(sendError(404, 'Failed to update project.'))
+            return res.status(404, 'Failed to update project.')
         }
         const newProj = {
             id,
             ...req.body
         }
         res.status(200).json(newProj);
-    } catch (error) {
-        next(sendError(500, "Failed to update project.", error.message))
+    } catch (err) {
+        return res.status(500).json(err);
     }
 })
 
 // DELETE
 
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:id', async (req, res) => {
     const id = Number(req.params.id);
 
     try {
         const response = await db.deleteProject(id);
         if (response[1] === 0){
-            return next(sendError(404, 'Failed to delete project.'))
+            return res.status(404, 'Failed to delete project.')
         }
         const project = response[0][0]
         project.competed = project.completed ? true: false;
         res.status(200).json(project);
-    } catch (error) {
-        next(sendError(500, "Failed to delete project.", error.message))
+    } catch (err) {
+        return res.status(500).json(err);
     }
 })
 
