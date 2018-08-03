a0ca47d1faa4df87924ec2dde2a240297023c9c8
Kevin <brackkevin@gmail.com>
2018-08-03 11:56:18 -0500

    Add POST PUT DELETE methods to actions router

--- routers/actions.js
+++ routers/actions.js
@@ -21,4 +21,39 @@ actionsRouter.get("/:id", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+actionsRouter.post("/", (req, res) => {
+  const action = req.body;
+  db.insert(action)
+    .into("actions")
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ id, post });
+    })
+    .catch(err => res.status(500).json(err.message));
+});
+
+actionsRouter.put("/:id", (req, res) => {
+  const changes = req.body;
+  const id = req.params.id;
+  db("actions")
+    .where("id", id)
+    .update(changes)
+    .then(ids => {
+      const id = ids[0];
+      res.status(200).json({ id, ...changes });
+    })
+    .catch(err => res.status(500).json(err.message));
+});
+
+actionsRouter.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  db("actions")
+    .where("id", id)
+    .del()
+    .then(ids => {
+      res.status(200).json("ACTION DELETED SUCCESSFULLY");
+    })
+    .catch(err => res.status(500).json(err.message));
+});
+
 module.exports = actionsRouter;
