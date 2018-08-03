1dd65eb84910ab43988217d948741a9d63be04cd
chainchompa <chainchompa@gmail.com>
2018-08-03 11:54:13 -0400

    added action and project routes

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
 const db = require('./data/db.js');
-
 const server = express();
+
+const projectRoutes = require('./routes/projectRoutes');
+const actionRoutes = require('./routes/actionRoutes');
 
 server.use(express.json());
 
+server.use('/projects', projectRoutes);
+server.use('/actions', actionRoutes);
+server.use('/', (req, res) => res.send('API up and running!'));
 
 const port = 8000;
 server.listen(port, function() {
