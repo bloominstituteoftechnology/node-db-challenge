8891889eb1a2c88ff41b5c05d257b65016ccfc1d
Simon <Simon_Freelance@hotmail.com>
2018-08-03 13:27:46 -0400

    Made actions appear by project id

--- api/actionRouter.js
+++ api/actionRouter.js
@@ -5,5 +5,16 @@ const db = require('../data/dbConfig');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+    db('actions')
+    .then(response => {
+        res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+        next(err);
+    })
+})
+
+
 
 module.exports = router;
