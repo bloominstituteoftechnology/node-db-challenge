51e1c21ad45ab5539e0bd7169c59829ebbfcde8e
Gabriel Duquette <gabrielduquette@gmail.com>
2018-08-03 12:24:03 -0400

    projectLibrary and projectRoutes done, GET :id not working

--- api/projectRoutes.js
+++ api/projectRoutes.js
@@ -1,43 +1,60 @@
 const express = require('express');
 const router = express.Router();
+const projects = require('../data/helpers/projectLibrary');
 
 router.get('/', async (req, res) => {
   try {
-
+    const allProjects = await projects.get();
+    return res.status(200).json(allProjects);
   } catch (error) {
-    res.status(500).json({  });
+    return res.status(500).json({ message: "Projects could not be retrieved." });
   }
 });
 
 router.get('/:id', async (req, res) => {
   try {
-
+    const project = await projects.get(req.params.id);
+    if (project.length === 0) {
+      return res.status(404).json({ message: "Project does not exist." });
+    }
+    return res.status(200).json(project);
   } catch (error) {
-    res.status(500).json({  });
+    return res.status(500).json({ message: "Project could not be retrieved." });
   }
 });
 
 router.post('/', async (req, res) => {
   try {
-
+    const newProject = await projects.insert(req.body);
+    return res.status(201).json(newProject);
   } catch (error) {
-    res.status(500).json({  });
+    return res.status(500).json({ message: "Project could not be added." });
   }
 });
 
 router.put('/:id', async (req, res) => {
   try {
-
+    const editedProject = await projects.update(req.params.id, req.body);
+    if (editedProject === 0) {
+      return res.status(404).json({ message: "Project does not exist." });
+    } else {
+      return res.status(200).json(editedProject);
+    }
   } catch (error) {
-    res.status(500).json({  });
+    return res.status(500).json({ message: "Project could not be edited." });
   }
 });
 
 router.delete('/:id', async (req, res) => {
   try {
-
+    const deletedProject = await projects.delete(req.params.id);
+    if (deletedProject === 0) {
+      return res.status(404).json({ message: "Project does not exist." });
+    } else {
+      return res.status(200).json(deletedProject);
+    }
   } catch (error) {
-    res.status(500).json({  });
+    res.status(500).json({ message: "Project could not be deleted." });
   }
 });
 
---
+++ data/helpers/projectLibrary.js
@@ -0,0 +1,32 @@
+const db = require('../db');
+
+module.exports = {
+  get: (id) => {
+    let projects = db('projects');
+
+    if (id) {
+      projects.where('projects', id).first();
+      const actions = [projects, db('actions').where('project_id', id)];
+      return Promise.all(actions).then(results => {
+        console.log(results);
+        if (results[1].length > 0) {
+          let [project, projectActions] = results;
+          project.actions = projectActions;
+          return project;
+        }
+        return results[0];
+      })
+    }
+
+    return projects;
+  },
+  insert: (project) => {
+    return db('projects').insert(project);
+  },
+  update: (id, editedProject) => {
+    return db('projects').where('id', id).update(editedProject);
+  },
+  delete: (id) => {
+    return db('projects').where('id', id).del();
+  }
+}
