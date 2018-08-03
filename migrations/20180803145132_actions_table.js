861752a1436aa329e41e572f032af0d1f5f77636
JJ Ashcraft <jj@jjashcraft.com>
2018-08-03 14:56:35 -0400

    initial commit * created projects table with knex migrations * created actions table with knex migrations * updated knexfile * added dependencies

---
+++ migrations/20180803145132_actions_table.js
@@ -0,0 +1,16 @@
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('actions', tbl => {
+        tbl.increments();
+        tbl.integer('project_id')
+            .references('id')
+            .inTable('projects');
+        tbl.string('description', 128).notNullable();
+        tbl.text('notes').notNullable();
+        tbl.boolean('completed').defaultTo(false);
+
+    })
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('actions');
+};
