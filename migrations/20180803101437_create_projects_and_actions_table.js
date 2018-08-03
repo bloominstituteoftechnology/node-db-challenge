b45ebc074ccaa867dae2442690613b02d2cb6218
Mariana Cerda <raymond_grc@yahoo.com>
2018-08-03 10:35:58 -0500

    Init, Add migration, untested

---
+++ migrations/20180803101437_create_projects_and_actions_table.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+  return knex.scheme.createTable('project', function (table){
+    table.increments();
+    table.string('name').notNullable;
+    table.string('description');
+    table.boolean('completed').defaultTo(false);
+  })
+  .createTable('actions', function(table){
+    table.increments();
+    table.string('description');
+    table.string('notes');
+    table.boolean('completed').defaultTo(false);
+    table.integer('project_id').references('id').inTable('project')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.scheme.dropTable('actions').dropTable('project')
+};
