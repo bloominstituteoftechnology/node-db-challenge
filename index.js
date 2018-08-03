81fbca40306851f8ccab7d241171a5e911f12a4a
Alec <afroenygam@gmail.com>
2018-08-03 13:05:38 -0400

    All CRUD operations updated for projects and actions

--- index.js
+++ index.js
@@ -27,6 +27,78 @@ server.get('/projects/:id', async (req, res) => {
   }
 });
 
+server.post('/projects', async (req,  res) => {
+  const {name, description} = req.body;
+  try {
+    const ids = await dbConfig.insert({name, description}).into('projects');
+    const id = ids[0];
+    res.status(201).json(await dbConfig('projects').where('id', id));
+  } catch(err) {
+    res.status(500).send(`Ya done goofed with ${err}`);
+  }
+});
+
+server.delete('/projects/:id', async (req, res) => {
+  try {
+    const result = await dbConfig('projects').where('id', req.params.id).del();
+    res.status(200).json(result);
+  } catch(err) {
+    res.status(500).send(`Try again and ${err}`);
+  }
+});
+
+server.put('/projects/:id', async (req, res) => {
+  const {name, description} = req.body;
+  try {
+    const index = await dbConfig('projects').where('id', req.params.id).first().update({name, description});
+    if (index > 0) return res.status(200).json(await dbConfig('projects').where('id', req.params.id).first());
+    res.status(200).send("It didn't trigger");
+  } catch(err) {
+    res.status(500).send(`Try again and ${err}`);
+  }
+});
+//END PROJECTS CRUD
+
+server.get('/actions', async (req, res) => {
+  try {
+    const actions = await dbConfig('actions');
+    res.status(200).json(actions);
+  } catch (err) {
+    res.status(500).send(`You done goofed with ${err}`);
+  }
+
+});
+
+server.post('/actions', async (req,  res) => {
+  const {project_id, description, notes} = req.body;
+  try {
+    const ids = await dbConfig.insert({project_id, description, notes}).into('actions');
+    const id = ids[0];
+    res.status(201).json(await dbConfig('actions').where('id', id));
+  } catch(err) {
+    res.status(500).send(`Ya done goofed with ${err}`);
+  }
+});
+
+server.delete('/actions/:id', async (req, res) => {
+  try {
+    const result = await dbConfig('actions').where('id', req.params.id).del();
+    res.status(200).json(result);
+  } catch(err) {
+    res.status(500).send(`Try again and ${err}`);
+  }
+});
+
+server.put('/actions/:id', async (req, res) => {
+  const {project_id, description, notes} = req.body;
+  try {
+    const index = await dbConfig('actions').where('id', req.params.id).first().update({project_id, description, notes});
+    if (index > 0) return res.status(200).json(await dbConfig('actions').where('id', req.params.id).first());
+    res.status(200).send("It didn't trigger");
+  } catch(err) {
+    res.status(500).send(`Try again and ${err}`);
+  }
+});
 
 
 server.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
