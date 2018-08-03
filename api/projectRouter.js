5f059139c0755cfca1f6dc2177f417e15480ae05
Simon <Simon_Freelance@hotmail.com>
2018-08-03 12:12:57 -0400

    GET requests for all project and project by id completed

--- api/projectRouter.js
+++ api/projectRouter.js
@@ -5,5 +5,24 @@ const db = require('../data/dbConfig');
 
 const router = express.Router();
 
-
+router.get('/', (req, res, next) => {
+    db('projects')
+    .then(response => {
+        res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+        next(err);
+    })
+})
+router.get('/:id', (req, res, next) => {
+    const { id } = req.params;
+    db('projects')
+    .where('id', id)
+    .then(response => {
+        res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+        next(err);
+    })
+})
 module.exports = router;
