755b01d8ab58754134797fff298cc8864c5da20a
TangledTessellations <mcevoya@oregonstate.edu>
2018-08-03 10:32:12 -0700

    Add route handlers to actions

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ server.get('/api/projects', (req, res) => {
         .catch(err => res.status(500).json(err))
 })
 
+server.get('/api/projects/:id', (req,res) => {
+    const {id} = req.params
+    db('projects')
+        .where({id})
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => res.status(500).json(err.message))
+})
+
 server.post('/api/projects', (req,res) => {
     const project = req.body
 
@@ -72,12 +82,64 @@ server.put('/api/projects/:id', (req, res) => {
 })
 // ************************************  actions *********************************
 
+
+server.get('/api/actions', (req, res) => {
+    db('actions')
+        .then(actions => {
+            res.status(200).json(actions)
+        })
+        .catch(err => res.status(500).json(err))
+})
+
+server.get('/api/actions/:id', (req, res) => {
+    const {id} = req.params
+    db('actions')
+        .where({id})
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(err => res.status(500).json(err.message))
+})
+
+
 server.post('/api/actions', (req, res) => {
     const action = req.body
     db.insert(action)
         .into('actions')
         .then(id => {
             res.status(200).json({inserted_Id : id, inserted : action})
+        })
+        .catch(err => res.status(500).json(err.message))
+})
+
+server.delete('/api/actions/:id', (req, res) => {
+    const {id} = req.params
+    db('actions')
+        .where({id})
+        .del()
+        .then(id => {
+            console.log(id)
+            res.status(200).json({"Deleted_id" : id})
+        })
+        .catch(err => res.status(500).json(err.message))
+})
+
+server.put('/api/actions/:id', (req, res) => {
+    const {id} = req.params
+    const updated = {...req.body}
+    db('actions')
+        .where({id})
+        .then(count => {
+            if(count.length > 0){
+                db('actions')
+                    .where({id})
+                    .update(updated)
+                    .then(
+                        res.status(200).json({updated})
+                    )
+            }else{
+                res.status(404).json({error: "No action by that ID"})
+            }
         })
         .catch(err => res.status(500).json(err.message))
 })
