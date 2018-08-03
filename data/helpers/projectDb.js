dfbd1f01d06d5517165b6a2e2c32c32ac036bf24
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-08-03 13:57:47 -0400

    Projects CRUD Done

--- data/helpers/projectDb.js
+++ data/helpers/projectDb.js
@@ -1,22 +1,56 @@
 const db = require('../db');
 
 module.exports = {
-    get: id => {
-        let query = db('project');
+    getProjects: function() {
+        const query = db('projects');
+        return query.then(projects => {
+            return projects.map(project => {
+                return {
+                    ...project,
+                    completed: project.completed ? true : false
+                }
+            })
+        })
+    },
 
-        if (id) {
-            return query.where('id', id).first();
-        }
+    findProjectById: function(id) {
+        const projectQuery = db('projects').where('id', id);
+        const actionsQuery = db('actions').where('project_id', id).select('id', 'description', 'notes', 'completed');
 
-        return query;
+        const promises = [projectQuery, actionsQuery]
+        return Promise.all(promises)
+        .then(results => {
+            const [projects, actions ] = results;
+
+            if (projects.length===0) {
+                return 0
+            } else {
+                projects.forEach(project => project.completed = project.completed ? true : false)
+            }
+
+            if (actions.length!==0) {
+                actions.forEach(action => action.completed = action.completed ? true : false)
+            }
+
+            return {
+                ...projects[0],
+                actions
+            }
+        })
     },
-    insert: project => {
-        return db('project').insert(project).then(ids => ({ id: ids[0] }));
+
+    addProject: function(project) {
+        return db('projects').insert(project);
     },
-    update: (id, project) => {
-        return db('project').where('id', id).update(project);
+
+    deleteProject: function(id) {
+        const project = db('projects').where('id', id);
+        const deleteResponse = db('projects').where('id', id).del();
+        const promises = [project, deleteResponse]
+        return Promise.all(promises)
     },
-    remove: id => {
-        return db('project').where('id', id).del();
+
+    updateProject: function(id, changes) {
+        return db('projects').where('id', id).update(changes);
     }
 }
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,14 +1,92 @@
 const express = require('express');
-const projectModel = require('../data/helpers/projectDb')
+const db = require('../data/helpers/projectDb')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    projectModel
-        .get()
-        .then(users => {res.json(users)})
-        .catch(err => {res.status(500).json({ err: 'Unable to retrieve project information!' })
-    })
+// GET Project
+
+router.get('/', async (req, res, next) => {
+    try {
+        const response = await db.getProjects();
+        res.status(200).json(response);
+    } catch (error) {
+        next(sendError(500, 'Failed to retrieve projects information.', error.message))
+    }
 })
+
+router.get('/:id', async (req, res, next) => {
+    const id = Number(req.params.id);
+    try {
+        const response = await db.findProjectById(id);
+        if (!response) {
+            return next(sendError(404, 'Failed to retrieve project information.'))
+        } 
+        res.status(200).json(response);
+    } catch (error) {
+        next(sendError(500, 'Failed to retrieve project information.', error.message))
+    }
+})
+
+//POST Project 
+
+router.post('/', async (req, res, next) => {
+    if (!(req.body.name && req.body.description)) {
+        return next(sendError(400, "Failed to save project to database."))
+    }
+    try {
+        const response = await db.addProject(req.body);
+        const newProject = {
+            id: response[0],
+            ...req.body,
+            completed: false
+        }
+        res.status(200).json(newProject);
+    } catch (error) {
+        next(sendError(500, "Failed to save project to database.", error.message))
+    }
+})
+
+// UPDATE Project
+
+router.put('/:id', async (req, res, next) => {
+    const id = Number(req.params.id);
+    if ((!req.body.name && !req.body.description)) {
+        return next(sendError(400, "Failed to update project."))
+    } 
+
+    try {
+        const response = await db.updateProject(id, req.body);
+        if (!response){
+            return next(sendError(404, 'Failed to update project.'))
+        }
+        const newProj = {
+            id,
+            ...req.body
+        }
+        res.status(200).json(newProj);
+    } catch (error) {
+        next(sendError(500, "Failed to update project.", error.message))
+    }
+})
+
+// DELETE
+
+router.delete('/:id', async (req, res, next) => {
+    const id = Number(req.params.id);
+
+    try {
+        const response = await db.deleteProject(id);
+        if (response[1] === 0){
+            return next(sendError(404, 'Failed to delete project.'))
+        }
+        const project = response[0][0]
+        project.competed = project.completed ? true: false;
+        res.status(200).json(project);
+    } catch (error) {
+        next(sendError(500, "Failed to delete project.", error.message))
+    }
+})
+
+
 
 module.exports = router;
