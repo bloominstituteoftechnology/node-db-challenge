ed1afde6a61c58a1d6d65c6f290d368d1b6ba658
Kevin Tena <kevten09@gmail.com>
2018-08-03 12:25:00 -0500

    CRUD for Projects complete

--- index.js
+++ index.js
@@ -6,6 +6,69 @@ const server = express();
 
 server.use(express.json());
 
+server.post('/projects', (req, res) => {
+    const project = req.body;
+    db.insert(project)
+        .into('projects')
+        .then(ids => {
+            res.status(201).json(ids);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+server.get('/projects', (req, res) => {
+    db.select()
+        .from('projects')
+        .then(projects => {
+            res.status(201).json(projects);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+
+server.get('/projects/:id', (req, res) => {
+    const {id} = req.params;
+    db('projects')
+        .where({id})
+        .then(projects => {
+            res.status(201).json(projects);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+server.put('/projects/:id', (req, res) => {
+    const changes = req.body;
+    const {id} = req.params;
+    db('projects')
+        .where({id})
+        .update(changes)
+        .then(count => {
+            res.status(201).json(count);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+server.delete('/projects/:id', (req, res) => {
+    const {id} = req.params;
+    db('projects')
+        .where({id})
+        .del()
+        .then(count => {
+            res.status(201).json(count);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
 
 const port = 8000;
 server.listen(port, function() {
