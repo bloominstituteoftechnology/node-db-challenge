450ce6949ded833200fd28446688ecc94ab10e31
Mariana Cerda <raymond_grc@yahoo.com>
2018-08-03 11:06:42 -0500

    Add project post method

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const server = express();
+
+const db = require("./data/db")
+
+server.use(express.json());
+
+const port = 8000;
+
+server.get("/projects", (req, res) => {
+  db.select().from('project')
+  .then(response => {
+    res.json({"Message": response});
+  })
+  .catch(error => {
+   res.status(500).send({ error: "Server Error" })
+ })
+});
+server.post("/projects", (req, res) => {
+  const { name, description, completed } = req.body;
+  const toBool = completed == "true";
+  db.insert({"name": name,
+              "description" : description,
+              "completed": toBool}).into('project')
+    .then(response => (res.json(response)))
+});
+
+
+
+
+
+
+
+server.listen(port, () => { console.log(`Server is running on port ${port}`)});
