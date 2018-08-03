306fd1de9b421dae60fc86384042e1332318f853
Kevin <brackkevin@gmail.com>
2018-08-03 11:46:08 -0500

    Add actions router GET methods

--- routers/actions.js
+++ routers/actions.js
@@ -2,4 +2,23 @@ const express = require("express");
 const actionsRouter = express.Router();
 const db = require("../data/db");
 
+actionsRouter.get("/", (req, res) => {
+  db("actions")
+    .then(action => {
+      res.status(200).json(action);
+    })
+    .catch(err => res.status(500).json(err.message));
+});
+
+actionsRouter.get("/:id", (req, res) => {
+  const id = req.params.id;
+  db("actions")
+    .where("id", id)
+    .first()
+    .then(action => {
+      res.status(200).json(action);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
 module.exports = actionsRouter;
