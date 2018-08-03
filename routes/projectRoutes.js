2a0a2fc11b08f55c5334d6e46014fc7410292988
chainchompa <chainchompa@gmail.com>
2018-08-03 12:03:01 -0400

    added endpoints to get projects and actions

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const db = require('../data/db');
 
+router.get('/', (req, res) => {
+  db('projects')
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 
 module.exports = router;
