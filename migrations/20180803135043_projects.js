41debd99d5c8e5326b06cefdc336751ab7305f33
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-08-03 13:58:12 -0500

    projects & actions tables created using migrations

---
+++ migrations/20180803135043_projects.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('projects', function(projects) {
+        projects.increments();
+    
+        projects.string('name').notNullable();
+        projects.text('description').notNullable();
+        projects.boolean('completed').defaultTo(false);
+      });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('projects');
+};
