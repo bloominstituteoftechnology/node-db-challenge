d0619a827c755b577de99d353a31cb7d182e7a5a
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-08-03 08:42:08 -0700

    sets up migrations

---
+++ data/migrations/20180803083705_actions.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('actions', function(t) {
+    t.increments(); // PK defaults to 'id'
+    t.string('name')
+      .unique()
+      .notNullable();
+    t.string('description').notNullable();
+    t.boolean('done').notNullable();
+    t.integer('created_at')
+      .notNullable()
+      .defaultTo(knex.raw('CURRENT_TIMESTAMP'));
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('actions');
+};
