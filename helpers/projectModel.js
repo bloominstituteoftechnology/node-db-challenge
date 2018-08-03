5311e7b400c32413dc89dc290d7370fd35ede032
Wdifulvio523 <w.difulvio523@gmail.com>
2018-08-03 13:55:56 -0400

    Minor changes/formatting to make it easier to read, should not affect project

--- helpers/projectModel.js
+++ helpers/projectModel.js
@@ -1,33 +1,31 @@
-const db = require('../data/db');
-const mappers = require('./mappers');
-
-
+const db = require("../data/db");
+const mappers = require("./mappers");
 
 module.exports = {
-    get: function(id) {
-        let query = db('projects as p');
-    
-        if (id) {
-          query.where('p.id', id).first();
-    
-          const promises = [query, this.getProjectActions(id)]; // [ projects, actions ]
-    
-          return Promise.all(promises).then(function(results) {
-            let [project, actions] = results;
-            project.actions = actions;
-    
-            return mappers.projectToBody(project);
-          });
-        }
-    
-        return query.then(projects => {
-          return projects.map(project => mappers.projectToBody(project));
-        });
-      },
+  get: function(id) {
+    let query = db("projects as p");
 
-      getProjectActions: function(projectId) {
-        return db('actions')
-          .where('project_id', projectId)
-          .then(actions => actions.map(action => mappers.actionToBody(action)));
-      },
-}
+    if (id) {
+      query.where("p.id", id).first();
+
+      const promises = [query, this.getProjectActions(id)]; // [ projects, actions ]
+
+      return Promise.all(promises).then(function(results) {
+        let [project, actions] = results;
+        project.actions = actions;
+
+        return mappers.projectToBody(project);
+      });
+    }
+
+    return query.then(projects => {
+      return projects.map(project => mappers.projectToBody(project));
+    });
+  },
+
+  getProjectActions: function(projectId) {
+    return db("actions")
+      .where("project_id", projectId)
+      .then(actions => actions.map(action => mappers.actionToBody(action)));
+  }
+};
--- index.js
+++ index.js
@@ -1,192 +1,181 @@
-const express = require('express');
-
-const db = require('./data/db.js');
+const express = require("express");
+const db = require("./data/db.js");
 const projectModel = require("./helpers/projectModel");
-
-
 const server = express();
-
 server.use(express.json());
 
-///endpoints go here
-server.get('/', (req, res) => {
-    res.send('We runnin....')
-  })
+///Endpoints
+server.get("/", (req, res) => {
+  res.send("We runnin....");
+});
 
 // PROJECTS------------------------------------------------------------------------------
 //Get All Projects
-server.get('/projects', (req, res) =>{
-    db('projects')
+server.get("/projects", (req, res) => {
+  db("projects")
     .then(projects => {
-        res.status(200).json(projects);
+      res.status(200).json(projects);
     })
     .catch(err => {
-        res.status(500).json({ error: "The projects could not be retrieved." });
-      });
-})
-
+      res.status(500).json({ error: "The projects could not be retrieved." });
+    });
+});
 
 //Post New Project
-server.post('/projects', (req, res) => {
-    const { name, description } = req.body;
-    if (!name || !description) {
-        res.status(400).json({
-          errorMessage:
-            "Please provide name and description for the project."
-        });
-        return;
-      }
-    db()
+server.post("/projects", (req, res) => {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    res.status(400).json({
+      errorMessage: "Please provide name and description for the project."
+    });
+    return;
+  }
+  db()
     .insert({ name, description })
-    .into('projects')
+    .into("projects")
     .then(project => {
-        res.status(201).json({ name, description })
+      res.status(201).json({ name, description });
     })
     .catch(err => {
-        res.status(500).json({
-            error: "There was an error while saving the project to the database"
-          });
+      res.status(500).json({
+        error: "There was an error while saving the project to the database"
       });
-})
+    });
+});
 
 // Update project
 server.put("/projects/:id", (req, res) => {
-    const { id } = req.params;
-    const { name, description } = req.body;
-    if (!name || !description) {
-      res.status(400).json({
-        errorMessage: "Please provide name and description for the project."
-      });
+  const { id } = req.params;
+  const { name, description } = req.body;
+  if (!name || !description) {
+    res.status(400).json({
+      errorMessage: "Please provide name and description for the project."
+    });
+    return;
+  }
+  db("projects")
+    .where("id", Number(id))
+    .update({ name, description })
+    .into("projects")
+    .then(response => {
+      res.status(200).json({ name, description });
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The project information could not be modified." });
       return;
-    }
-    db('projects')
-    .where('id', Number(id))
-      .update({ name, description })
-      .into('projects')
-      .then(response => {
-        res.status(200).json({ name, description });
-      })
-      .catch(error => {
-        res
-          .status(500)
-          .json({ error: "The project information could not be modified." });
-        return;
-      });
-  });
-
+    });
+});
 
 //Delete Project
-server.delete('/projects/:id', (req, res) => {
-    const { id } = req.params;
-    db('projects')
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  db("projects")
     .where("id", Number(id))
     .delete()
     .then(projects => {
-        if(projects.length === 0) {
-            res.status(404).json({ message: "That ID doesn't exists"});
-        }
-        res.status(200).json({message: "Success in deleting Project"});
+      if (projects.length === 0) {
+        res.status(404).json({ message: "That ID doesn't exists" });
+      }
+      res.status(200).json({ message: "Success in deleting Project" });
     })
     .catch(error => {
-        res.status(500).json({ error: "Error Deleting Project"})
+      res.status(500).json({ error: "Error Deleting Project" });
     });
-})
+});
 
 //Get Specific Project by ID
-server.get('/projects/:id', (req, res) => {
-    projectModel
+server.get("/projects/:id", (req, res) => {
+  projectModel
     .get(req.params.id)
-   .then(project => {
-       res.status(200).json(project)
-   })
-   .catch(err => {
-    res
-      .status(500)
-      .json({ error: "The project information could not be retrieved." });
-  })
-})
-
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The project information could not be retrieved." });
+    });
+});
 
 // ACTIONS------------------------------------------------------------------------------
 //Get All Actions
-server.get('/actions', (req, res) =>{
-    db('actions')
+server.get("/actions", (req, res) => {
+  db("actions")
     .then(actions => {
-        res.status(200).json(actions);
+      res.status(200).json(actions);
     })
-    .catch(err => res.status(500).json(err))
-})
-
+    .catch(err => res.status(500).json(err));
+});
 
 //Post New Action
-server.post('/actions', (req, res) => {
-    const { project_id, description, notes } = req.body;
-    if (!project_id || !description) {
-        res.status(400).json({
-          errorMessage:
-            "Please provide project id and description for the action."
-        });
-        return;
-      }
-    db()
+server.post("/actions", (req, res) => {
+  const { project_id, description, notes } = req.body;
+  if (!project_id || !description) {
+    res.status(400).json({
+      errorMessage: "Please provide project id and description for the action."
+    });
+    return;
+  }
+  db()
     .insert({ project_id, description, notes })
-    .into('actions')
+    .into("actions")
     .then(project => {
-        res.status(201).json({ project_id, description, notes})
+      res.status(201).json({ project_id, description, notes });
     })
     .catch(err => {
-        res.status(500).json({
-            error: "There was an error while saving the action to the database"
-          });
+      res.status(500).json({
+        error: "There was an error while saving the action to the database"
       });
-})
-
+    });
+});
 
 // Update Action
 server.put("/actions/:id", (req, res) => {
-    const { id } = req.params;
-    const {  project_id, description, notes} = req.body;
-    if (!project_id || !description) {
-      res.status(400).json({
-        errorMessage: "Please provide project ID and description for the action."
-      });
+  const { id } = req.params;
+  const { project_id, description, notes } = req.body;
+  if (!project_id || !description) {
+    res.status(400).json({
+      errorMessage: "Please provide project ID and description for the action."
+    });
+    return;
+  }
+  db("actions")
+    .where("id", Number(id))
+    .update({ project_id, description, notes })
+    .into("actions")
+    .then(response => {
+      res.status(200).json({ project_id, description, notes });
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The action information could not be modified." });
       return;
-    }
-    db('actions')
-    .where('id', Number(id))
-      .update({ project_id, description, notes })
-      .into('actions')
-      .then(response => {
-        res.status(200).json({ project_id, description, notes });
-      })
-      .catch(error => {
-        res
-          .status(500)
-          .json({ error: "The action information could not be modified." });
-        return;
-      });
-  });
+    });
+});
 
 //Delete Action
-server.delete('/actions/:id', (req, res) => {
-    const { id } = req.params;
-    db('actions')
+server.delete("/actions/:id", (req, res) => {
+  const { id } = req.params;
+  db("actions")
     .where("id", Number(id))
     .delete()
     .then(actions => {
-        if(actions.length === 0) {
-            res.status(404).json({ message: "That ID doesn't exists"});
-        }
-        res.status(200).json({message: "Success in deleting action"});
+      if (actions.length === 0) {
+        res.status(404).json({ message: "That ID doesn't exists" });
+      }
+      res.status(200).json({ message: "Success in deleting action" });
     })
     .catch(error => {
-        res.status(500).json({ error: "Error Deleting action"})
+      res.status(500).json({ error: "Error Deleting action" });
     });
-})
-
+});
 
 const port = 3300;
 server.listen(port, function() {
- console.log(`\n=== Yo Yo, Web API Listening on http://localhost:${port} ===\n`);
+  console.log(
+    `\n=== Yo Yo, Web API Listening on http://localhost:${port} ===\n`
+  );
 });
-
