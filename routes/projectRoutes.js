eb30b20dfd9a7f7e88d6e3145adb6d9f6e6356e2
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-08-03 11:43:46 -0400

    CRUD for Projects complete

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -4,4 +4,64 @@ const db = require('../data/db');
 
 // endpoints go here
 
+server.get('/', (req, res) => {
+    db('projects')
+        .then(project => {
+            res.status(200).json(project);
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+server.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db('projects')
+        .where({ id: Number(id) })
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+server.post('/', (req, res) => {
+    const project = req.body;
+    const { name, description, completed } = project;
+
+    db.insert(project)
+        .into('projects')
+        .then(ids => {
+            const id = ids[0];
+            res.status(201).json({ id, ...project });
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+server.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    db('projects')
+        .where({ id: Number(id) })
+        .delete()
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+server.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const {name, description, completed} = req.body;
+    if (!name) res.status(400).json({ err });
+    else {
+        db('projects')
+            .where({ id: Number(id) })
+            .update({name, description, completed})
+            .then(project => {
+                if (project > 0) res.status(200).json(project);
+                else res.status(400).json({ err });
+            })
+            .catch(err => res.status(500).json(err));
+    }
+});
+
  module.exports = server;
