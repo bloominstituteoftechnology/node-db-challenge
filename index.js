263ed34dedbdd014877138d7f7274976ea52a75c
CLN <cln@Sags-MacBook-Pro.local>
2018-08-03 13:26:56 -0400

    Finished actions CRUD, .

--- index.js
+++ index.js
@@ -62,7 +62,60 @@ server.delete('/projects/:id', (req, res) => {//delete project
       });
       
   });
+  server.get('/actions', (req, res) => {//get all actions
+    db('actions').then(action => {
+        res.status(200).json(action)
+    }).catch(err => res.status(500).json(err))
+});
+server.get('/actions/:id', (req, res) => {//get actions by id
+    const id = req.params.id;
+    db('actions')
+    .where('id', '=', id)
+    .then(action => {
+        res.status(200).json(action)
+    }).catch(err => res.status(500).json(err))
+});
+server.post('/actions', (req, res) => {//add action
+    const action = req.body;
 
+    db.insert(action).into('actions').then(actions =>{
+        const id = actions[0];
+        res.status(201).json({id, ...action})
+    }).catch(err => res.status(500).json(err))
+})
+server.delete('/actions/:id', (req, res) => {//delete action
+    const id = req.params.id;
+    
+    db('actions')
+    .where('id', '=', id)
+    .del()
+      .then(action => {
+        if (action === 0) {
+          res.status(404)
+          .json({ error: "The actions with the specified ID does not exist." })
+        }
+        res.status(200).json(action)
+      }).catch(error => {
+        res.status(500)
+          .json({ error: "error 2." })
+      });
+  })
+  server.put('/actions/:id', (req, res) => {// update actions
+    const changes = req.body;
+    const id = req.params.id;
+  
+    db('actions')
+      .where('id', '=', id) // or .where({ id: id })
+      .update(changes)
+      .then(count => {
+        // count === number of records updated
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+      
+  });
 const port = 6500;
 server.listen(port, function () {
     console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
