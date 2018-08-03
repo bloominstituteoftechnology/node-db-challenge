d273808eb08492da1b75d8f7b68124dfe96f28dd
-tristan- <tristan.linehan@gmail.com>
2018-08-03 10:11:59 -0700

    projects endpoints GET & POST created

---
+++ data/dataShape.js
@@ -0,0 +1,16 @@
+//PROJECTS
+{
+    id: 1, // AUTO-SET
+    name: 'text', // REQUIRED & UNIQUE
+    description: 'text', 
+    notes: 'text',
+    completed: false // DEFAULT SET TO FALSE
+}
+//ACTIONS
+{
+    id: 1, // AUTO-SET
+    project_id: 1, // REQUIRED & FK
+    description: 'text', // REQUIRED
+    notes: 'text',
+    completed: false // DEFAULT SET TO FALSE
+}
--- server.js
+++ server.js
@@ -13,6 +13,33 @@ server.get('/', (req, res) => {
     res.send('Rock-n-Roll!!');
 });
 
+//POST CREATE NEW PROJECT
+server.post('/api/projects', (req, res) => {
+    const { name, description, completed } = req.body;
+    const newProject = { name, description, completed };
+    if (!name) {
+        res.status(400).json({'ERROR!!! dataShape': {
+            name: 'REQUIRED & UNIQUE',
+            description: 'string', 
+            notes: 'string',
+            completed: 'BOOLEAN, DEFAULT SET TO FALSE BY SERVER'
+            } 
+        });
+        return; 
+    };
+    db('projects').insert(newProject).then(response => {
+        res.status(201).json(response)
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({error: 'Did not save to server!', err});
+    });
+});
+//GET PROJECTS
+server.get('/api/projects', (req, res) => {
+    db('projects').then(projects => {
+        res.status(200).json({projects});
+    });
+});
 
 
 server.listen(port, () => 
