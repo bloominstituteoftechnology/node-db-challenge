0f959e2ad9373a42eac7f84edaba69170a4859fb
Allen Hai <allenhai03@gmail.com>
2018-08-03 12:50:40 -0500

    add proper foreign key methods for projectId field in actions migration

--- data/migrations/20180803104251_create_actions_table.js
+++ data/migrations/20180803104251_create_actions_table.js
@@ -1,13 +1,19 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('actions', t => {
     t.increments('id')
-    t.integer('projectId').notNullable()
+    
+    t
+      .integer('projectId')
+      .notNullable()
+      .references('id')
+      .inTable('projects')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+
     t.text('description').notNullable()
     t.text('notes').nullable()
     t.boolean('completed').notNullable().defaultTo(false)
     t.timestamp('createdAt').notNullable().defaultTo(knex.fn.now())
-    t.onUpdate('CASCADE')
-    t.onDelete('CASCADE')
   })
 };
 
