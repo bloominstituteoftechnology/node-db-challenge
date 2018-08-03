750192ec2338d77a49397970d95fc6aa17037a16
DirupT <iamcooled@gmail.com>
2018-08-03 11:31:46 -0500

    added foreign key to action table / added project id in seeds

--- data/migrations/20180803102949_action.js
+++ data/migrations/20180803102949_action.js
@@ -1,6 +1,7 @@
 exports.up = function (knex, Promise) {
     return knex.schema.createTable('action', table => {
         table.increments();
+        table.integer('project_id').unsigned().notNullable().references('id').inTable('project').onUpdate('CASCADE').onDelete('CASCADE');
         table.text('description').notNullable();
         table.text('notes').notNullable();
         table.boolean('completed').defaultTo(0);
--- data/seeds/01-action.js
+++ data/seeds/01-action.js
@@ -5,9 +5,9 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('action').insert([
-        { description: 'Action 1', notes: 'Notes - Action 1', completed: true },
-        { description: 'Action 2', notes: 'Notes - Action 2', completed: false },
-        { description: 'Action 3', notes: 'Notes - Action 3', completed: true },
+        { project_id: 1, description: 'Action 1', notes: 'Notes - Action 1', completed: true },
+        { project_id: 2, description: 'Action 2', notes: 'Notes - Action 2', completed: false },
+        { project_id: 3, description: 'Action 3', notes: 'Notes - Action 3', completed: true },
       ]);
     });
 };
