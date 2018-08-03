5025baf0974585bc93c15c6b1656d6517fcc075a
Randy Calderon <randycweb@gmail.com>
2018-08-03 12:20:54 -0700

    Added post put for projects

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -28,5 +28,23 @@ router.get('/:id/actions', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    const { project, description, completed} = req.body
+    db('projects').insert({project, description, completed}).then(project => {
+        res.status(200).json(err)
+    }).catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const { project, description, completed} = req.body
+    db('projects').where({id: id}.insert({ project, description, completed}).then(updatePost => {
+        res.status(200).json(updatePost)
+    }).catch(err => {
+        res.status(500).json(err)
+    })
+})
 
 module.exports = router;
