a3557c0f648836bf6be264c6e83ae6f6fdd7a522
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-08-10 11:34:16 -0600

    CRUD endpoints for projects

--- index.js
+++ index.js
@@ -20,6 +20,58 @@ server.get('/projects', (req, res) => {
     })
 });
 
+
+server.post('/projects', (req, res) => {
+    const project = req.body;
+  
+    db
+    .insert(project)
+    .into('projects')
+    .then(ids => {
+      const id = ids[0];
+       res.status(201).json({ id, ...project });
+    })
+    .catch(err => res.status(500).json(err));
+  });
+  
+  server.put('/projects/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+  
+    db('projects')
+    .where({ id })
+    .update(changes)
+    .then(count => {
+      if (count) {
+  
+        db('projects')
+        .select('name')
+        .where({ id })
+        .then(project => {
+          res.status(200).json(project);
+        })
+        .catch(err => {
+          res.status(500).json(err);
+        })
+      } else {
+        res.status(404).json({ message: 'The project was not found'});
+      }
+   })
+    .catch(err => res.status(500).json(err))
+  });
+  
+  server.delete('/projects/:id', (req,res) => {
+    const { id } = req.params;
+  
+    db('projects')
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => res.status(500).json(err))
+  });
+
 server.listen(PORT, () => {
     console.log('Server up and running on ${PORT}');
 })
