eed17701ea92caf27509798be42a2c26324f1ccd
Daniel Chamorro <dan@dcitllc.com>
2018-08-10 12:56:32 -0400

    Added POST to actions

--- routes/actionsRoutes.js
+++ routes/actionsRoutes.js
@@ -10,4 +10,30 @@ router.get("/", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+//* GET with id
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("actions")
+    .select() //! <------ Knex SELECT
+    .where("id", id) //! <----- WHERE id =
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+//* POST to Actions
+router.post("/", (req, res) => {
+  const action = req.body;
+
+  db.insert(action)
+    .into("actions")
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ id, ...action });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
 module.exports = router;
