961e5fe18ff5fc383804fa33cf86f3b9257e1a76
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-10 12:38:21 -0700

    Fix typo (bug) in actions_table migration

--- migrations/20180810100106_actions_table.js
+++ migrations/20180810100106_actions_table.js
@@ -5,7 +5,7 @@ exports.up = function(knex, Promise) {
     action.string('description')
     action.string('notes')
     action.boolean('completed').defaultTo(false)
-    action.integer('project_id').references('id').inTable('project')
+    action.integer('project_id').references('id').inTable('projects')
   });
 };
 
