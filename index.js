18759afbb1c3110d390a3c49b13dc06474b5653a
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-08-10 09:25:58 -0700

    update and delete routes added 🖤

--- index.js
+++ index.js
@@ -40,6 +40,29 @@ server.post('/projects', (req, res) => {
     });
 });
 
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const project = req.body;
+  db('projects')
+    .where({ id })
+    .update(project)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  db('projects')
+    .where({ id })
+    .del()
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
 const port = 5000;
 server.listen(port, () => {
   console.log(`server on http://localhost:${port}`);
