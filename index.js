041bce2cf28a58daf4cb279df70eda6754e15f5c
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-10 11:03:06 -0700

    Implement GET single project and GET actions from a project endpoints

--- index.js
+++ index.js
@@ -23,6 +23,28 @@ server.post('/projects', (req, res) => {
 server.get('/projects', (req, res) => {
   db('projects')
     .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+})
+
+server.get('/projects/:id', (req, res) => {
+  const id = req.params.id;
+  db('projects').where('id', id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+})
+
+server.get('/projects/:id/actions', (req, res) => {
+  const id = req.params.id;
+  db('actions').where('userId', id)
+    .then(response => {
       res.status(201).json(response);
     })
     .catch(err => {
