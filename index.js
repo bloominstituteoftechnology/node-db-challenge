4b7d60db54eac77c1d4ceb07b9fe2d4c95d8cda9
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-10 10:49:31 -0700

    Implement POST project and GET all projects endpoints

--- index.js
+++ index.js
@@ -7,6 +7,29 @@ server.use(express.json())
 
 const PORT = 8000;
 
+//==========Projects==========
+
+server.post('/projects', (req, res) => {
+  const project = req.body;
+  db.insert(project).into('projects')
+    .then(response => {
+      res.status(201).json(response);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+})
+
+server.get('/projects', (req, res) => {
+  db('projects')
+    .then(response => {
+      res.status(201).json(response);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+})
+
 server.listen(PORT, () => {
   console.log(`Up and running on ${PORT}`)
 })
