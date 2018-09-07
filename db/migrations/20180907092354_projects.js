71d72e17c51d09d9df3571ef5f9601f6e6769730
szincone <sawyerzincone@gmail.com>
2018-09-07 09:33:09 -0600

    project and actions schemas finished

--- db/migrations/20180907092354_projects.js
+++ db/migrations/20180907092354_projects.js
@@ -1,8 +1,21 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("projects", function(table) {
+    table.increments("project_id");
+
+    table
+      .string("project_name", 50)
+      .notNullable()
+      .unique("uq_project_name");
+
+    table
+      .string("project_description", 50)
+      .notNullable()
+      .unique("uq_project_description");
+
+    table.boolean("project_completed").notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("projects");
 };
--- db/migrations/20180907092409_actions.js
+++ db/migrations/20180907092409_actions.js
@@ -1,8 +1,29 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("actions", function(table) {
+    table.increments("action_id");
+
+    table
+      .string("action_description", 50)
+      .notNullable()
+      .unique("uq_action_description");
+
+    table
+      .string("action_notes", 120)
+      .notNullable()
+      .unique("uq_action_notes");
+
+    table.boolean("action_completed").notNullable();
+
+    // foreign key
+    table
+      .integer("project_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("projects");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("actions");
 };
