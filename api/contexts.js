768062d91bb106dfa853bfbae4c2e4f4cadf4792
johnoro <officialjohnorourke@gmail.com>
2018-09-07 13:07:49 -0400

    Added endpoints for the new contexts table.

--- api/contexts.js
+++ api/contexts.js
@@ -2,30 +2,24 @@ const router = require('express').Router();
 const db = require('knex')(require('../knexfile').development);
 
 router.get('/', (req, res) => {
-  db('actions')
-    .then(actions => res.status(200).json(actions))
+  db('contexts')
+    .then(contexts => res.status(200).json(contexts))
     .catch(err => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  db('actions as a')
-    .where({ 'a.id': id })
-    .then(action => {
-      db('contexts-to-actions')
-        .where({ 'action_id': id })
-        .select('c.id', 'c.description')
-        .join('contexts as c', { 'c.id': 'context_id' })
-        .then(contexts => {
-          res.status(200).json(Object.assign(action[0], { contexts }))
-        })
-        .catch(err => res.status(500).json(err));
+  db('contexts')
+    .where({ id })
+    .then(context => {
+      if (!(context.length === 0)) res.status(200).json(context[0]);
+      else res.status(404).json({ error: 'The context with the specified ID wasn\'t found.' })
     })
     .catch(err => res.status(500).json(err));
 });
 
 router.post('/', (req, res) => {
-  db('actions')
+  db('contexts')
     .insert(req.body)
     .then(id => res.status(201).json(id))
     .catch(err => res.status(500).json(err));
@@ -33,24 +27,24 @@ router.post('/', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  db('actions')
+  db('contexts')
     .where({ id })
     .del()
     .then(count => {
       if (count) res.status(200).json(count);
-      else res.status(404).json({ error: 'The action with the specified ID wasn\'t found.' });
+      else res.status(404).json({ error: 'The context with the specified ID wasn\'t found.' });
     })
     .catch(err => res.status(500).json(err));
 });
 
 router.put('/:id', (req, res) => {
   const { id } = req.params;
-  db('actions')
+  db('contexts')
     .where({ id })
     .update(req.body)
     .then(count => {
       if (count) res.status(200).json(count);
-      else res.status(404).json({ error: 'The action with the specified ID wasn\'t found.' });
+      else res.status(404).json({ error: 'The context with the specified ID wasn\'t found.' });
     })
     .catch(err => res.status(500).json(err));
 });
--- api/index.js
+++ api/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use('/projects', require('./projects'));
 router.use('/actions', require('./actions'));
+router.use('/contexts', require('./contexts'));
 
 module.exports = router;
