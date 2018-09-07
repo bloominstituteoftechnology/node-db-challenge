22d669c64ddc4e1f026bf87488e2b5a1af0a5c23
dasgma <dasgrigoma@gmail.com>
2018-09-07 08:37:19 -0700

    Migrations done

--- db/migrations/20180907082328_project.js
+++ db/migrations/20180907082328_project.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('project', table => {
+    table.increments()
+    table.string('name', 128)
+         .notNullable()
+    table.text('description')
+         .notNullable()
+    table.boolean('completed')
+         .defaultTo(false) 
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('project');
 };
