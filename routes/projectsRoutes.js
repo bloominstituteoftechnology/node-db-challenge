68b77b96816069520b3108e4bfbabbad04de7b15
codejoncode <jonathanjholloway@gmail.com>
2018-09-07 11:09:12 -0500

    able to get all of the projects

--- routes/projectsRoutes.js
+++ routes/projectsRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express')
-
 const projectRouter = express.Router()
 
+const db = require('../db/dbConfig')
 
 
+projectRouter.get('/', (req, res) => {
+  db('projects')
+    .then(projects => {
+      res.status(200).json(projects)
+    })
+    .catch(error => {
+      res.status(500).json({error, message: error.message})
+    })
+})
 
 
 
