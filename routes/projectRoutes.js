c4c8cb9809c06eb906dcf51e0c16bde7ffd5ce65
francistse23 <wtse.ucsd@gmail.com>
2018-09-07 09:47:19 -0700

    PUT and DELETE methods for project implemented.

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -7,6 +7,7 @@ const db = knex(dbConfig.development);
 
 router.use(express.json());
 
+//projects
 router.get('/', async (req, res) => {
     try {
         const projects = await db('project')
@@ -26,7 +27,7 @@ router.post('/', async (req, res) => {
     } else {
         try {
             const project = await db.insert(newProject).into('project')
-            res.status(201).json( project );
+            res.status(201).json( project )
         }
         catch ( err ) {
             res.status(500).json( err.message );
@@ -34,5 +35,44 @@ router.post('/', async (req, res) => {
     };
 });
 
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const project = await db('project').where({ id });
+        res.status(200).json( project )
+    }
+    catch ( err ) {
+        res.status(500).json( err.message );
+    };
+});
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const updateProject = req.body;
+    if ( !updateProject.name || !updateProject.description ) {
+        res.status(400).json({
+            message: "Project name and description are required."
+        })
+    } else {
+        try {
+            const updated = await db('project').where({ id }).update(updateProject)
+            res.status(200).json( updated );
+        }
+        catch ( err ) {
+            res.status(500).json( err.message )
+        };
+    };
+});
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deleted = db('project').where({ id }).del();
+        res.status(200).json( deleted );
+    }
+    catch ( err ) {
+        res.status(500).json( err.message );
+    };
+});
 
 module.exports = router;
