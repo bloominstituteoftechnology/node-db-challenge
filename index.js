8b84803fe3e75cc44fa44dc72952afa05b4a59a9
Josue Peralta <jperal77@gmail.com>
2018-09-07 13:07:04 -0500

    added post & get endpoints to actions & projects

--- index.js
+++ index.js
@@ -15,4 +15,40 @@ server.get('/', (req, res) => {
     res.send('API Running...');
 });
 
+server.get('/api/projects', (req, res) => {
+    db('projects')
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(err => res.status(500).json(err));
+} );
+
+server.get('/api/actions', (req, res) => {
+    db('actions')
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(err => res.status(500).json(err));
+} );
+
+server.post('/api/projects',(req, res) => {
+    
+    db.insert(req.body).into('projects').then(response => {
+        res.status(201).json(response);
+    }).catch(err => {
+        console.error('Error', err);
+        res.status(500).json({message: "Error creating project"});
+    });
+});
+
+server.post('/api/actions',(req, res) => {
+    
+    db.insert(req.body).into('actions').then(response => {
+        res.status(201).json(response);
+    }).catch(err => {
+        console.error('Error', err);
+        res.status(500).json({message: "Error creating project"});
+    });
+});
+
 server.listen(8000);
