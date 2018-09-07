176903b6c51faccfeacf1dca68f3f98b46e03c02
LaikaFusion <newrad4@gmail.com>
2018-09-07 11:40:03 -0400

    Basic get endpoint works (actions and projects)

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dbhelpers = require("./dbhelpers/helpers");
+
+
+const server = express();
+
+server.get("/api/:whichtable", async (req, res) => {
+  let table = "projects";
+  if(req.params.whichtable ==="actions"){
+    table = "actions";
+  }
+  try {
+    const results = await dbhelpers.get(null,table);
+    res.status(200).json(results);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+server.use("/", (req, res) =>
+  res
+    .status(404)
+    .json({ errorMessage: "You probably want to use a different endpoint" })
+);
+
+const port = 3300;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
