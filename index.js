44592b5d183d5ea584cdc176c9571b99334bd24c
Carey <careycbaldwin@gmail.com>
2018-09-07 12:44:08 -0400

    wrote and tested all endpoints specific to actions

--- index.js
+++ index.js
@@ -54,35 +54,126 @@ server.get('/api/projects/:id', (req,res) => {
     .catch(err => res.status(500).json({message: "There was an error looking for the specified record"}));
 })
 
-    server.put('/api/projects/:id', (req,res) => {
-        const changes = req.body;
-        const id = req.params.id;
-        db('projects')
-            .where('id', '=', id)
-            .update(changes)
-            .then(count => { //tells the number of records updated
-            res.status(200).json(count);
-            })
-            .catch(err => {
-            res.status(500).json(err);
-            });
+//PUT Request
+server.put('/api/projects/:id', (req,res) => {
+    const changes = req.body;
+    const id = req.params.id;
+    db('projects')
+        .where('id', '=', id)
+        .update(changes)
+        .then(count => { //tells the number of records updated
+        res.status(200).json(count);
+        })
+        .catch(err => {
+        res.status(500).json(err);
         });
+    });
 
-
-    server.delete('/api/projects/:id', (req, res) => {
-        const id = req.params.id;
-        db('projects')
-            .where('id', '=', id)
-            .del()
-            .then(count => {
-            // count === number of records deleted
-            res.status(200).json(count);
-            })
-            .catch(err => {
-            res.status(500).json(err);
-            });
+//DELETE Request
+server.delete('/api/projects/:id', (req, res) => {
+    const id = req.params.id;
+    db('projects')
+        .where('id', '=', id)
+        .del()
+        .then(count => {
+        // count === number of records deleted
+        res.status(200).json(count);
+        })
+        .catch(err => {
+        res.status(500).json(err);
         });
+    });
+
+// - - - - - - - - Get Actions by Project - - - - -
+
+server.get('/api/projects/actions/:projectId', (req,res)=> {
+    const projectId = req.params.projectId;
+    db
+    .getUserPosts(projectId) //function defined in 'userDb.js'
+    .then(projectActions => {
+        if (projectActions === 0){
+            res.status(404).json({message: 'Unable to find specified record'});
+            return;
+        }
+        res.json(projectActions);
+    })
+    .catch(error => {
+        res.status(500).json({message: 'problem encountered in database'});
+        return;
+    });
+});
 
 // - - - - - - - - Actions Endpoints - - - - - - - - 
+
+//POST REQUEST
+server.post('/api/actions', (req, res) => {
+    const action = req.body;
+    // console.log(project);
+    db.insert(action)
+    .into('actions')
+    .then(id => {
+    res.status(201).json(id);
+    })
+    .catch(err => res.status(500).json(err));
+    });
+
+//GET ALL REQUEST
+server.get('/api/actions', (req, res) => {
+    db('actions')
+    // .select('name')
+    .then(actions => {
+    res.status(200).json(actions);
+    })
+    .catch(err => res.status(500).json(err));
+    });
+
+//GET Request for individual record
+server.get('/api/actions/:id', (req,res) => {
+    const id = req.params.id;
+    db('actions')
+    .where('id', '=', id)
+    .then(action => {
+    if (action.length == 0) {
+    res.status(404).json({message: "Cannot find corresponding record"})
+    }
+    res.status(200).json(action);
+    })
+    .catch(err => res.status(500).json({message: "There was an error looking for the specified record"}));
+})
+
+//PUT Request
+server.put('/api/actions/:id', (req,res) => {
+    const changes = req.body;
+    const id = req.params.id;
+    db('actions')
+        .where('id', '=', id)
+        .update(changes)
+        .then(count => { //tells the number of records updated
+        res.status(200).json(count);
+        })
+        .catch(err => {
+        res.status(500).json(err);
+        });
+    });
+
+//DELETE Request
+server.delete('/api/actions/:id', (req, res) => {
+    const id = req.params.id;
+    db('actions')
+        .where('id', '=', id)
+        .del()
+        .then(count => {
+        // count === number of records deleted
+        res.status(200).json(count);
+        })
+        .catch(err => {
+        res.status(500).json(err);
+        });
+    });
+
+
+
+
+
 
 server.listen(9000);
