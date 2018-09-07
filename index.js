ed3cc3b76069f98bd81dfada37c0ec5fb927e2b8
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-07 09:45:36 -0600

    Added all dependecies, server set up and hooked to db

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express'); 
+const knex = require('knex'); 
+const dbConfig = require('./knexfile'); 
+
+const db = knex(dbConfig.development); 
+
+const server = express(); 
+
+
+server.listen(6500, () => {console.log("Starting port at PORT 6500")}); 
+
