324b27b997db0be2860e05ff3e7fd6065882d1fa
codejoncode <jonathanjholloway@gmail.com>
2018-09-07 10:32:46 -0500

    set up the tables with the columns and relationship

--- db/migrations/20180907101407_projects_table.js
+++ db/migrations/20180907101407_projects_table.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('projects', function(tbl){
+    tbl.increments()
+    tbl
+      .string('name')
+      .notNullable()
+    tbl 
+      .string('description')
+    tbl
+      .boolean('completed')
+      .notNullable()
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('projects')
 };
--- db/migrations/20180907101429_actions_table.js
+++ db/migrations/20180907101429_actions_table.js
@@ -1,8 +1,24 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('actions', function(tbl){
+    tbl.increments()
+    tbl
+      .string('description')
+      .notNullable()
+    tbl
+      .string('notes')
+    tbl
+      .boolean('completed')
+      .notNullable()
+    tbl
+      .integer('projects_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .InTable('projects')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('actions')
 };
