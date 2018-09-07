314c0b27fbaa83fbb4a227b936865bc8b574cf65
William <kwontaewoongah@gmail.com>
2018-09-07 11:34:27 -0700

    finished actions

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ server.put('/api/projects/:id',(req,res)=>{
     db('projects')
         .where({'id':req.params.id})
         .update(updatedProject)
-        .then(count=>res.status(201).json(count))
+        .then(count=>res.status(200).json(count))
         .catch(err=>res.status(500).json(err)):
-        res.status(404).json({message: 'Fill out fields'})
+    res.status(404).json({message: 'Fill out fields'})
 })
 server.delete('/api/projects/:id',(req,res)=>{
     db('projects')
@@ -48,6 +48,49 @@ server.delete('/api/projects/:id',(req,res)=>{
         .del()
         .then(count=>res.status(200).json(count))
         .catch(err=>res.status(500).json(err));
+})
+server.get('/api/actions',(req,res)=>{
+    db('actions')
+        .then(response=>res.status(200).json(response))
+        .catch(err=>res.status(500).json(err));
+})
+server.get('/api/actions/:id',(req,res)=>{
+    db('actions')
+        .where({'id':req.params.id})
+        .then(response=>res.status(200).json(response))
+        .catch(err=>res.status(500).json(err))
+})
+server.post('/api/actions',(req,res)=>{
+    const action=req.body;
+    action.name &&
+    action.description &&
+    action.complete &&
+    action.project_id ?
+    db.insert(action)
+        .into('actions')
+        .then(count=>res.status(201).json(count))
+        .catch(err=>res.status(500).json(err)):
+    res.status(404).json({error:'Fill out fields'})
+})
+server.put('/api/actions/:id',(req,res)=>{
+    const updatedAction=req.body;
+    updatedAction.name &&
+    updatedAction.description &&
+    updatedAction.complete &&
+    updatedAction.project_id ?
+    db('actions')
+        .where({'id':req.params.id})
+        .update(updatedAction)
+        .then(count=>res.status(200).json(count))
+        .catch(err=>res.status(500).json(err)):
+    res.status(404).json({error:'Fill out fields'})
+})
+server.delete('/api/actions/:id',(req,res)=>{
+    db('actions')
+        .where({'id':req.params.id})
+        .del()
+        .then(count=>res.status(200).json(count))
+        .catch(err=>res.status(500).json(err))
 })
 const port=9000;
 server.listen(port,()=>console.log('Engines firing server starting new horizons venturing.'));
