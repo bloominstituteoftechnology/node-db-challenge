301150a846b90733aefdf6dcbee69a826746f75a
johnoro <officialjohnorourke@gmail.com>
2018-09-07 13:59:11 -0400

    Displaying projects on client-side.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, CardTitle, CardText, CardSubtitle } from 'reactstrap';
 
 class App extends Component {
+  state = { projects: [] };
+
+  componentDidMount() {
+    fetch('http://localhost:8000/api/projects')
+      .then(res => res.json())
+      .then(projects => this.setState({ projects }))
+      .catch(err => console.error(err));
+  }
+
+  getProject = id => { // to use w/ react router preferably
+    fetch(`http://localhost:8000/api/projects/${id}`)
+      .then(res => res.json())
+      .then(project => this.setState({ project }))
+      .catch(err => console.error(err));
+  };
+
   render() {
+    console.log(this.state.projects)
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Project Tracker</h1>
         </header>
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br />
+        <div className="projects">
+          {this.state.projects.map(project => {
+            const { name, description, completed } = project;
+            return (
+              <Card>
+                <CardTitle>{name}</CardTitle>
+                <CardText>{description}</CardText>
+                <CardSubtitle>
+                  {completed ? 'completed' : 'in progress'}
+                </CardSubtitle>
+              </Card>
+            );
+          })}
+        </div>
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const server = express();
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+server.use(require('cors')());
 server.use('/api', require('./api'));
 
 server.listen(8000, () => console.log('Server listening on port 8000.'));
