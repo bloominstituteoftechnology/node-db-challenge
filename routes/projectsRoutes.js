a14c5de180bf3c375e94c2bb754acd7f31fff1cf
Trevor <trevorfehrman@gmail.com>
2018-09-07 09:01:37 -0700

    Get and post implemented

--- routes/projectsRoutes.js
+++ routes/projectsRoutes.js
@@ -2,6 +2,23 @@ const express = require("express");
 const router = express.Router();
 const db = require("../dbConfig");
 
+router.post("/", (req, res) => {
+	const project = req.body;
+	if (!project.name || !project.description) {
+		return res
+			.status(400)
+			.json({ message: "Please provide a name and/or description" });
+	}
+	db.insert(project)
+		.into("projects")
+		.then(ids => {
+			res.status(201).json(ids);
+		})
+		.catch(err => {
+			res.status(500).json(err);
+		});
+});
+
 router.get("/", (req, res) => {
 	db("projects")
 		.then(projects => {
