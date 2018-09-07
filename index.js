7551e75d7dfd1b6baa5dda95f23d3c7c3405591d
Carey <careycbaldwin@gmail.com>
2018-09-07 12:28:10 -0400

    completed and tested remaining endpoints for projects data

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ server.get('/', (req, res) => {
 res.send('API Running...');
 });
 
+// - - - - - - - - Project Endpoints - - - - - - - - 
+
 //POST REQUEST
 server.post('/api/projects', (req, res) => {
     const project = req.body;
@@ -37,5 +39,50 @@ server.get('/api/projects', (req, res) => {
     })
     .catch(err => res.status(500).json(err));
     });
+
+//GET Request for individual record
+server.get('/api/projects/:id', (req,res) => {
+    const id = req.params.id;
+    db('projects')
+    .where('id', '=', id)
+    .then(project => {
+    if (project.length == 0) {
+    res.status(404).json({message: "Cannot find corresponding record "})
+    }
+    res.status(200).json(project);
+    })
+    .catch(err => res.status(500).json({message: "There was an error looking for the specified record"}));
+})
+
+    server.put('/api/projects/:id', (req,res) => {
+        const changes = req.body;
+        const id = req.params.id;
+        db('projects')
+            .where('id', '=', id)
+            .update(changes)
+            .then(count => { //tells the number of records updated
+            res.status(200).json(count);
+            })
+            .catch(err => {
+            res.status(500).json(err);
+            });
+        });
+
+
+    server.delete('/api/projects/:id', (req, res) => {
+        const id = req.params.id;
+        db('projects')
+            .where('id', '=', id)
+            .del()
+            .then(count => {
+            // count === number of records deleted
+            res.status(200).json(count);
+            })
+            .catch(err => {
+            res.status(500).json(err);
+            });
+        });
+
+// - - - - - - - - Actions Endpoints - - - - - - - - 
 
 server.listen(9000);
