c20bc5a89b0d1cf97e4852fc94d00226b6c20eb9
szincone <sawyerzincone@gmail.com>
2018-09-07 09:38:08 -0600

    added seeding data

--- db/seeds/01-projects.js
+++ db/seeds/01-projects.js
@@ -1,13 +1,28 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return (
+    knex("projects")
+      // .del()
+      .truncate()
+      .then(function() {
+        // Inserts seed entries
+        return knex("projects").insert([
+          {
+            project_name: "Shoot Baskets",
+            project_description: "Shoot baskets at the gym.",
+            project_completed: false,
+          },
+          {
+            project_name: "Cut Grass",
+            project_description: "Mow the lawn.",
+            project_completed: false,
+          },
+          {
+            project_name: "Play Fortnite",
+            project_description: "Get a dub in Fortnite.",
+            project_completed: true,
+          },
+        ]);
+      })
+  );
 };
--- db/seeds/02-actions.js
+++ db/seeds/02-actions.js
@@ -1,13 +1,37 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return (
+    knex("actions")
+      // .del()
+      .truncate()
+      .then(function() {
+        // Inserts seed entries
+        return knex("actions").insert([
+          {
+            action_description: "Take 100 3's.",
+            action_notes: "Make sure to shoot from all over the court",
+            action_completed: false,
+            project_id: 1,
+          },
+          {
+            action_description: "Take 100 2's.",
+            action_notes: "Layups and floaters",
+            action_completed: false,
+            project_id: 1,
+          },
+          {
+            action_description: "Add Gas.",
+            action_notes: "Add gas to lawnmower",
+            action_completed: false,
+            project_id: 2,
+          },
+          {
+            action_description: "Hide.",
+            action_notes: "Hide in bush.",
+            action_completed: false,
+            project_id: 3,
+          },
+        ]);
+      })
+  );
 };
