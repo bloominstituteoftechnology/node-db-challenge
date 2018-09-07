4e6f5a4b82d4688e55234ee5b24fa5d3f05290a7
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-09-07 10:49:23 -0500

    Working on MVP
    I have moved on to the index.js, finshed my migrations

--- db /migrations/20180907101819_projects.js
+++ db /migrations/20180907101819_projects.js
@@ -3,11 +3,14 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("projects", function(table){
       table.increments(); 
       table
-        .string()
-        .string()
+        .string("name", 128)
+        .notNullable()
+        .string("description", 360)
+        .boolean('completed').defaultTo(false)
+        .unique("name")
   })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("projects")
 };
