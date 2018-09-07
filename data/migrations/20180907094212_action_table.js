24b8caf4e9837c2acdf8758d8df8c6205c0dd4f7
David Pok <david.pok@gmail.com>
2018-09-07 09:55:05 -0700

    implements migration tables

--- data/migrations/20180907094212_action_table.js
+++ data/migrations/20180907094212_action_table.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("actions", tbl => {
+      tbl.increments("id");
+      tbl.string("description");
+      tbl.string("notes");
+      tbl.boolean("completed").defaultTo(false);
+      tbl.integer("project_id")
+      .notNullable()
+      .references("id")
+      .inTable("projects");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("actions");
 };
