b984c47634987d1d174c8382a24b3e68e91999dd
Lauren <llwor94@gmail.com>
2018-09-06 21:35:36 -0700

    actions migration

--- data/migrations/20180906211044_actions.js
+++ data/migrations/20180906211044_actions.js
@@ -1,8 +1,17 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('actions', table => {
+    table.increments();
+    table.string('description').notNullable();
+    table.text('notes').notNullable();
+    table
+      .integer('project_id')
+      .notNullable()
+      .references('id')
+      .inTable('projects');
+    table.boolean('completed').defaultTo(false);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('actions');
 };
