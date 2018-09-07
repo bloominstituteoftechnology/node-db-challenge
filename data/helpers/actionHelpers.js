95b804c9b98eb3429a923ad1653ee67282c13683
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-09-07 13:47:44 -0400

    updated getting action by id to return contexts

--- data/helpers/actionHelpers.js
+++ data/helpers/actionHelpers.js
@@ -6,9 +6,26 @@ module.exports = {
 
     if(id) {
       query.where({ id });
+
+      let promises = [query, this.getContexts(id)];
+
+      return Promise.all(promises).then(function(results) {
+        let [actions, contexts] = results;
+        let action = actions[0];
+        action.contexts = contexts;
+
+        return action;
+      });
     }
 
     return query;
+  },
+  getContexts: function(actionId) {
+    return db('contexts')
+      .select('contexts.id as id', 'contexts.context as context')
+      .join('contexts_to_actions as ca', 'ca.context_id', 'contexts.id')
+      .orderBy('contexts.id')
+      .where('ca.action_id', actionId);
   },
   insert: function(action) {
     return db('actions')
