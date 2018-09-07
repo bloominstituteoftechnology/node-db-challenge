25acbd957fd4d830547a7eb697f4a5343d5cb078
Sean <sean47900@gmail.com>
2018-09-07 13:02:10 -0500

    actions endpoints done

--- index.js
+++ index.js
@@ -90,6 +90,80 @@ server.put('/api/projects/:id', (req, res) => {
 });
 
 
+// actions
+
+
+server.post('/api/actions', (req, res) => {
+    const action = req.body;
+    const { description } = req.body;
+    if (!description) {
+        res.status(400).json({ errorMessage: 'Description required' });
+        return;
+    }
+  
+    db.insert(action)
+      .into('actions')
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => res.status(500).json(err));
+});
+
+server.get('/api/actions', (req, res) => {
+    db('actions')
+      .then(actions => {
+        res.status(200).json(actions);
+      })
+      .catch(err => res.status(500).json(err));
+});
+
+server.get('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+  
+    db('actions')
+      .where('id', '=', id)
+      .then(action => {
+          if (!action) {
+              res.status(404).json({ message: 'Action with that ID does not exist.' });
+              return;
+          }
+          res.status(200).json(action);
+      })
+      .catch(err => {
+          console.error('error', err);
+          res.status(500).json({ error: 'Action information not available.'})
+    })
+});
+
+server.put('/api/actions/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+
+    db('actions')
+        .where('id', '=', id) 
+        .update(changes)
+        .then(count => {
+        res.status(200).json(count);
+        })
+        .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+server.delete('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+  
+    db('actions')
+      .where({ id }) 
+      .del()
+      .then(count => {
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+});
+
 const port = 2000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
