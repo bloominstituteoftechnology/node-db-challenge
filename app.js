b5d89d765e4d0613dcd6ca9bd6d45bd11bcb655e
kamalnrf <kamalnrf@hotmail.com>
2018-09-07 21:51:25 +0530

    Add data model API

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const dbModel = require('./db/dbHelper/model')
+
+// dbModel.get(`actions`)
+//     .then(data => {
+//         console.log(`actions Data`)
+//         console.log(data)
+//     })
+
+// dbModel.addAction({
+//     name: 'Random action',
+//     description: 'No description',
+//     flag: 1,
+//     notes: 'No notes',
+//     project_id: 1
+// }).then (data => {
+//     console.log(`Added`)
+//     console.log(data)
+// })
+
+dbModel.getProject(1)
+    .then(data => {
+        console.log(`project data`)
+        console.log(data)
+    })
+
+/*
+    projects.name as project_name,
+		projects.description as projects_description,
+		projects.flag as projects_flag,
+		actions.name as actions_name,
+		actions.description as actions_description,
+		actions.notes as action_notes,
+		actions.flag as actions_flag
+*/
---
+++ db/dbHelper/model.js
@@ -0,0 +1,89 @@
+"use strict";
+
+const db = require("../dbConfig.js");
+
+module.exports = {
+  async get(tableName) {
+    return await db(tableName).select();
+  },
+
+  async getProject(id) {
+    //   return await db(`actions`)
+    //     .select(`projects.name as project_name`,
+    //             `projects.description as projects_description`,
+    //             `projects.flag as projects_flag`,
+    //             `actions.name as actions_name`,
+    //             `actions.description as actions_description`,
+    //             `actions.notes as action_notes`,
+    //             `actions.flag as actions_flag`)
+    //     .innerJoin(`projects`, `projects.id`, `actions.id`)
+    //     .where(`projects.id`, id)
+    const project = await db(`actions`).where({
+        id: id
+    }).select('id', 'name', 'description', 'flag as completed')
+    const actions = await db(`actions`)
+      .where({
+        project_id: id
+      })
+      .select();
+
+    return {
+        id: project.id,
+        name: project.name,
+        description: project.description,
+        completed: project.completed,
+        actions: actions
+    }
+  },
+
+  async addProject(data) {
+    return await db(`projects`).insert({
+      name: data.name,
+      description: data.description,
+      falg: data.flag
+    });
+  },
+
+  async addAction(data) {
+    return await db(`actions`).insert({
+      name: data.name,
+      description: data.description,
+      flag: data.flag,
+      notes: data.notes,
+      project_id: data.project_id
+    });
+  },
+
+  async delete(tableName, id) {
+    return await db(tableName)
+      .where({
+        id: id
+      })
+      .del();
+  },
+
+  async updateProject(data) {
+    return await db(`projects`)
+      .where({
+        id: data.id
+      })
+      .update({
+        name: data.name,
+        description: data.description,
+        falg: data.flag
+      });
+  },
+
+  async updateAction(data) {
+    return await db(`actions`)
+      .where({
+        id: data.id
+      })
+      .update({
+        name: data.name,
+        description: data.description,
+        falg: data.flag,
+        notes: data.notes
+      });
+  }
+};
