e5c8cdb52d85994b5f43dbdac6dc975f6d674e9a
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-07 11:21:26 -0500

    added CRUD for actions data

--- index.js
+++ index.js
@@ -98,6 +98,87 @@ server.put('/api/projects/:id', (req, res) => {
 });
 
 
+// action endpoints -----------------------------------------
+// add an action
+server.post('/api/actions', (req, res) => {
+    const action = req.body;
+    const { description } = req.body;
+    if (!description) {
+        res.status(400).json({ errorMessage: 'The action description is required, please enter the name and try again.' });
+        return;
+    }
+  
+    db.insert(action)
+      .into('actions')
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => res.status(500).json(err));
+});
+
+// get all actions
+server.get('/api/actions', (req, res) => {
+    db('actions')
+      .then(actions => {
+        res.status(200).json(actions);
+      })
+      .catch(err => res.status(500).json(err));
+});
+
+// get a specific action
+server.get('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+  
+    db('actions')
+      .where('id', '=', id)
+      .then(action => {
+          if (!action) {
+              res.status(404).json({ message: 'The action with the specified ID does not exist.' });
+              return;
+          }
+          res.status(200).json(action);
+      })
+      .catch(err => {
+          console.error('error', err);
+          res.status(500).json({ error: 'The action information could not be retrieved.'})
+    })
+});
+
+// update an action's info
+server.put('/api/actions/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+
+    db('actions')
+        .where('id', '=', id) // or .where({ id: id })
+        .update(changes)
+        .then(count => {
+        // count === number of records updated
+        res.status(200).json(count);
+        })
+        .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+// delete an action
+server.delete('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+  
+    db('actions')
+      .where({ id }) // or .where(id, '=', id)
+      .del()
+      .then(count => {
+        // count === number of records deleted
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+});
+
+
+
 const port = 3100;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
