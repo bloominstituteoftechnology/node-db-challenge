deeb0c66544698579af42f32ab4844353ca4f181
vanceleon <vanceleon44@gmail.com>
2018-09-14 17:05:29 -0400

    actions is posting but can't confirm get request yet

--- index.js
+++ index.js
@@ -18,23 +18,10 @@ server.post("/projects", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
-
-//post for Actions
-server.post("/projects/:id/actions", (req, res) => {
-  const actions = req.body;
-  db("actions")
-    .insert(actions)
-    .then(ids => {
-      res.status(201).json(ids);
-    })
-    .catch(err => res.status(500).json(err));
-});
-
-
 //get all projects
 server.get("/projects", (req, res) => {
   db("projects")
-    .then(projects => {
+  .then(projects => {
       res.status(200).json(projects);
     })
     .catch(err => res.status(500).json(err));
@@ -57,6 +44,52 @@ server.get("/projects/:id", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+//put for projects
+server.put("/projects/:id", (req, res) => {
+    const changes = req.body;
+    const id = req.params.id;
+  
+    db("projects")
+      .where("id", "=", id)
+      .update(changes)
+      .then(count => {
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json({
+          err
+        });
+      });
+  });
+
+//Delete for Projects
+server.delete("/projects/:id", (req, res) => {
+    const id = req.params.id;
+    db("projects")
+    .where("id", "=", id)
+    .del(id)
+      .then(projects => {
+        res.status(200).json(`Deleted Project with id: ${id}`);
+      })
+      .catch(err => res.status(500).json(err));
+  });
+  
+
+
+//post for Actions
+server.post("/projects/:id/actions", (req, res) => {
+  const actions = req.body;
+  db("actions")
+    .insert(actions)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+
+
+
 //get actions for each project
 server.get("/projects/:id/actions", (req, res) => {
   db("actions")
@@ -68,39 +101,12 @@ server.get("/projects/:id/actions", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
-//put for projects
-server.put("/projects/:id", (req, res) => {
-  const changes = req.body;
-  const id = req.params.id;
-
-  db("projects")
-    .where("id", "=", id)
-    .update(changes)
-    .then(count => {
-      res.status(200).json(count);
-    })
-    .catch(err => {
-      res.status(500).json({
-        err
-      });
-    });
-});
 
 
 
 
 
-//Delete for Projects
-server.delete("/projects/:id", (req, res) => {
-  const id = req.params.id;
-  db("projects")
-  .where("id", "=", id)
-  .del(id)
-    .then(projects => {
-      res.status(200).json(`Deleted Project with id: ${id}`);
-    })
-    .catch(err => res.status(500).json(err));
-});
+
 
 //Delete for actions
 server.delete("/projects/:id/actions", (req, res) => {
