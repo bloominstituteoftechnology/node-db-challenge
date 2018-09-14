46fbdd834c400fed15e2cd677e639ed28442e6c2
vanceleon <vanceleon44@gmail.com>
2018-09-14 13:11:29 -0400

    latest backup

--- db/migrations/20180914115140_projects.js
+++ db/migrations/20180914115140_projects.js
@@ -15,5 +15,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable("projects");
+  return knex.schema.dropTableIfExists("projects");
 };
