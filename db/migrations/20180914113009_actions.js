cf1c224ecbdc1bbd468e8df1af3563b7def5458d
bri79319 <bri79319@gmail.com>
2018-09-14 13:39:47 -0400

    trial and error approach

--- db/migrations/20180914113009_actions.js
+++ db/migrations/20180914113009_actions.js
@@ -4,15 +4,15 @@ exports.up = function(knex, Promise) {
     tbl
       .string("description", 128)
       .notNullable()
-      .unique("uq_descrip");
-    tbl.string("notes", 128).notNullable();
-    tbl.string("completed").boolean(false);
-    tbl
-      .integer("projects_id")
-      .unsigned()
-      .notNullable()
-      .reference("id")
-      .inTable("projects");
+      .unique("description");
+    // tbl.string("notes", 128).notNullable();
+    // tbl.string("completed").boolean(false);
+    // tbl
+    //   .integer("projects_id")
+    //   .unsigned()
+    //   .notNullable()
+    //   .reference("id")
+    //   .inTable("projects");
   });
 };
 
