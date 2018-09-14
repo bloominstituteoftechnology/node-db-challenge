ee54cfe8d304d357918d5860c40c950e007e7761
Synch3 <jonsimo@aol.com>
2018-09-14 18:00:36 -0400

    New migrations and new code

--- Review.md
+++ Review.md
@@ -3,7 +3,13 @@
 You can exercise your Google-Fu for this and any other _Sprint Challenge_ in the future.
 
 1.  Explain the difference between `RDBMS` and `SQL`.
+
+RBDMS stands for Relational DataBase Management System, and is the database software itself used to manage storage, querying, updating, data. Structed Query Language is the language that is used to access that data by the specific queries.
+
 1.  Why do tables need a `primary key`?
+
+Tables need a primary key to uniquely identify each
+
 1.  What is the name given to a table column that references the primary key
     on another table.
 1.  What do we need in order to have a _many to many_ relationship between two
--- index.js
+++ index.js
@@ -1,67 +1,92 @@
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
 
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex');
-
-const dbConfig = require('./knexfile');
+const dbConfig = require("./knexfile");
 const db = knex(dbConfig.development);
 
 const server = express();
 
-
-
-
 server.use(helmet());
 server.use(express.json());
-server.get('/', (req, res) => {
-  res.send('API fully operational')
+
+server.get("/", (req, res) => {
+  res.send("API fully operational");
 });
 
-
-server.get('/projects', (req, res) => {
-  db('projects').then(projects => {
-    res.status(200).json(projects);
-  }).catch(err => {
-    res.status(500).json(err)
-  })
+server.get("/projects", (req, res) => {
+  db("projects")
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-
-server.get('/actions', (req, res) => {
-  db('actions').then(actions => {
-    res.status(200).json(actions);
-  }).catch(err => {
-    res.status(500).json(err);
-  })
+server.get("/actions", (req, res) => {
+  db("actions")
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-
-server.post('/projects', (req, res) => {
-  db.insert(req.body).into('projects').then(ids => {
-    res.status(201).json(ids)
-  }).catch(err => {
-    res.status(500).json(err);
-  });
+server.post("/projects", (req, res) => {
+  db.insert(req.body)
+    .into("projects")
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-
-server.post('/actions', (req, res) => {
-  db.insert(req.body).into('actions').then(ids => {
-    res.status(201).json(ids);
-  }).catch(err => {
-    res.status(500).json(err);
-  });
+server.post("/actions", (req, res) => {
+  db.insert(req.body)
+    .into("actions")
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
+// server.get('/projects/:id', (req, res) => {
+//   const projActionView = db('projects').join('actions', {'projects.id': 'actions.project_id'});
+//   projActionView.select().where('project.id', parseInt(req.params.id)).then(project => {
+//     res.status(200).json(project);
+//   }).catch(err => {
+//     res.status(500).json(err);
+//   });
+// });
 
-server.get('/projects/:id', (req, res) => {
-  const projActionView = db('projects').join('actions', {'projects.id': 'actions.project_id'});
-  projActionView.select().where('project.id', parseInt(req.params.id)).then(project => {
-    res.status(200).json(project);
-  }).catch(err => {
-    res.status(500).json(err);
-  });
+server.get("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("projects")
+    .where({ id })
+    .then(project => {
+      if (project.length) {
+        db("actions")
+          .where({ project_id: id })
+          .then(actions => {
+            res.status(200).json({ project, actions });
+          })
+          .catch(err => {
+            res.status(500).json(err);
+          });
+      } else {
+        res.status(404).json({ msg: "not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
-
 
 server.listen(9000);
