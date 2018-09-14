76e276ad8a59a484a287e04a06604ff22165ba21
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-09-14 12:51:49 -0400

    fixed errors

--- seeds/actions.js
+++ seeds/actions.js
@@ -1,12 +1,12 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('actions').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 3, action_description:'create crud endpoints',project_id:3},
-        {id: 4, action_description:'create crud endpoints,migrations and seeds'}
+      return knex('actions').insert([
+        {id: 3, description:'create crud endpoints',project_id:3},
+        {id: 4, description:'create crud endpoints,migrations and seeds',project_id:4}
        
       ]);
     });
