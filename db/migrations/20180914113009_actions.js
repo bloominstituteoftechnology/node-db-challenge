f4ce43a6fb9c5e5510080541bb23b27c899ae925
bri79319 <bri79319@gmail.com>
2018-09-14 12:14:00 -0400

    set up for actions migration file

--- db/migrations/20180914113009_actions.js
+++ db/migrations/20180914113009_actions.js
@@ -1,8 +1,25 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("actions", function(tbl) {
+    tbl.increments();
+    tbl
+      .string("description", 128)
+      .notNullable()
+      .unique("uq_descrip");
+    tbl
+      .string("notes", 128)
+      .notNullable();
+    tbl
+      .string("completed")
+      .boolean();
+    tbl
+      .integer("projects_id")
+      .unsigned()
+      .notNullable()
+      .reference("id")
+      .inTable("projects");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("actions");
 };
