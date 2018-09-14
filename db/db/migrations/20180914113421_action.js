95e1878477c8293330ee742c3ce95132767511f1
Kairi <kairijb@gmail.com>
2018-09-14 11:50:04 -0500

    created action schema

--- db/db/migrations/20180914113421_action.js
+++ db/db/migrations/20180914113421_action.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable('action', function(tbl) {
+        tbl.increments();
   
+        tbl.string('name', 128).notNullable().unique('action_name');
+  
+        tbl.string('description',128).notNullable();
+  
+        tbl.boolean('complete').defaultTo(false)
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('action')
+
 };
