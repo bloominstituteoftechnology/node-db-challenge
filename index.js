708bbaec5141953bbdb073a5e75ce4758ac28aa2
Kairi <kairijb@gmail.com>
2018-09-14 12:11:18 -0500

    project crud operations created

--- index.js
+++ index.js
@@ -10,14 +10,55 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
- 
+
 server.get('/', (req, res) => {
-  res.send('Api Online')
+    res.send('Api Online')
 })
 
+// project
 
+server.get('/api/project', (req, res) => {
+    db('project')
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => res.status(500).json(err))
+})
+
+server.post('/api/project', (req, res) => {
+    db.insert(project)
+        .into('project')
+        .then(id => {
+            res.status(201).json(id)
+        })
+        .catch(err => res.status(500).json(err))
+})
+
+server.put('/api/project/:id', (req, res) => {
+    const { id } = req.params
+
+    db('project')
+        .where({ id }).update(req.body)
+        .then(count => {
+            res.status(200).json(count)
+        })
+        .catch(err => res.status(500).json(err))
+})
+
+server.delete('/api/project/id', (req, res) => {
+    const { id } = req.params;
+    db("project")
+        .where({ id })
+        .del()
+        .then(count => {
+            res.status(200).json(count);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+})
 
 const port = 3500;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+server.listen(port, function () {
+    console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
