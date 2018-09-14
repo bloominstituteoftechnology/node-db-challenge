2569c31f0d554469be5cdfaf6c302746e3246f5b
Erica Thompson <ericathompson.nc@gmail.com>
2018-09-14 08:24:58 -0700

    Project and Action DBs Created

---
+++ migrations/20180914082421_actions.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('actions', function(tbl){
+
+    tbl
+    .increments();
+
+    tbl
+    .string('name', 128)
+    .notNullable()
+
+
+
+});
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('actions');
+};
