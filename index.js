a6ad3abd4dfc42b0a3adb5ef5eb73e5c60cda345
vanceleon <vanceleon44@gmail.com>
2018-09-14 13:15:32 -0400

    with get function

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const server = express();
+const knex = require('knex');
+const dbConfig = require("./knexfile");
+
+const db = knex(dbConfig.development);
+
+server.use(express.json());
+
+
 
 server.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+server.get('/api/projects', (req, res) => {
+    db("projects")
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(err => res.status(500).json(err));
+});
 
 
 
