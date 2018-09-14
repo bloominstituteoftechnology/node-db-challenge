42bbe5f8defece5ebe3f26474f7f4cd9e33ef8e3
bri79319 <bri79319@gmail.com>
2018-09-14 12:06:43 -0400

    set up projects migration file

--- db/migrations/20180914112949_projects.js
+++ db/migrations/20180914112949_projects.js
@@ -1,8 +1,25 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("projects", function(tbl) {
+    tbl.increments();
+    tbl
+      .string("name", 128)
+      .notNullable()
+      .unique("name");
+    tbl
+      .string("description", 128)
+      .notNullable();
+    tbl
+      .string("completed")
+      .boolean();
+    tbl
+      .integer("projects_id")
+      .unsigned()
+      .notNullable()
+      .reference("id")
+      .inTable("projects");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("projects");
 };
