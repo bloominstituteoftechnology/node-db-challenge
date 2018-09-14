01093b35b868f8743c4db56073c3fc70f5413e8d
Fred <sohnf90@yahoo.com>
2018-09-14 08:43:22 -0700

    Start of project Added dependencies and server stuff

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+
+const server = express();
+
+const dbConfig = require('./knexfile');
+
+const db = knex(dbConfig.development);
+
+server.use(express.json());
+server.use(helmet());
+
+const port = 3300;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
