77e86e59abfc1e6f3c3f250972be255153133773
vanceleon <vanceleon44@gmail.com>
2018-09-14 13:55:22 -0400

    reconfig actions table

--- db/migrations/20180914115155_actions.js
+++ db/migrations/20180914115155_actions.js
@@ -1,11 +1,15 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("actions", tbl => {
     tbl.increments();
+    tbl
+      .integer("project_id")
+      .notNullable()
+      .references("id")
+      .inTable("projects");
+
     tbl.string("description", 128).notNullable();
     tbl.string("notes", 1000).notNullable();
-    tbl
-      .string("completed", 128)
-      .defaultTo(false);
+    tbl.string("completed", 128).defaultTo(false);
   });
 };
 
