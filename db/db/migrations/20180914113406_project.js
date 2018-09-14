657ebc84bd8067f3f795d4a13bc0146c51c1f141
Kairi <kairijb@gmail.com>
2018-09-14 11:43:08 -0500

    made schema for projects

---
+++ db/db/migrations/20180914113406_project.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('project', function(tbl) {
+      tbl.increments();
+
+      tbl.string('name', 128).notNullable().unique('project_name');
+
+      tbl.string('description',128).notNullable();
+
+      tbl.boolean('complete').defaultTo(false)
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('project')
+};
