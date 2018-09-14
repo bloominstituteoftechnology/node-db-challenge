88a48017ab9ac1d4a6a0b055121fbcd9831d1a65
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-09-14 11:19:54 -0400

    migrations are done + running

--- migrations/20180914105808_actions.js
+++ migrations/20180914105808_actions.js
@@ -1,8 +1,22 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('actions', tbl => {
+      tbl.increments();
+
+      tbl
+      .string('description')
+      .notNullable();
+
+      tbl
+      .integer('project_id')
+      .notNullable();
+
+      tbl
+      .boolean('completed')
+      .defaultTo(false);
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('actions');
 };
