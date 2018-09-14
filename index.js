c1eb1f7d3ae9cdcc108cea7df5fbb8086f7b3ab4
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-09-14 14:11:59 -0500

    project CRUD

--- index.js
+++ index.js
@@ -78,3 +78,64 @@ const port = 6600;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
+//project operations
+
+server.get('/api/projects', (req, res) => {
+  db('projects') 
+   .then(projects => res.status(200).json(projects))
+   .catch(err => res.status(500).json({ error: 'Projects not retrieved'}));
+});
+
+
+server.get('/api/projects/:id', (req, res) => {
+  const { id } =  req.params;
+  db('projects')
+   .WHERE(id)
+   .then(project => {
+       if(project) {
+           res.status(200).json(project);
+       } else {
+           res.status(404).json({error: 'The project with this ID is not found'});
+       }
+   })
+   .catch(error => res.status(500).json({ error: 'Cannot retrieve project information'}));
+});
+
+
+server.post('/',(req, res) => {
+  const { project, description } = req.body;
+
+  if (!projects && !description ) res.status(400).json({ error: 'Please provide a project and description' });
+  project
+    .insert({ project, description })
+    .then(project => res.status(201).json( project ))
+    .catch(err => res.status(500).json({ error: 'Project not saved' }));
+});
+
+server.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { edit } = req.body;
+
+  db('projects')
+    .WHERE(id)
+    .update( edit )
+    .then(project => res.status(200).json([project]))
+    .catch(err => res.status(500).json({ error: 'Project with that ID not found' }));
+});
+
+
+server.delete('/api/projects/:id', (req, res) => {
+  const { id } = req.params;
+  project
+    .WHERE(id)
+    .remove()
+    .then(project => {
+      if (project) {
+        res.status(200).json({message: 'Project succesfully deleted'})
+      } else {
+        res.status(404).json({ errMsg: `Project not found` });
+      }
+    })
+    .catch(err => res.status(500).json({ error: 'Project could not be deleted' }));
+});
