8925595e8fc5dd93d67422d793817fa3b142a996
bri79319 <bri79319@gmail.com>
2018-09-15 13:30:53 -0400

    now code works

--- index.js
+++ index.js
@@ -26,33 +26,45 @@ server.get("/api/project", (req, res) => {
     });
 });
 
-server.get("/api/project/:id", (req, res) => {
-  const id = req.params.id;
+server.get("/api/project/:id", async (req, res) => {
+  // const id = req.params.id;
 
-  db("projects")
-    .select()
-    .where({ id })
-    .first() // get result[0]
-    .then(projects => {
-      // a single object
-      // let returnedProject = projects;
-      console.log("ReturnProject ", projects);
-      db("actions")
-        .select()
-        .where({ project_id: id })
-        .then(actions => {
-          // let returnedActions = actions;
-          console.log("ReturnActions ", actions);
-          // returnedProject.actions = returnedActions
-          projects.actions = actions;
-          // console.log(returnedProject)
-          // res.send(200).json(returnedProject.actions = returnedActions);
-          res.status(200).json(projects);
-        });
-      // console.log(returnedProject)
-      // console.log("Final result ", returnedProject);
-    })
-    .catch(err => res.status(500).json(err));
+  // db("projects")
+  //   .select()
+  //   .where({ id })
+  //   .first() // get result[0]
+  //   .then(projects => {
+  //     // a single object
+  //     // let returnedProject = projects;
+  //     console.log("ReturnProject ", projects);
+  //     db("actions")
+  //       .select()
+  //       .where({ project_id: id })
+  //       .then(actions => {
+  //         // let returnedActions = actions;
+  //         console.log("ReturnActions ", actions);
+  //         // returnedProject.actions = returnedActions
+  //         projects.actions = actions;
+  //         // console.log(returnedProject)
+  //         // res.send(200).json(returnedProject.actions = returnedActions);
+  //         res.status(200).json(projects);
+  //       });
+  //     // console.log(returnedProject)
+  //     // console.log("Final result ", returnedProject);
+  //   })
+  //   .catch(err => res.status(500).json(err));
+  try {
+    const projectObject = await db('projects').where({ project_id: req.params.id });
+    const actionArray = await db('actions').where({ project_id: req.params.id });
+    console.log('projectObject:', projectObject);
+    console.log('action array:', actionArray);
+    projectObject[0].actions = actionArray;
+     res.status(200).json(projectObject);
+}
+catch (err) {
+    console.log(err);
+    res.status(500).send('oops');
+}
 });
 
 server.post("/api/project", (req, res) => {
@@ -126,20 +138,6 @@ server.get("/api/action/:id", (req, res) => {
       res.status(500).json({ Error: "Cannot retrieve with id" });
     });
 });
-
-// server.get("/api/action/:id", (req, res) => {
-//   const { id } = req.params;
-//   db("actions")
-//     // .select()
-//     .where({ action_id: id })
-//     .then(project => {
-//       res.status(200).json(project);
-//     })
-//     .catch(err => {
-//       console.log("Error: ", err);
-//       res.status(500).json({ Error: "Cannot retrieve with id" });
-//     });
-// });
 
 server.post("/api/action", (req, res) => {
   const projects = req.body;
