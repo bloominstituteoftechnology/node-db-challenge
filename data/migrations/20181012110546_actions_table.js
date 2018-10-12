d336900bae486e8323d0a43bf1f0a72ab05d6a16
Eva Li <evalee904@gmail.com>
2018-10-12 12:49:34 -0500

    mvp.

--- data/migrations/20181012110546_actions_table.js
+++ data/migrations/20181012110546_actions_table.js
@@ -2,6 +2,12 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('actions', function(tbl){
     tbl.increments();
+    tbl
+    .integer('project_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('projects');
     tbl.string('description', 400).notNullable();
     tbl.string('notes', 400);
     tbl.boolean('completed').defaultTo(0);
--- projects/projectRoutes.js
+++ projects/projectRoutes.js
@@ -20,4 +20,24 @@ router.route('/')
       .catch(err => errHelper(500, 'Error adding projects.', res))
   })
 
+router.route('/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    projects.getById(id)
+      .then(project => {
+        if(!project || project < 1) {
+          return errHelper(404, 'No records found.', res);
+        } else {
+          res.status(200).json(project)
+        }
+        actions.where('project_id', id)
+        .then(actions => {
+          projects[0].actions = actions;
+          res.status(200).json(actions)
+        }
+      })
+      .catch(err => errHelper(500, 'Error adding projects.', res))
+  })
+
+
 module.exports = router;
