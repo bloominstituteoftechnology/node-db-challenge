b2dc80259bb38256d1614e8756b3f2dc59d8738c
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-12 12:48:07 -0400

    set up router file, fixed an error where I did not migrate to latest so data was not saving. everything functioning correctly at the moment.

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
    development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/get_it_done.sqlite3'
+      filename: './data/data.sqlite3'
     },
     useNullAsDefault: true,
     migrations: {
---
+++ newRouter.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./models/project_models');
+
+// post to projects
+
+router.post('/', (req, res) => {
+  const { name, description, completed } = req.body;
+  const project = { name, description, completed };
+  db.addProject(project)
+    .then(complete => res.status(201).json(complete))
+    .catch(err => res.status(500).json(err));
+});
+
+
+// get projects
+
+router.get('/', (req, res) => {
+  db.getProjects()
+    .then(projects => res.status(200).json(projects))
+    .catch(err => res.status(500).json(err.message));
+});
+
+
+module.exports = router;
