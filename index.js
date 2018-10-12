e1dec7e3449470b4ab11478b7543788debf16a04
mparedes003 <michelle.g.paredes@gmail.com>
2018-10-12 09:02:12 -0700

    Initail POST for projects works.

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const logger = require('morgan');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development);
 
 const server = express();// creates the server
 
@@ -17,7 +22,59 @@ server.get('/', (req, res) => {
   res.send('<h1>Here we go!</h1>');
 });
 
+// ========================PROJECTS ENDPOINTS=========================
 
+// Add GET ROUTE HANDLER to access the projects
+server.get('/api/projects', (req, res) => {
+  db('projects')
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+//Add POST ROUTE HANDLER to add a project
+server.post('/api/projects', (req, res) => {
+  if(!req.body.name || !req.body.description) {
+        return res.status(400).send({ errorMessage: "Please provide name and a description for this project." });
+        }
+      else if(req.body.name.length > 128 && req.body.description ) {
+        return res.status(400).send({error: " User name must be less than 128 characters"})
+      }
+  const project = req.body;
+
+  db.insert(project)
+    .into('projects')
+    .then(ids => {
+      res.status(201).json(ids[0]);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+
+
+
+// server.post('/api/projects', (req, res) => {
+//   // Check that name and description is present. If not return error message.
+//   if(!req.body.name || !req.body.description) {
+//     return res.status(400).send({ errorMessage: "Please provide name and a description for this project." });
+//     }
+//   else if(req.body.name.length > 128 && req.body.description ) {
+//     return res.status(400).send({error: " User name must be less than 128 characters"})
+//   }
+//   const { name, description } = req.body;
+//   const newProject = { name, description };
+//   db
+//     .insert(newProject)
+//     .then(newProject => {
+//         console.log(newProject);
+//         res.status(201).json(newProject);
+//       })
+//     .catch(err => res.status(500).send({ error: "There was an error while saving a project to the database" }));
+
+//   });
 
 // Name port
 const port = 2000;
