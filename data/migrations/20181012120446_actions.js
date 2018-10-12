e5eeb8d71e32fda7cad8d6ac0ab3114b97b4679b
Andrew <ghobrial.andrew@gmail.com>
2018-10-12 12:17:49 -0400

    added some modifications to actions migration

--- data/migrations/20181012120446_actions.js
+++ data/migrations/20181012120446_actions.js
@@ -3,6 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("actions", function(tbl) {
     tbl.increments();
     tbl.string("description", 128).notNullable();
+    tbl.string("notes", 256);
+    tbl.boolean("completed");
     tbl
       .integer("project_id")
       .unsigned()
