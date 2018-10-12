dae1f295bf8021311bd74c05ce55778988e5c07c
Nicocchi <nicocchilover@gmail.com>
2018-10-12 11:34:00 -0400

    knex migrations schemas created

---
+++ data/migrations/20181012112716_project_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('project', tbl => {
+      tbl.increment();
+      tbl.string('name', 255).notNullable();
+      tbl.string('description', 255).notNullable();
+      tbl.boolean('completed').notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.DropTableIfExists('project');
+};
---
+++ data/migrations/20181012112948_action_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('action', tbl => {
+        tbl.increment();
+        tbl.integer('project_id').unsigned().notNullable().references('id').inTable('project');
+        tbl.string('description', 255).notNullable();
+        tbl.string('notes', 255);
+        tbl.boolean('completed').notNullable();
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.DropTableIfExists('action');
+};
