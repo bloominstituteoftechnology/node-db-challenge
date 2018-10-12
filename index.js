a5cc54242694357860dac3e761092eb96abfe31e
JeanetteFernandez <jeanfern555@gmail.com>
2018-10-12 10:33:06 -0500

    MVP - Added index.js skeleton with a GET testing endpoint.

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const port = 3333;
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+//Testing Server
+server.get('/', (req, res) => {
+    res.send('Am I Alive?????');
+});
+
+server.listen(port, () => {
+    console.log(`\n=== Listening on Port ${port} ===\n`)
+});
