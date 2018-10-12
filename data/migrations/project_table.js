1f123a41e1d260660a6bb47383cd0f75248b1916
Lucas Beemer <you@example.com>
2018-10-12 08:58:00 -0700

    migrations and some crud methods

---
+++ data/migrations/project_table.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("projects", function(projects) {
+      projects.increments();
+      projects.string("name", 128).notNullable();
+      projects.string("notes").notNullable();
+      projects.boolean("complete").defaultTo(false);
+    });
+  };
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("projects");
+  };
--- index.js
+++ index.js
@@ -9,9 +9,74 @@ const port = 9000;
 server.use(helmet());
 server.use(express.json());
 
+//======== MIDDLEWARE =========//
+function verifyProject(req, res, next) {
+    const id = req.body.project_id;
+    db("projects")
+      .where({ id: id })
+      .then(project => {
+        if (project.length === 0) {
+          res.status(404).json({ message: "The project needs an ID" });
+        } else next();
+      })
+      .catch(err => res.status(500).json(err));
+  }
+
+
+//========== ENDPOINTS ==========//
 server.get('/', (req, res) => {
 	res.send('<h2>Server is running.</h2>');
 })
+
+// GET PROJECTS
+server.get("/api/projects", (req, res) => {
+    db("projects")
+      .then(projects => {
+        res.status(200).json(projects);
+      })
+      .catch(err =>
+        res
+          .status(500)
+          .json({ error: "Uh oh! The project could not be retrieved." })
+      );
+  });
+
+  // POST
+  server.post("/api/projects", (req, res) => {
+    const project = req.body;
+    if (!project.name || !project.comments) {
+      res
+        .status(400)
+        .json({ error: "This project needs a name and comments!" });
+    } else
+      db.insert(project)
+        .into("projects")
+        .then(ids => {
+          res.status(201).json(ids);
+        })
+        .catch(err =>
+          res
+            .status(500)
+            .json({ error: "Uh oh! The project could not be saved." })
+        );
+  });
+
+  // GET ID
+  server.get('/api/projects/:id', (req, res) => {
+    const id = req.params.id;
+     db("projects")
+        .select()
+        .where("id", id)
+        .then(projects => {
+            if(projects){
+                res.status(200).json(projects);
+            }else{
+                res.status(404).json({message: "Uh oh! There is no project with this ID!"});
+            }
+  
+        })
+        .catch(err => res.status(500).json(err));
+  });
 
 
 server.listen(port, function() {
