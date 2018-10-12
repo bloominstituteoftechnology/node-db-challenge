470561cd9ef012a16bea80589c32d61ee4b049da
m-j-hacker <mjhacker@gmail.com>
2018-10-12 11:27:02 -0500

    Created post endpoint for projects

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const projects = require('./projects/projectsModel.js');
+
 const server = express();
 
 server.use(helmet());
@@ -10,6 +12,19 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send("It's allliiiiiiiiiiive!!!!");
+});
+
+server.post('/api/projects', (req, res) => {
+    const project = req.body;
+
+    projects
+        .addProject(project)
+        .then(ids => {
+            res.status(201).json(ids[0]);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
 const port = 9999;
