6495e3220eeacedb8be2ffc7d26a84ce50034b1d
Casey Baker <caseye.baker@gmail.com>
2018-10-12 09:33:07 -0700

    added console logs for success and errors

--- index.js
+++ index.js
@@ -10,25 +10,27 @@ const port = 9000;
 server.use(helmet());
 server.use(express.json());
 
-// sanity check
+/* == Sanity Check == */
 server.get('/', (req, res) => {
-  res.send("You got this")
+  res.send("You got this!!")
 });
 
 
-/* ===== Projects ===== */
+/* == Projects == */
 
 server.get('/projects', (req, res) => {
   db('projects')
     .then(projects => {
+      console.log(`\n== PROJECTS FOUND ==\n`, projects)
       res
         .status(200)
         .json(projects);
     })
     .catch(err => {
+      console.log(`\n== CANNOT GET PROJECTS ==\n`, err)
       res
         .status(500)
-        .json(err)
+        .json({ error: "The projects could not be retrieved." })
     });
 })
 
@@ -39,6 +41,7 @@ server.get('/projects/:id', (req, res) => {
     .where('id', id)
     .then(project => {
       if (!project.length) {
+        console.log(`\n== PROJECT ID NOT FOUND ==\n`)
         res
           .status(401)
           .json({ error: "There is no project with the specified ID."});
@@ -47,11 +50,13 @@ server.get('/projects/:id', (req, res) => {
           .where('project_id', id)
           .then(actions => {
             project[0].actions = actions;
+            console.log(`\n== PROJECT FOUND ==\n`, project)
             res
               .status(200)
               .json(project)
           })
           .catch(err => {
+            console.log(`\n== CANNOT GET PROJECT ==\n`, err)
             res
               .status(500)
               .json({ error: "The project could not be retrieved."})
@@ -63,6 +68,7 @@ server.post('/projects', (req, res) => {
   const project = req.body;
   const { name } = req.body;
   if (!name) {
+    console.log(`\n== PROJECT NEEDS A NAME ==\n`)
     res
       .status(400)
       .json({ error: "Please add a name for this project."})
@@ -71,11 +77,13 @@ server.post('/projects', (req, res) => {
   db.insert(project)
     .into('projects')
     .then(ids => {
+      console.log(`\n== PROJECT ADDED ==\n`, project)
       res
         .status(201)
         .json(ids);
     })
     .catch(err => {
+      console.log(`\n== CANNOT ADD PROJECT ==\n`, err)
       res
         .status(500)
         .json(err)
@@ -90,27 +98,31 @@ server.put('/projects/:id', (req, res) => {
     .where('id', id)
     .update(project)
     .then(update => {
+      console.log(`\n== PROJECT UPDATED ==\n`, project)
       res
         .status(200)
         .json(update);
     })
     .catch(err => {
+      console.log(`\n== CANNOT UPDATE PROJECT ==\n`, err)
       res
         .status(500)
         .json(err);
     })
 })
 
-/* ===== Actions ===== */
+/* == Actions == */
 
 server.get('/actions', (req, res) => {
   db('actions')
     .then(actions => {
       res
+      console.log(`\n== ACTIONS FOUND ==\n`)
         .status(200)
         .json(actions)
     })
     .catch(err => {
+      console.log(`\n== CANNOT FIND ACTIONS ==\n`, err)
       res
         .status(500)
         .json(err)
@@ -125,15 +137,18 @@ server.get('/actions/:id', (req, res) => {
     .then(action => {
       if (!action.length) {
         res
+        console.log(`\n== ACTION ID NOT FOUND ==\n`, err)
           .status(401)
           .json({ error: "There is no action with the specified ID." })
       }
         res
+        console.log(`\n== ACTION FOUND ==\n`, err)
           .status(200)
           .json(action)
     })
     .catch(err => {
       res
+      console.log(`\n== CANNOT FIND ACTION ==\n`, err)
         .status(500)
         .json({ error: "The action could not be retrieved." })
     })
@@ -146,11 +161,13 @@ server.post('/actions', (req, res) => {
   db.insert(action)
     .into('actions')
     .then(ids => {
+      console.log(`\n== ACTION ADDED ==\n`)
       res
         .status(201)
         .json(ids);
     })
     .catch(err => {
+      console.log(`\n== CANNOT ADD ACTION ==\n`, err)
       res
         .status(500)
         .json(err)
