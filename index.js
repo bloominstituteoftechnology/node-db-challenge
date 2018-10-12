7a39fd382c3c8fe5c253eee52291fa6523097e3a
Clint Kunz <clintkunz@gmail.com>
2018-10-12 09:49:20 -0600

    index file set up with router

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const projectRoutes = require('./data/helpers/projectRoutes');
+const actionRoutes = require('./data/helpers/actionRoutes');
+
 const server = express();
 const port = 8500;
 
@@ -9,6 +12,7 @@ server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
-
+server.use('/projects', projectRoutes);
+server.use('/actions', actionRoutes);
 
 server.listen(port, () => console.log(`\nAPI running on http://localhost:${port}\n`));
