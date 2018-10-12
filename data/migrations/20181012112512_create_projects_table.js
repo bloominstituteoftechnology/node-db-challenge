25b8ecdbd3b83bdb3de4a59d10ba253d1bc09eca
Timothy Hoang <timh1203@gmail.com>
2018-10-12 11:28:31 -0400

    Add migration to create projects table

---
+++ data/migrations/20181012112512_create_projects_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('projects', function(t) {
+    t.increments();
+    t.string('name').notNullable().unique('project_name');
+    t.string('description');
+    t.boolean('completed');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('projects');
+};
