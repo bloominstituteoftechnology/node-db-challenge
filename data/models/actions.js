2e2077f7dbc8b77a03807fce8b9f7a9a0653be73
Andrew <ghobrial.andrew@gmail.com>
2018-10-12 13:24:34 -0400

    added models

---
+++ data/models/actions.js
@@ -0,0 +1,13 @@
+const knex = require('knex');
+ const knexConfig = require('../knexfile.js');
+
+const db = knex(knexConfig.development);
+ module.exports = {
+    addAction
+};
+
+function addAction(action) {
+    return db('actions')
+    .insert(action)
+    .into('actions');
+}
---
+++ data/models/projects.js
@@ -0,0 +1,13 @@
+const knex = require('knex');
+const knexConfig = require('../knexfile.js');
+const db = knex(knexConfig.development);
+ 
+module.exports = {
+    addProject
+};
+ 
+function addProject(project) {
+    return db('projects')
+    .insert(project)
+    .into('projects');
+}
