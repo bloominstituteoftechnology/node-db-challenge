4fb19f514b0eb8ac919f2fa63e2e48016c998d21
Andrew <ghobrial.andrew@gmail.com>
2018-10-12 19:36:59 -0400

    pushed back up functions from model that were erroneously removed before last push

--- data/models/actions.js
+++ data/models/actions.js
@@ -2,7 +2,6 @@ const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 const db = knex(knexConfig.development);
 
-const db = knex(knexConfig.development);
  module.exports = {
     addAction
 };
--- data/models/projects.js
+++ data/models/projects.js
@@ -2,11 +2,23 @@ const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 const db = knex(knexConfig.development);
 
-const db = knex(knexConfig.development);
  module.exports = {   
     addProject,
-    getProjectActions
+    getProjectActions,
+    findById,
+    find
 };
+
+const find = () => {
+    return db("projects");
+};
+
+const findById = id => {
+    return db("projects")
+      .where({ id })
+      .first();
+  };
+
 function addProject(project) {
     return db('projects')
     .insert(project)
