c0bba6421fb35ef5cb84b6f81ee1e49ffc7e537d
alejandrok93 <alejandrok93@gmail.com>
2018-10-12 10:04:27 -0700

    Finished API endpoint to retrieve project with actions

---
+++ data/dataModel.js
@@ -0,0 +1,27 @@
+const knex = require("knex");
+
+//set up database access
+const knexConfig = require("../knexfile.js");
+const db = knex(knexConfig.development);
+
+const find = () => {
+  return db("projects");
+};
+
+const findById = id => {
+  return db("projects")
+    .where({ id })
+    .first();
+};
+
+const getProjectActions = id => {
+  return db
+    .where({ project_id: id })
+    .select("id", "description", "notes", "complete")
+    .from("actions");
+};
+
+const insert = newProject => {
+  return db("projects").insert(newProject);
+};
+module.exports = { find, insert, findById, getProjectActions };
--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const knex = require("knex");
 
 const server = express();
 
-//set up database access
-const knexConfig = require("./knexfile.js");
-const db = knex(knexConfig.development);
+//import data model
+const db = require("./data/dataModel.js");
 
 //apply middleware
 server.use(express.json());
@@ -14,9 +13,51 @@ server.use(helmet());
 
 //Routes
 server.get("/api/projects", (req, res) => {
-  db("projects")
+  db.find()
     .then(projects => res.status(200).json(projects))
     .catch(err => res.status(500).json(err));
+});
+
+server.post("/api/projects", (req, res) => {
+  const newProject = req.body;
+  db.insert(newProject)
+    .then(id => res.status(201).json(id))
+    .catch(err => res.status(500).json(err));
+});
+
+server.get("/api/projects/:id", (req, res) => {
+  const id = req.params.id;
+  let project = {};
+  if (!id) {
+    res.status(400).json({ error: "Bad request" });
+  }
+  //retrieve project
+  db.findById(id)
+    .then(result => {
+      if (result) {
+        project = result;
+
+        //retreive project actions
+        db.getProjectActions(id)
+          .then(actionsObj => {
+            project.actions = actionsObj;
+            res.status(200).json(project);
+          })
+          .catch(err =>
+            res
+              .status(200)
+              .json({ error: "There was an error retrieving the project" })
+          );
+      } else
+        res
+          .status(200)
+          .json({ error: "There was an error retrieving the project" });
+    })
+    .catch(err =>
+      res
+        .status(200)
+        .json({ error: "There was an error retrieving the project" })
+    );
 });
 
 //Listen on port
