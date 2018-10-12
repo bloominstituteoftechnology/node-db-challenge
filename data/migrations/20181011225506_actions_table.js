534c0e6e594e213cea2f36589b2b842b53744b4b
Alexandra <alexswartz9753@gmail.com>
2018-10-11 22:59:11 -0400

    created projects and actions tables

--- data/migrations/20181011225506_actions_table.js
+++ data/migrations/20181011225506_actions_table.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('actions', function(table) {
+      table.increments();
+      table.string('description', 280).notNullable();
+      table.string('notes', 500);
+      table.boolean('completed');
+      table.integer('project_id')
+        .unsigned()
+        .references('id')
+        .inTable('projects');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('actions');
 };
