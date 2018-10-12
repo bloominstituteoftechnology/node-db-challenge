6bcef911d571bc47ca6dc7e142f4fe9c3aa29337
m-j-hacker <mjhacker@gmail.com>
2018-10-12 10:51:05 -0500

    created migration table 'actions'

--- data/migrations/20181012104245_02_actions_table.js
+++ data/migrations/20181012104245_02_actions_table.js
@@ -1,8 +1,22 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('actions', function(tbl) {
+    tbl.increments();
+
+    tbl.string('description').notNullable();
+
+    tbl.string('notes');
+
+    tbl.boolean('completed');
+
+    tbl
+        .integer('project_id')
+        .unsigned()
+        .references('id')
+        .inTable('projects')
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('actions');
 };
