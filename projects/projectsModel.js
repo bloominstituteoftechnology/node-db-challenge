5c0bbe980e0dc09b8c5794185c02992aebcc2a5d
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-10-12 11:31:55 -0700

    New attempt at getting project by ID with actions inside. Not correct closer than before

--- projects/projectsModel.js
+++ projects/projectsModel.js
@@ -20,25 +20,25 @@ function findById(id) {
     //         .from('actions')
     //         .where('project_id', id);
     // }
-    return db('projects')
+
+    // return db
+    //     .select('*')
+    //     .from('projects', 'actions')
+        // .where('project.id', '=', 'actions.project_id')
+
+        return db('projects')
         .select('*')
-        .from('projects')
-        .where({id})
-        // .findActions(id);
-        // .select({
-        //     project_name: 'projects.name',
-        //     action_description: 'actions.description'
-        // })
-        // .join('actions', 'actions.project_id', 'projects.id')
-        // .where('projects.id', id);
+        .join('actions', 'actions.project_id', 'projects.id')
+        .where('projects.id', id);
+
 }
 
-function findActions(id) {
-    return db('actions')
-        .select('*')
-        .from('actions')
-        .where('project_id', id);
-}
+// function findActions(id) {
+//     return db('actions')
+//         .select('*')
+//         .from('actions')
+//         .where('project_id', id);
+// }
 
 function add(project) {
     return db('projects')
