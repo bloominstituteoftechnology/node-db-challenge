a33054dcd9320b6b6f1f0de006fa3b57c9777d7c
decagondev <decagondevelopment@gmail.com>
2018-10-12 18:15:13 +0100

    (MVP) : add get endpoint for action at id

--- index.js
+++ index.js
@@ -114,22 +114,65 @@ server
       .catch(next);
   });
 
-server.route("/api/actions").post(function(req, res, next) {
-  const { notes, description, project_id } = req.body;
+server
+  .route("/api/actions")
+  .post(function(req, res, next) {
+    const { notes, description, project_id } = req.body;
 
-  if (!notes) return res.status(400).json({ message: "notes is missing" });
-  if (!description)
-    return res.status(400).json({ message: "description is missing" });
-  if (!project_id)
-    return res.status(400).json({ message: "project_id is missing" });
+    if (!notes) return res.status(400).json({ message: "notes is missing" });
+    if (!description)
+      return res.status(400).json({ message: "description is missing" });
+    if (!project_id)
+      return res.status(400).json({ message: "project_id is missing" });
 
-  db("actions")
-    .insert({ notes, description, project_id })
-    .then(([id]) =>
-      res
-        .status(201)
-        .json({ message: "the action has been created successfully", id })
-    )
+    db("actions")
+      .insert({ notes, description, project_id })
+      .then(([id]) =>
+        res
+          .status(201)
+          .json({ message: "the action has been created successfully", id })
+      )
+      .catch(next);
+  })
+  .get(function(req, res, next) {
+    db("actions")
+      .then(data => res.status(200).json(data.map(booleanMap("complete"))))
+      .catch(next);
+  });
+
+server.route("/api/actions/:id").get(function(req, res, next) {
+  const contexts = db("actions as a")
+    .select("ctx.id as id", "context")
+    .join("action_contexts as ac", "ac.action_id", "a.id")
+    .join("contexts as ctx", "ac.context_id", "ctx.id")
+    .where("a.id", req.params.id);
+
+  const action = db("actions as a")
+    .select({
+      id: "a.id",
+      description: "a.description",
+      notes: "a.notes",
+      complete: "a.complete",
+      project_name: "p.name",
+      project_description: "p.description",
+      project_complete: "p.complete"
+    })
+    .where("a.id", req.params.id)
+    .join("projects as p", "a.project_id", "p.id")
+    .first();
+
+  Promise.all([action, contexts])
+    .then(([action, contexts]) => {
+      if (!action)
+        return res.status(404).json({ message: "the action was not found " });
+
+      const result = {
+        ...booleanMap("complete", "project_complete")(action),
+        contexts
+      };
+
+      return res.status(200).json(result);
+    })
     .catch(next);
 });
 
