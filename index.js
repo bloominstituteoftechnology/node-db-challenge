e87e5c7590fdc21fd546fe0918f13c12262ccfcd
JeanetteFernandez <jeanfern555@gmail.com>
2018-10-12 11:41:03 -0500

    Stretch - Used GET operation to test that projects and actions database was working properly.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js')
 
 const port = 3333;
 const server = express();
+const db = knex(knexConfig.development);
 
 server.use(express.json());
 server.use(helmet());
@@ -11,6 +15,30 @@ server.use(helmet());
 server.get('/', (req, res) => {
     res.send('Am I Alive?????'); //YES!
 });
+
+//Testing Project & Actions GET
+server.get('/api/projects', (req, res) => {
+    db('projects')
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+server.get('/api/actions', (req, res) => {
+    db('actions')
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+
+server.get
 
 server.listen(port, () => {
     console.log(`\n=== Listening on Port ${port} ===\n`)
