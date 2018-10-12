3780bf19398077db85d03401f3678e9c5c253d1a
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-12 11:24:22 -0400

    Created tables using knex migrations

--- data/migrations/20181012111720_add_projects_table.js
+++ data/migrations/20181012111720_add_projects_table.js
@@ -1,8 +1,17 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("projects", function(tbl) {
+    tbl.increments();
+
+    tbl.string("name").notNullable();
+
+    tbl.unique("name");
+
+    tbl.string("description").notNullable();
+
+    tbl.boolean("complete");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("projects");
 };
--- data/migrations/20181012111737_add_actions_table.js
+++ data/migrations/20181012111737_add_actions_table.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("actions", function(tbl) {
+    tbl.increments();
+
+    tbl.string("description").notNullable();
+
+    tbl.string("notes").notNullable();
+
+    tbl.boolean("complete");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("actions");
 };
