89554b4e94cc8e3c89b50a91465d30b202d9b994
Anthony Greb <agreb255@gmail.com>
2018-10-12 14:15:22 -0400

    starting over

---
+++ data/migrations/20181012141312_projects_table.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("projects", function(tbl) {
+    tbl.increments();
+    tbl.string("name", 255).notNullable();
+    tbl.string("description", 255).notNullable();
+    tbl.boolean("complete").defaultTo(false);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("projects");
+};
---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const helmet = require("helmet");
+const knexConfig = require("./knexfile.js");
+const knex = require("knex");
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+server.get("/api/projects/:id", (req, res) => {
+  const { id } = req.params;
+  db("projects")
+    .where(id)
+    .then(project => {
+      if (project.length === 0) {
+        res.status(400).json({ error: "there is no project by that id." });
+      }
+      db("actions")
+        .where(id)
+        .then(actions => {
+          project.actions = actions;
+          res.status(200).json(project);
+        })
+        .catch(err => {
+          res.status(500).json(err);
+        });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.post("/api/projects", (req, res) => {
+  const project = req.body;
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ error: "please provide a name for your project." });
+  }
+  db.insert(project)
+    .into("projects")
+    .then(id => {
+      res.status(200).json(id);
+    })
+    .catch(err => res.status(500).json(err.message));
+});
+
+server.post("/api/actions", (req, res) => {
+  const action = req.body;
+  if (!action) {
+    res.status(400).json({ error: "please provide an action." });
+  }
+  db.insert(action)
+    .into("actions")
+    .then(id => {
+      res.status(200).json(id);
+    })
+    .catch(err => res.status(500).json(err.message));
+});
+
+const port = 8000;
+server.listen(port, function() {
+  console.log(`\n=== API Listening on http://localhost:${port} ===\n`);
+});
