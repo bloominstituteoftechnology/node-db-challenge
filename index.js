b5d3bf2767d7b9ba5dde56bde8e363edf2554b18
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-12 12:40:22 -0400

    added router to index.js to simplify previous code

--- index.js
+++ index.js
@@ -2,28 +2,13 @@ const express = require('express');
 const helmet = require('helmet');
 const dbProjects = require('./models/project_models');
 const dbActions = require('./models/action_models');
+const newRouter = require('./newRouter');
 
 const server = express();
+
 server.use(express.json());
 server.use(helmet());
-
-// post to projects
-
-server.post('/api/projects', (req, res) => {
-  const { name, description } = req.body;
-  dbProjects.add({ name, description })
-  .into('projects')
-  .then(newProject => res.status(201).json(newProject))
-  .catch(err => res.status(500).json(err));
-});
-
-// get projects
-
-server.get('/api/projects',(req,res)=> {
-  dbProjects.find('projects')
-    .then(projects => res.status(200).json(projects))
-    .catch(err => res.status(500).json(err));
-});
+server.use('/api/projects', newRouter);
 
 // server instantiation
 
