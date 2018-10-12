c93576345cadf4f5c750303310cf2ed16875259b
m-j-hacker <mjhacker@gmail.com>
2018-10-12 11:53:33 -0500

    Added actionsRoute to index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 
 const projectsRoute = require('./projects/projectsRoutes.js');
+const actionsRoute = require('./actions/actionsRoutes.js');
 
 const server = express();
 
@@ -15,6 +16,7 @@ server.get('/', (req, res) => {
 });
 
 server.use('/api/projects', projectsRoute);
+server.use('/api/actions', actionsRoute);
 
 const port = 9999;
 server.listen(port, () => console.log(`***API is running on ${port}`));
