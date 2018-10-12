998ebdf7784e889d42c13d64c0cd9102d9fe47bf
Andrew <ghobrial.andrew@gmail.com>
2018-10-12 12:59:19 -0400

    POST endpoints added

--- index.js
+++ index.js
@@ -9,7 +9,29 @@ const db = knex(knexConfig.development);
 server.use(express.json());
 server.use(helmet());
 
-//Endpoints go here
+// POST //
+server.post("/api/projects", (req, res) => {
+  const { name } = req.body;
+  const project = { name };
+  db.insert(project)
+    .into("projects")
+    .then(response => res.status(201).json(response))
+    .catch(err => {
+      err.errno === 1 ? res.status(400).json({ error: "invalid input"}): res.status(500).json({error: `Server error: ${err}`})
+    });
+});
+
+server.post("/api/actions", (req, res) => {
+  const { name } = req.body;
+  const action = { name };
+  db.insert(action)
+    .into("actions")
+    .then(response => res.status(201).json(response))
+    .catch(err => {
+      err.errno === 1 ? res.status(400).json({ error: "invalid input"}): res.status(500).json({error: `Server error --> ${err}`})
+    });
+});
+
 
 ///////////////////
 
