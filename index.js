96f412f5ec3862c1e603575f5be6411c29f240e4
decagondev <decagondevelopment@gmail.com>
2018-10-12 16:56:10 +0100

    (MVP) : add get route for projects

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const db = require("knex")(require("./knexfile").development);
+const cors = require("cors");
+const _ = require("lodash");
+
+const server = express();
+
+const booleanMap = (...args) => {
+  return object => {
+    return args.reduce(
+      (acumulated, next) => (
+        { ...acumulated, [next]: !!acumulated[next] }, object
+      )
+    );
+  };
+};
+
+server.use(cors());
+server.use(express.json());
+
+server.route("/api/projects").get((req, res, next) => {
+  db("projects")
+    .then(response =>
+      res.status(200).json(response.map(booleanMap("complete")))
+    )
+    .catch(next);
+});
