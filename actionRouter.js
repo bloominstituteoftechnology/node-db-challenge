e195a064dc36d7747d74b829cede125604c40676
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-12 13:19:34 -0400

    added a function to get project by id, mvp complete

--- actionRouter.js
+++ actionRouter.js
@@ -6,9 +6,9 @@ const db = require('./models/action_models');
 // post to actions
 
 router.post('/', (req, res) => {
-  const { description, notes, completed } = req.body;
-  const action = { description, notes, completed };
-  db.addAction(action)
+  const { project_id, description, notes, completed } = req.body;
+  const newAction = { project_id, description, notes, completed };
+  db.addAction(newAction)
     .then(complete => res.status(201).json(complete))
     .catch(err => res.status(500).json(err.message));
 });
@@ -20,4 +20,28 @@ router.get('/', (req, res) => {
     .then(actions => res.status(200).json(actions))
     .catch(err => res.status(500).json(err.message));
 });
+
+// retrieve a project by its ID
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+   db.getActions('actions')
+    .where('id', id)
+    .then(action => {
+      if (!action.length) {
+        res
+          .status(401)
+          .json({ error: "Invalid ID! Please try again." })
+      }
+        res
+          .status(200)
+          .json(action)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The action could not be retrieved." })
+    })
+})
+
 module.exports = router;
--- newRouter.js
+++ newRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const db = require('./models/project_models');
-const dbAction = require('./models/action_models');
 
 // post to projects
 
