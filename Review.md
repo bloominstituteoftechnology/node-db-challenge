5357055456cf22bc1f035f6ada037410ba0ff448
David Benavidez <benavidez_david13@yahoo.com>
2018-10-12 10:28:53 -0700

    Did some sprint

--- Review.md
+++ Review.md
@@ -3,10 +3,22 @@
 You can exercise your Google-Fu for this and any other _Sprint Challenge_ in the future.
 
 1.  Explain the difference between `RDBMS` and `SQL`.
+An RDBMS is a Relational Database Management System. It is the database software itself.
+SQL, the Structured Query Language, is a language which is typically used to query the RDBMS.
+
+
 1.  Why do tables need a `primary key`?
+
+
 1.  What is the name given to a table column that references the primary key
     on another table.
+
+
 1.  What do we need in order to have a _many to many_ relationship between two
     tables.
+
+
 1.  What SQL statement is used to retrieve data from a table?
+
+
 1.  What SQL clause is used to filter the results of a query?
--- index.js
+++ index.js
@@ -22,6 +22,28 @@ server.get('/api/projects', (req, res) => {
         })
 })
 
+//Get project by id
+server.get("/api/projects/:id", (req, res) => {
+    const { id } = req.params;
+    db("Projects")
+      .where({ id })
+      .then(project => {
+          db("Actions")
+            .where({ project_id: id })
+            .then(actions => {
+              res.status(200).json({ project, actions });
+            })
+            .catch(err => {
+              res.status(500).json(err);
+            });
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+  
+
+
 //Post projects
 server.post('/api/projects', (req, res) => {
     const project = req.body
@@ -36,6 +58,10 @@ server.post('/api/projects', (req, res) => {
 
 })
 
+//Delete projects
+
+
+
 
 //get actions
 server.get('/api/actions', (req, res) => {
@@ -48,40 +74,53 @@ server.get('/api/actions', (req, res) => {
         })
 })
 
-// post actions 
+//get actions by id
 
-server.post("/api/actions",(req,res) => {
-    const action = req.body
-    db.insert(action)
-    .into('Actions')
-    .then(addAction => {
-        res.status(200).json(addAction)
+
+server.get('/api/actions/:id',(req,res)=>{
+    const {id} = req.params
+    db('Actions')
+    .where({id})
+    .first()
+    .then(getAction =>{
+        res.status(200).json(getAction)
     })
     .catch(err => {
-        res.status(500).json(err)
+        res.status(200).json(err)
+    })
+
+})
+
+// post actions 
+
+server.post("/api/actions", (req, res) => {
+    const action = req.body
+    db.insert(action)
+        .into('Actions')
+        .then(addAction => {
+            res.status(200).json(addAction)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+//delete actions 
+
+server.delete("/api/actions/:id",(req,res) => {
+    const {id} = req.params
+    db("Actions")
+    .where({id})
+    .del()
+    .then(del => {
+        res.status(200).json(del)
+    })
+    .catch(err=>{
+        res.status(400).json(err)
     })
 })
 
-
 //Structured get for Project and Actions
-
-server.get('/api/dishes/:id/recipe' ,(req,res)  => {
-    const {id} = req.params
-    db('Recipes') 
-    .where({dish_id:id})
-    .then(ids => {
-        if (ids) {
-          res.status(200).json(ids);
-        } else {
-          res
-            .status(404)
-            .json({ error: `Recipe with id of ${id} could not be found `});
-        }
-      })
-      .catch(err => res.status(500).json({ err: "Could not retrieve data" }));
-    
-    }  )
-    
 
 
 server.listen(9000, () => console.log('api running on 9000'))
