9917959393c50d92ad473490bf2a9381959dd34d
Timothy Hoang <timh1203@gmail.com>
2018-10-12 16:12:39 -0400

    Fix formatting of specific project return

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -36,15 +36,18 @@ router.route('/actions')
 router.route('/projects/:id')
   .get((req, res) => {
     const { id } = req.params
-    db('projects').where({ id })
+    db('projects')
+      .first()
+      .where({ id })
       .then(project => {
         if (!project || project < 1) return res.status(404).json({ error: 'The project with the specified ID was not found.' });
         return db('actions')
           .select('actions.id', 'actions.description', 'actions.notes', 'actions.completed')
           .where({ 'project_id': id })
-          .then(actions => 
-            res.status(200).json([...project, { actions }])
-          )
+          .then(actions => {
+            project.actions = actions
+            res.status(200).json(project)
+          })
           .catch(err => res.status(500).json({ error: 'The action with that project ID was not found.' }))
       })
       .catch(err => res.status(500).json({ error: 'The project with the specified ID could not be retrieved.' }));
