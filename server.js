e2b01b5b852fa4672c4370cc9998ba0d3bbaa5b7
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-10-12 10:06:08 -0500

    Knexfile completed.  Server is up and running.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+// ENPOINTS HERE
+
+server.get('/', (req, res => res.json('Server is up and running!')));
+
+// SERVER PORT
+
+const port = 6000;
+server.listen(port, () => console.log(`API is listening on port ${port}.`));
