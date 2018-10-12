f4ec612b43aad6d5bf0b9eb8fbac9090178d9a14
Eva Li <evalee904@gmail.com>
2018-10-12 11:05:17 -0500

    Projects migration.

---
+++ data/migrations/20181012105524_projects_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('projects', function(tbl){
+    tbl.increments();
+    tbl.string('name', 128).notNullable();
+    tbl.unique('name');
+    tbl.string('description', 400);
+    tbl.boolean('completed'),defaultTo(0);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('projects');
+};
