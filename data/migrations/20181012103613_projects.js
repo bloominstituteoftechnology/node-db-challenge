03ce138b701b3f0a365db6f17101c3eeaa004498
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-10-12 11:01:05 -0500

    migrations

---
+++ data/migrations/20181012103613_projects.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('projects', function(tbl) {
+        tbl.increments();
+        tbl.string('name', 255).notNullable();
+        tbl.string('description');
+        tbl.boolean('completed');
+    })
+  
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('projects');
+};
