60da53627f49905720653721a7e8fdcbca9277d5
Nicocchi <nicocchilover@gmail.com>
2018-10-12 12:27:41 -0400

    GET routes set-up and Project POST

--- routes/projectModel.js
+++ routes/projectModel.js
@@ -5,8 +5,28 @@ const db = knex(knexConfig.development);
 
 module.exports = {
     getProjects,
+    getProject,
+    getAction,
+    addProject,
+    addAction,
 };
 
 function getProjects() {
     return db('project');
+}
+
+function getProject(id) {
+    return db.select('*').from('project').where('project.id', id).first();
+}
+
+function addProject(proj) {
+    return db('project').insert(proj).into('project');
+}
+
+function getAction(id) {
+    return db.select('*').from('action').where('project_id', id)
+}
+
+function addAction(act) {
+    return db('action').insert(act).into('action');
 }
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,12 +1,41 @@
 const express = require('express');
-const project = require('./projectModel.js');
+const projectDb = require('./projectModel.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    project.getProjects().then(projects => {
+    projectDb.getProjects().then(projects => {
         res.status(200).json(projects);
     })
         .catch(err => res.status(500).json(err));
+});
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    projectDb.getProject(id).then(project => {
+        if(!project) res.status(404).json({message: 'Project not found'});
+
+        projectDb.getAction(project.id).then(action => {
+            if(!action) res.status(404).json({message: 'Action not found'});
+            let proj = project;
+            proj.actions = action;
+
+            res.status(200).json(proj);
+        }).catch(err => res.status(500).json(err.message));
+
+    }).catch(err => res.status(500).json(err.message));
+});
+
+router.post('/', (req, res) => {
+    // grab data from body
+    const newProject = req.body;
+
+    //save to database
+    projectDb.addProject(newProject).then(ids => {
+        res.status(201).json(ids);
+    }).catch( err => {
+        res.status(500).json(err.message);
+    });
 });
 
 module.exports = router;
