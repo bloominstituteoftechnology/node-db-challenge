4b5ad35745afc64966c83d33b86cefb0fd4bd470
mich-shrak <micah.shrak@gmail.com>
2018-10-19 10:12:58 -0700

    Added db and tables via migration

--- data/migrations/20181019095507_projects_table.js
+++ data/migrations/20181019095507_projects_table.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex, Promise) {
-  
+	return knex.schema.createTable('projects', function(tbl) {
+		tbl.increments();
+		tbl.string('name').notNullable();
+		tbl.string('description');
+		tbl.boolean('complete');
+	});
 };
 
 exports.down = function(knex, Promise) {
-  
+	return knex.schema.dropTableIfExist('projects');
 };
