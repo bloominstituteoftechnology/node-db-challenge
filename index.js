2fcf7615791915cc35fa43b4d7d9cffeea329bf7
Joseph Chretien-Golden <joechre@gmail.com>
2018-10-19 11:05:04 -0500

    CREATE -db promise and body request add

--- index.js
+++ index.js
@@ -12,3 +12,31 @@ server.use(helmet(), express.json());
 /// ---- CREATE New Project ----
 server.post('/api/projects', (request, response) => {
     // Deconstruct Request Body
+    server.post('/api/projects', (request, response) => {
+        // Deconstruct Request Body
+        let { name, description, complete } = request.body;
+    
+        // Validation
+        if ( !name || name.length < 5 ) {
+            return response.status(400).json({ errorMessage: "Please provide a name when creating a project. Name must be at least five characters long." })
+        }
+    
+        if ( !description ) {
+            description = "";
+        }
+    
+        if ( !complete ) {
+            complete = false;
+        }
+        
+        // Database Promise Methods
+    db.insert(newProject)
+    .into('project')
+    .then( created => {
+        if ( !created || created.length < 1 ) {
+            return response.status(400).json({errorMessage: "We were unable to create a project with the specified information."})
+        }
+        response.status(201).json(created[0]);
+    })
+    .catch( error => response.status(500).json(error) );
+});
