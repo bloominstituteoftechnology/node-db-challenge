f79f9167359227913640e733155cf7a0f1c37915
Joseph Chretien-Golden <joechre@gmail.com>
2018-10-19 12:13:54 -0500

    CREATE -add new action -add delete proj

--- index.js
+++ index.js
@@ -28,7 +28,9 @@ server.post('/api/projects', (request, response) => {
         if ( !complete ) {
             complete = false;
         }
-        
+        // Construct New Project Object
+    const newProject = { name, description, complete };
+
         // Database Promise Methods
     db.insert(newProject)
     .into('project')
@@ -66,3 +68,91 @@ server.get('/api/projects/:id', (request, response) => {
     'a.complete')
     .where('p.id', id)
     .then(project => { 
+
+        // Validation of Projects
+        if ( Object.keys(project).length < 1 ) {
+            return response.status(404).json({errorMessage:"We were unable to find any projects with the provided id."})
+        }
+
+        // Deconstruct Action + Promise Join
+        const { project_id, project_name, project_description, project_completed } = project[0];
+
+        // Empty Array of Actions as Default
+        let actions = [];
+
+        // Construct Array of Actions if Actions Exist
+        if (project[0].id !== null) {
+        actions = project.map(
+             action => {
+                 return { id: action.id, description: action.description, notes: action.notes, completed: action.complete }
+                });
+            }
+
+        // Construct Project Object
+        const projectAndActions = { id: project_id, name: project_name, description: project_description, completed: project_completed, actions:actions };
+        response.status(200).json(projectAndActions);
+        })
+    .catch(error => response.status(500).json(error));
+});
+
+/// ---- DELETE Project ----
+
+server.delete('/api/projects/:id', (request, response) => {
+    // Extract URL Parameters
+    const { id } = request.params;
+
+    if ( !id || id < 1 ) {
+        return response.status(400).json({ errorMessage: "The provided project id is invalid." })
+    }
+    
+    db('project')
+    .where({ id })
+    .del()
+    .then( deleted => {
+        if (!deleted || deleted < 1) {
+            return response.json({errorMessage:"We were unable to delete a project with the specified id."})
+        }
+
+        response.json(deleted)
+    }
+
+    )
+    .catch(error => response.status(500).json(error))
+
+});
+
+/// ---- CREATE New Action ----
+server.post('/api/actions', (request, response) => {
+    // Deconstruct Request Body
+    let { description, notes, complete, project_id } = request.body;
+
+    // Validation
+    if ( !description || description.length < 5 || !project_id || project_id < 1 ) {
+        return response.status(400).json({ errorMessage: "Please provide a description and project_id when creating an action. Description must be at least five characters long." })
+    }
+
+    if ( !notes ) {
+        notes = "";
+    }
+
+    if ( complete !== true ) {
+        complete = false;
+    }
+
+    // Construct New Project Object
+    const newAction = { project_id, description, notes, complete };
+
+    // Database Promise Methods
+    db.insert(newAction)
+    .into('action')
+    .then( created => {
+        if ( !created || created.length < 1 ) {
+            return response.status(400).json({ errorMessage: "We were unable to create an action with the specified information." })
+        }
+        response.status(201).json(created[0]);
+    })
+    .catch( error => response.status(500).json(error) );
+});
+
+const port = "4242";
+server.listen(port, () => {console.log(`#####- Projects Server Active on Port: ${port} -#####`)});
