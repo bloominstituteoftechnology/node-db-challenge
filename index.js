3724c34033556266d4e2097ea3ed4ff39ec93bda
Sasha <taylork85@gmail.com>
2018-10-19 11:52:25 -0500

    CREATE GET & POST ROUTES -index.js

--- index.js
+++ index.js
@@ -9,8 +9,46 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+server.get('/api/projects', (req, res) => {
+    db('projects')
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.send(err.message)
+    })
+})
+
+server.post('/api/projects', (req, res) => {
+    const project = req.body;
+    db.insert(project)
+      .into('projects')
+      .then(id => {
+        res.status(201).json(id)
+      })
+      .catch(err => res.send(err.message))
+})
+
+server.get('/api/actions', (req, res) => {
+    db('actions')
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(err => {
+        res.send(err.message)
+    })
+})
 
 
+server.post('/api/actions', (req, res) => {
+    const action = req.body;
+    db.insert(action)
+      .into('actions')
+      .then(id => {
+        res.status(201).json(id)
+      })
+      .catch(err => res.send(err.message))
+})
 
 
 
