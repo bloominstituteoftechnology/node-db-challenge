c14b378733531a3c674db8b518fb960a42eb07db
Marious Davis <mlancelot88@gmail.com>
2018-10-19 13:21:43 -0500

    added seed for actions in actionsListing.js

---
+++ data/seeds/actionsListing.js
@@ -0,0 +1,32 @@
+exports.seed = function(knex, Promise) {
+  return knex("actions")
+    .truncate()
+    .then(function() {
+      return knex("actions").insert([
+        {
+          project_id: 1,
+          description: "Open Visual Studio Code.",
+          notes: "Ensure you are running the latest version.",
+          completed: "false"
+        },
+        {
+          project_id: 1,
+          description: "Fire up your terminal.",
+          notes: "Terminal will run a server.",
+          completed: "false"
+        },
+        {
+          project_id: 1,
+          description: "Head over to project repo instructions.",
+          notes: "Read instructions carefully and thoroughly.",
+          completed: "false"
+        },
+        {
+          project_id: 1,
+          description: "Initialize a yarn file.",
+          notes: "Run yarn init -y",
+          completed: 0
+        }
+      ]);
+    });
+};
