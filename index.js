6f1c430172ba1377790f47ec75da35e0827671f0
IlanL75 <anatoly.leytman@gmail.com>
2018-10-19 09:56:32 -0700

    Adding GET, POST for projects and actions

--- index.js
+++ index.js
@@ -15,6 +15,49 @@ server.get('/', (req, res) => {
 });
 
 
+server.post('/projects', (req,res) => {
+    const project = req.body;
+    db.insert(project).into('projects')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+  
+server.get('/projects', (req, res) => {
+    db('projects')
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.post('/actions', (req,res) => {
+    const action = req.body;
+    db.insert(action).into('actions')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+  
+server.get('/actions', (req, res) => {
+    db('actions')
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+
 
 const port = 5000;
 server.listen(port, function() {
