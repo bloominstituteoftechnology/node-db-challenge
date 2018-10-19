a0061f8bb5685ca0ede7489ef5bc674823ce9c2e
lyang9 <lyang9@buffalo.edu>
2018-10-19 12:18:37 -0400

    Changed createTable to projects

--- data/migrations/20181019113305_projects_table.js
+++ data/migrations/20181019113305_projects_table.js
@@ -1,5 +1,5 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('project', function(tbl) {
+  return knex.schema.createTable('projects', function(tbl) {
     tbl.increments();
     tbl.string('name', 20).notNullable();
     tbl.string('description', 50).notNullable();
