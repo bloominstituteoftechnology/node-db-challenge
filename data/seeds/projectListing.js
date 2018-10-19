85d969e6d3e812e0d16071ffaa422dc295b7c3b2
Marious Davis <mlancelot88@gmail.com>
2018-10-19 13:21:07 -0500

    added seed for projects in projectListing.js

---
+++ data/seeds/projectListing.js
@@ -0,0 +1,20 @@
+exports.seed = function(knex, Promise) {
+  return knex("projects")
+    .truncate()
+    .then(function() {
+      return knex("projects").insert([
+        {
+          id: 1,
+          name: "Sprint Challenge: Week 12",
+          description: "Challenge for the week",
+          completed: 0
+        },
+        {
+          id: 2,
+          name: "Sprint Challenge: Week 11",
+          description: "Challenge for the week",
+          completed: 0
+        }
+      ]);
+    });
+};
