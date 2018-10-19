859cc736bcbc6cc462a8d76d4ae1fc0eb7489076
Marious Davis <mlancelot88@gmail.com>
2018-10-19 12:14:14 -0500

    updated access.js

---
+++ routes/access.js
@@ -0,0 +1,16 @@
+const knex = require("knex");
+const knexConfig = require("../knexfile.js");
+const db = knex(knexConfig.development);
+
+function addProject(project) {
+  return db.insert(project).into("projects");
+}
+
+function addAction(action) {
+  return db.insert(action).into("actions");
+}
+
+module.exports = {
+  addProject,
+  addAction
+};
