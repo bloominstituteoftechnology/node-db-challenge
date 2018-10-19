fe6a4a853dde3e8e797980b30998646e44de6754
Marious Davis <mlancelot88@gmail.com>
2018-10-19 13:00:07 -0500

    stripped knex config statements from allRoutes.js

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const db = knex(knexConfig.development);
-const knex = require("knex");
-const knexConfig = require("../knexfile.js");
+
 const router = express.Router();
 
 router
