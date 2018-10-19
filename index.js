1b44334e61191d472550868d91673e0e871c4c73
lyang9 <lyang9@buffalo.edu>
2018-10-19 13:22:27 -0400

    server.get read all projects/server.post create projects

--- index.js
+++ index.js
@@ -11,6 +11,36 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+// endpoints here
+server.get('/', (req, res) => {
+  res.send('It is working!');
+});
+
+// read all projects
+server.get('/api/projects', (req, res) => {
+  db('projects')
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
+// create projects
+server.post('/api/projects', (req, res) => {
+  const project = req.body;
+  db.insert(project)
+    .into('projects')
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
+
 // listening port
 const port = 5000;
 server.listen(port, function() {
