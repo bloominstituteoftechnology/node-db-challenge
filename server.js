4365cd9d7d657ef0fa02129fa7e96915f2493aee
A2i <abdulfatah.a.ahmad@gmx.com>
2018-10-19 12:00:11 -0400

    Instantiated Application Server
    #Comment
    
    Added an application server instance using express()
    Plugged in all the middleware using 'express.use()'

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const knex = require('knex');
+
+//Server Instantiation
+const appServ = express();
+
+//Third party middleware
+//express.json returns json objects of the response
+//All global middlewares that will be used across enpoints must also be plugged into the app
+//cors and helmet middlewares are not used
+appServ.use(express.json(), logger('combined'), cors(), helmet());
+
+//Root Request/Route Handler
+appServ.get('/', (req, res) => {
+    res.send('Test for root endpoint!')
+});
