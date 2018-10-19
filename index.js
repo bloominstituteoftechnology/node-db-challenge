13fdc1ffd00112d0ebb3b3264f19650f90ac07fc
lyang9 <lyang9@buffalo.edu>
2018-10-19 12:33:03 -0400

    Added listening port

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ const server = express();
 
 server.use(express.json());
 server.use(helmet());
+
+// listening port
+const port = 5000;
+server.listen(port, function() {
+  console.log(`\n=== API listening on port ${port} ===\n`);
+});
