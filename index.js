252ca442cc09a221bde9dcec58f374db1b96aa35
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-19 10:26:55 -0500

    all dependencies in place. API functional. setup complete

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const helmet = require('helmet');
+
+// const routes = require('./Routes/routes.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send("It's Alive");
+});
+
+// server.use('/api/projects', routes);
+// server.use('/api/students', routes);
+
+server.listen(9000, () => console.log('\nAPI running on 9k\n'));
