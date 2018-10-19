e0c670b87ad46c39aaa0cfd752368a7f5dedfc9f
Marious Davis <mlancelot88@gmail.com>
2018-10-19 14:35:49 -0500

    rest of revisions

--- routes/access.js
+++ routes/access.js
@@ -1,38 +1,42 @@
-// const knex = require("knex");
-// const knexConfig = require("../knexfile.js");
-// const db = knex(knexConfig.development);
+const knex = require("knex");
+const knexConfig = require("../knexfile.js");
+const db = knex(knexConfig.development);
 
-// function addProject(project) {
-//   return db.insert(project).into("projects");
-// }
+function getProjects() {
+  return db("projects");
+}
 
-// function addAction(action) {
-//   return db.insert(action).into("actions");
-// }
+function getActions() {
+  return db("actions");
+}
 
-// function getProjectById(id) {
-//   db("projects")
-//     .where({ id })
-//     .then(project => {
-//       return db("actions")
-//         .select(
-//           "actions.id",
-//           "actions.description",
-//           "actions.notes",
-//           "actions.completed"
-//         )
-//         .where({ project_id: id })
-//         .then(actions => {
-//           const completeProject = [...projects, { actions }];
-//           return completeProject;
-//         })
-//         .catch(err => err);
-//     })
-//     .catch(err => err);
-// }
+function addProject(project) {
+  return db.insert(project).into("projects");
+}
 
-// module.exports = {
-//   addProject,
-//   addAction,
-//   getProjectById
-// };
+function addAction(action) {
+  return db.insert(action).into("actions");
+}
+function getProjectById(id) {
+  return db("projects")
+    .first()
+    .where({ id });
+}
+function addActionsToProject(id) {
+  return db("actions")
+    .select(
+      "actions.id",
+      "actions.description",
+      "actions.notes",
+      "actions.completed"
+    )
+    .where({ project_id: id });
+}
+module.exports = {
+  getProjects,
+  getActions,
+  addProject,
+  addAction,
+  getProjectById,
+  addActionsToProject
+};
