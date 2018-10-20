0eb362090e254b36464e6054ac7f1509cdf0f5bc
lyang9 <lyang9@buffalo.edu>
2018-10-20 16:05:57 -0400

    Added unique to name

--- data/migrations/20181019113305_projects_table.js
+++ data/migrations/20181019113305_projects_table.js
@@ -1,10 +1,13 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('projects', function(tbl) {
     tbl.increments();
+    
     tbl.string('name', 20).notNullable();
     tbl.string('description', 50).notNullable();
 
     tbl.boolean('completed').defaultTo(0);
+
+    tbl.unique('name');
   })
 };
 
