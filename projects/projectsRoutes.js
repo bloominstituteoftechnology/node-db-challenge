9e193885ee35973df3a2edc4bf2c3d29d814410c
lyang9 <lyang9@buffalo.edu>
2018-10-20 18:42:29 -0400

    Changed server.get read projects by id

--- projects/projectsRoutes.js
+++ projects/projectsRoutes.js
@@ -8,92 +8,104 @@ const router = express.Router();
 
 // read all projects
 router.get('/', (req, res) => {
-    db('projects')
-      .then(projects => {
-        res.status(200).json(projects);
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      })
-  });
-  
-  // read projects by id
-  router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    db('projects').where({ id })
-      .then(project => {
-        if (project) {
-          res.status(200).json(project[0]);
-        } else {
-          res.status(404).json({ message: 'project not found' });
-        }
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      })
-  });
-  
-  // get all actions by project
-  router.get('/:id/actions', (req, res) => {
-    const { id } = req.params;
-    db('actions')
-      .where({ project_id: id })
-      .then(actions => {
-        res.status(200).json(actions);
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      })
-  });
-  
-  // create projects
-  router.post('/', (req, res) => {
-    const project = req.body;
-    db.insert(project)
-      .into('projects')
-      .then(id => {
-        res.status(201).json(id);
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      })
-  });
-  
-  // update projects
-  router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const newProject = req.body;
-    db('projects')
-      .where({ id })
-      .update(newProject)
-      .then(project => {
-        if (!project || project < 1) {
-          res.status(404).json({ message: 'No records found to update' });
-        } else {
-          res.status(200).json(project);
-        }
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      })
-  });
-  
-  // delete projects
-  router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    db('projects')
-      .where({ id })
-      .del()
-      .then(project => {
-        if (!project || project < 1) {
-          res.status(404).json({ message: 'No records found to delete' });
-        } else {
-          res.status(200).json(project);
-        }
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      })
-  });
+  db('projects')
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
 
-  module.exports = router;
+// read projects by id
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  db('projects')
+    .where({ id })
+    .first()
+    .then(project => {
+      if (project) {
+        db('actions')
+          .where({ project_id: id })
+          .then(actions => {
+            project.actions = actions;
+            res.status(200).json(project);
+          })
+          .catch(err => {
+            res.status(500).json(err);
+          })
+      } else {
+        res.status(404).json({ message: 'project not found' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
+
+
+// get all actions by project
+router.get('/:id/actions', (req, res) => {
+  const { id } = req.params;
+  db('actions')
+    .where({ project_id: id })
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
+// create projects
+router.post('/', (req, res) => {
+  const project = req.body;
+  db.insert(project)
+    .into('projects')
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
+// update projects
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const newProject = req.body;
+  db('projects')
+    .where({ id })
+    .update(newProject)
+    .then(project => {
+      if (!project || project < 1) {
+        res.status(404).json({ message: 'No records found to update' });
+      } else {
+        res.status(200).json(project);
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
+// delete projects
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  db('projects')
+    .where({ id })
+    .del()
+    .then(project => {
+      if (!project || project < 1) {
+        res.status(404).json({ message: 'No records found to delete' });
+      } else {
+        res.status(200).json(project);
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
+module.exports = router;
