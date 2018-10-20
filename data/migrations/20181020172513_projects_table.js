fc594675146b6f30c125131a6494df8cd501c0f9
lyang9 <lyang9@buffalo.edu>
2018-10-20 17:27:45 -0400

    Completed migration projects_table

---
+++ data/migrations/20181020172513_projects_table.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('projects', function(tbl) {
+    tbl.increments();
+    
+    tbl.string('name', 20).notNullable();
+    tbl.string('description', 50).notNullable();
+
+    tbl.boolean('completed').defaultTo(0);
+
+    tbl.unique('name');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('projects');
+};
