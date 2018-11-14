b0ef15f1fd77b83bf894ac24916de35202c879b9
Lee Len Formento <leebatef@gmail.com>
2018-11-14 00:22:56 -0500

    Fixed typo

--- data/migrations/20181114001818_projects_table.js
+++ data/migrations/20181114001818_projects_table.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex, Promise) {
-    return knex.schema.table('projects', function(tbl) {
+    return knex.schema.createTable('projects', function(tbl) {
         tbl.increments();
 
         tbl
