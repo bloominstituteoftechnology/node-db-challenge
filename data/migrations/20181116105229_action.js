e76dbc39d2b6316b9a524d8b88d2b780f64d3d5f
Steven O'Neill <oneillsteven957@gmail.com>
2018-11-16 11:02:18 -0600

    Finished migrations

---
+++ data/migrations/20181116105229_action.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('actions', tbl => {
+      tbl.increments();
+
+      tbl
+        .string('description', 255)
+        .notNullable();
+      tbl
+        .text('notes')
+        .notNullable();
+      tbl
+        .boolean('completed')
+        .defaultTo(false);
+      tbl
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('actions');
+};
