57b342cb16db5136043fc00c9a856440691d34fa
Matthew Wright <matthewbwright@gmail.com>
2018-11-16 09:58:34 -0700

    adds sanity check

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const server = express();
 
 server.use(express.json());
 
+//sanity check
+server.get('/', (req, res) => {
+  res.json({ api: 'are you ready to be productive?' });
+});
+
 const port = 7000;
 server.listen(port, function() {
   console.log(`\n=== Web API listening on http://localhost:${port} ===\n`);
