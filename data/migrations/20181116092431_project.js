bb9ee00681c8815a73b15e17400cf2416b61d86b
SavannahGreuel <smallidgesavannah@gmail.com>
2018-11-16 09:37:31 -0700

    migration for projects

--- data/migrations/20181116092431_project.js
+++ data/migrations/20181116092431_project.js
@@ -1,6 +1,22 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('projects', tbl => {
+      tbl
+      .increments()
+      .primary();
+
+      tbl
+      .string('name', 250)
+      .notNullable();
+
+      tbl
+      .string('description', 1000)
+      .notNullable();
+
+      tbl
+      .boolean('completed')
+      .defaultTo(false);
+  })
 };
 
 exports.down = function(knex, Promise) {
