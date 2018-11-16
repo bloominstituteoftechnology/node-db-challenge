2f9e5850686d52c9a64ff58738694600eeba2d2b
Yanrong Xu <xuyanrong@icloud.com>
2018-11-16 11:33:22 -0500

    create migration project table

---
+++ data/migrations/20181116112837_projects_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('project', function(tbl){
+      tbl.increments()
+      tbl.string('name', 20).notNullable()
+      tbl.string('description', 50).notNullable()
+      tbl.boolean('complete').defaultTo(0)
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('project')
+};
