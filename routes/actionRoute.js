780f75ab4b818ac63073d150a88e48c6eb8ffba2
Gannon <gkleblanc@gmail.com>
2018-11-16 13:47:03 -0500

    fixed bug and changed delete and put endpoints

--- routes/actionRoute.js
+++ routes/actionRoute.js
@@ -8,9 +8,9 @@ const db = knex(knexConfig.development)
 
 //endpoints
 router.post('/', async (req, res) => {
-    const { name, description } = req.body
-    if (!name || !description) {
-        res.status(404).json({ message: 'Adding an action requires a name and description' })
+    const { name, description, projectId } = req.body
+    if (!name || !description || !projectId) {
+        res.status(404).json({ message: 'Adding an action requires a name and description for the action and projectId' })
     }
     try {
         const action = await db('actions').insert(req.body)
@@ -31,19 +31,19 @@ router.delete('/:id', async (req, res) => {
     try {
         const count = await db('actions').where({ 'id': req.params.id }).del()
         count > 0
-            ? res.status(200).json(id)
+            ? res.status(200).json(count)
             : res.status(404).json({ errorMessage: 'An action with that ID cannot be found.' })
     } catch (err) {
         res.status(500).json({ error: 'An error occuried while trying to delete that action from the database.' })
     }
 })
 router.put('/:id', async (req, res) => {
-    const { name, description } = req.body
+    const { name, description, projectId } = req.body
     const { id } = req.params
     if (!name && !description) res.status(400).json({ message: 'Updating an action requires a name and description.' })
     try {
-        const actionUpdate = await db('actions').update(req.body).where('id', id)
-        actionUpdate !== 0 ? res.status(200).json(actUp) : res.status(400).json({ errorMessage: 'That ID does not exist.' })
+        const count = await db('actions').update(req.body).where('id', id)
+        count !== 0 ? res.status(200).json(count) : res.status(400).json({ errorMessage: 'That ID does not exist.' })
     } catch (err) {
         res.status(500).json({ error: 'An error occuried while trying to update the action.' })
     }
--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -46,26 +46,34 @@ router.get('/:id', async (req, res) => {
 })
 router.delete('/:id', async (req, res) => {
     try {
-        const count =
-            (await db('projects').where({ 'id': req.params.id }).del()) &&
-            (await db('actions').where({ 'projectId': req.params.id }).del())
-        count > 0
-            ? res.status(200).json(count)
-            : res.status(400).json({ message: 'A project with that ID could not be found.' })
+        const pCount =
+            (await db('projects').where({ 'id': req.params.id }).del())
+        if (pCount > 0) {
+            try {
+                const aCount = (await db('actions').where({ 'projectId': req.params.id }).del())
+                aCount !== 0
+                    ? res.status(200).json({ message: 'Project deleted' })
+                    : res.status(400).jscon({ message: 'Could not delete actions for the project' })
+            } catch (err) {
+                res.status(500).json({ error: "The project actions could not be deleted" })
+            }
+        } else {
+            res.status(404).json({ message: 'A project with that ID could not be found.' })
+        }
     } catch (err) {
         res.status(500).json({ error: 'The project could not be deleted.' })
     }
-})
-router.put('/:id', async (req, res) => {
-    const { name, description } = req.body
-    const { id } = req.params
-    if (!name && !description)
-        res.status(404).json({ message: 'Editing a project requires both a name and a description.' })
-    try {
-        const projUp = await db('projects').update(req.body).where({ 'id': id })
-        projUp !== 0 ? res.status(200).json(projUp) : res.status(400).json({ errorMessage: 'A project with that ID could not be found.' })
-    } catch (err) {
-        res.status(500).json({ error: 'The project could not be saved.' })
-    }
-})
-module.exports = router
+    router.put('/:id', async (req, res) => {
+        const { name, description } = req.body
+        const { id } = req.params
+        if (!name && !description)
+            res.status(404).json({ message: 'Editing a project requires both a name and a description.' })
+        try {
+            const projUp = await db('projects').update(req.body).where({ 'id': id })
+            projUp !== 0 ? res.status(200).json(projUp) : res.status(400).json({ errorMessage: 'A project with that ID could not be found.' })
+        } catch (err) {
+            res.status(500).json({ error: 'The project could not be saved.' })
+        }
+    })
+
+    module.exports = router
