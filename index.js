e2f6c52af34a6a0c1034a51977dd535a1942202c
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-11-16 12:49:07 -0600

    finished refactoring

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const port = process.env.PORT || 9000;
 const server = express();
+const cors = require('cors');
 const actionRoutes = require('./db/Routes/actionRoutes');
 const projectRoutes = require('./db/Routes/projectRoutes');
 
 server.use(express.json());
+server.use(cors());
 server.use('/actions', actionRoutes);
 server.use('/projects', projectRoutes);
 
