1e6246a643f9a9ddcf3ead9520a275bb8311202c
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-11-16 08:48:00 -0800

    actions migration

--- migrations/20181116083858_actions.js
+++ migrations/20181116083858_actions.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("actions", tbl => {
+    tbl.increments();
+    tbl.string("description", 255).notNullable();
+    tbl.text("notes");
+    tbl
+      .boolean("completed")
+      .notNullable()
+      .defaultTo(false);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("actions");
 };
