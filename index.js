a2b54b8ee2275f22e00f428ff4660eec4cf3c920
SavannahGreuel <smallidgesavannah@gmail.com>
2018-11-16 10:22:12 -0700

    added post endpoints for projects and actions

--- index.js
+++ index.js
@@ -10,8 +10,50 @@ const server = express();
 server.use(express.json());
 
 
+
+//endpoints for projects
+server.get('/api/projects', (req, res) => {
+    db('projects')
+      .then(project => res.status(200).json(project))
+      .catch(err => res.status(500).json(err));
+  });
+
+server.post('/api/projects', (req, res) => {
+    const project = req.body;
+
+    db('projects')
+        .insert(project)
+        .returning('id')
+        .then(ids => {
+            res.status(201).json({message: 'successfuly added project with the id of...', ids});
+        })
+        .catch(error => {
+            res.status(500).json({message: 'error inserting', error})
+        })
+});
+
+
+//endpoints for actions
+server.post('/api/projects/actions', (req, res) => {
+    const action = req.body;
+
+    db('actions')
+        .insert(action)
+        .returning('id')
+        .then(ids => {
+            res.status(201).json({message: 'successfuly added action with the id of...', ids});
+        })
+        .catch(error => {
+            res.status(500).json({message: 'error inserting', error})
+        })
+});
+
+
+//test endpoint
 server.get('/', (req, res) => {
     res.json({ api: 'up' });
   });
   
+
+//defines port  
   server.listen(9000, () => console.log('\n== Port 9k ==\n'));
