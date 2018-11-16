a496161fbb810b68e7db70dfa00993c9184abc3c
Mindysueasaurus <mymindyyeager@gmail.com>
2018-11-16 07:44:44 -0600

    makes knex, index, migrations, route and api files

---
+++ data/migrations/20181115235249_projects.js
@@ -0,0 +1,12 @@
+// A `project` can contain multiple actions and has:
+//   - a unique Id.
+//   - a name.
+//   - a description.
+//   - a flag that indicates if the project is complete or not.
+exports.up = function(knex, Promise) {
+  
+};
+
+exports.down = function(knex, Promise) {
+  
+};
---
+++ data/migrations/20181115235306_actions.js
@@ -0,0 +1,12 @@
+// An `action` belongs to only one project. An action has:
+//   - a unique id.
+//   - a description of what needs to be done.
+//   - a notes column to add additional information.
+//   - a flag that indicates if the action has been completed.
+exports.up = function(knex, Promise) {
+  
+};
+
+exports.down = function(knex, Promise) {
+  
+};
