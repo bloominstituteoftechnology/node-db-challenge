cdc5f477f0160ed52c0f9d07cf38c6f0a645b8ab
Drewsup123c <drewsup123c@gmail.com>
2018-11-16 10:22:11 -0700

    finished POST for adding actions

--- index.js
+++ index.js
@@ -6,4 +6,35 @@ const server = express();
 const port = 9000;
 server.use(express.json());
 
+server.get('/api/projects',(req,res) => {
+    db('projects')
+    .then(projects => res.status(200).json(projects))
+    .catch(err => res.status(500).json(err));
+})
+
+//   MVP  Endpoints
+server.post('/api/projects', (req,res) => {
+    const addedProject = req.body;
+    if(!addedProject){
+        res.status(500).send("Please provide all fields")
+    }else{
+        db('projects')
+        .insert(addedProject)
+        .then(projectID => res.status(200).json(projectID))
+        .catch(err => res.status(500).json({youdonefuckedup : err}));
+    }
+})
+
+server.post('/api/actions', (req,res) => {
+    const addedAction = req.body;
+    if(!addedAction.name || !addedAction.project_id || !addedAction.description){
+        res.status(500).send("Please provide all fields")
+    }else{
+        db('actions')
+        .insert(addedAction)
+        .then(actionID => res.status(200).json(actionID))
+        .catch(err => res.status(500).json({youdonefuckedup : err}));
+    }
+})
+
 server.listen(port,() => {console.log(`server listening on port ${port}`)})
