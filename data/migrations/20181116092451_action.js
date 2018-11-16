800c1a1a11c932df82584e9b6e8fa7b0952abf00
SavannahGreuel <smallidgesavannah@gmail.com>
2018-11-16 09:45:49 -0700

    migrations for actions

--- data/migrations/20181116092451_action.js
+++ data/migrations/20181116092451_action.js
@@ -1,8 +1,22 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.shema.createTable('actions', tbl => {
+      tbl
+      .increments()
+      
+      tbl
+      .string('description', 1000)
+      .notNullable()
+
+      tbl
+      .string('notes', 1000)
+      
+      tbl
+      .boolean('completed')
+      .defaultTo(false)
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('actions')
 };
