2c8e562c11d2fd9eaa38e152b61980a4f85d3924
AM_Myrick <amyrick.820@gmail.com>
2018-11-16 11:32:05 -0500

    creates and runs seeds

--- data/seeds/001-projects.js
+++ data/seeds/001-projects.js
@@ -1,13 +1,12 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('projects').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('projects').insert([
+        {name: "Complete Gatsby refactor of UI project", description: "A list of additional goals", completed: false},
+        {name: "Complete Pro Gatsby Course", description: "Watch videos and personalize project", completed: false},
       ]);
     });
 };
--- data/seeds/002-actions.js
+++ data/seeds/002-actions.js
@@ -1,13 +1,18 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('actions').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('actions').insert([
+        {description: "Componetize the footer", notes: "", completed: false, project_id: 1},
+        {description: "Componetize the jumbotron with conditional rendering", notes: "", completed: false, project_id: 1},
+        {description: "Add animations", notes: "", completed: false, project_id: 1},
+        {description: "Add gatsby-image", notes: "", completed: false, project_id: 1},
+        {description: "If necessary, refactor images", notes: "", completed: false, project_id: 1},
+        {description: "Watch Scott Tolinski's Pro Gatsby Series", notes: "", completed: false, project_id: 2},
+        {description: "Complete the code along blog project", notes: "", completed: false, project_id: 2},
+        {description: "Refactor if necessary", notes: "", completed: false, project_id: 2},
       ]);
     });
 };
