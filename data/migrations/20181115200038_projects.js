bd6fb9a567c4902e10d50666742e9403a421f004
Brandon Vilaychith <blvilaychith@outlook.com>
2018-11-15 20:05:05 -0600

    project table created

---
+++ data/migrations/20181115200038_projects.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('projects', table => {
+    // Creates an ID that auto increments
+    table.increments();
+
+    table
+      .string('name')
+      .notNullable()
+      .unique();
+    table.string('description');
+    table.boolean('completed');
+  });
+};
+
+exports.down = function(knex, Promise) {};
