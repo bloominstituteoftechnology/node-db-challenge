17c559df26c423a8c69d09544c3e849c5a404fc5
KHollobaugh <khollobaugh@gmail.com>
2018-11-16 12:23:49 -0500

    added post endpoints

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ server.get('/', (req, res) => {
     res.json({ api: 'server is running!'});
 })
 
+server.post('/api/projects', (req, res) => {
+    const body = req.body;
+    db('projects')
+    .insert(body)
+    .then(ids => {
+        res.status(201).json(ids);
+    })
+    .catch(error => res.status(500).json({ message: "Error posting a new project"}))
+})
+
+server.post('/api/actions', (req, res) => {
+    const body = req.body;
+    db('actions')
+    .insert(body)
+    .then(ids => {
+        res.status(201).json(ids);
+    })
+    .catch(error => res.status(500).json({ message: "Error posting new action"}))
+})
+
 const port = 4000;
 server.listen(port, function() {
     console.log(`\n API is listening on http://localhost:${port}`)
