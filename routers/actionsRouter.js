87ee2a49686147398165692ef310558fe30a2592
Steven O'Neill <oneillsteven957@gmail.com>
2018-11-16 12:29:21 -0600

    added the other endpoints for actions

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -21,4 +21,42 @@ router.post('/', (req, res) => {
       });
   });
 
+  router.get('/', (req, res) => {
+    db('actions')
+      .then(actions => res.status(200).json(actions))
+      .catch(err => res.status(500).json({ message: 'could not get actions', err }));
+  });
+  
+  router.get('/:id', (req, res) => {
+    db('actions')
+      .where({ id: req.params.id })
+      .then(actions => res.status(200).json(actions))
+      .catch(err => res.status(500).json({ message: 'could not get action', err }));
+  });
+  
+  router.put('/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+    
+    db('actions')
+      .where({ id })
+      .update(changes)
+      .then(count => {
+        res.status(200).json({ count });
+      })
+      .catch(err => res.status(500).json({ message: 'could not update action', err }));
+  });
+  
+  router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    
+    db('actions')
+      .where({ id })
+      .del()
+      .then(count => {
+        res.status(200).json({ count });
+      })
+      .catch(err => res.status(500).json({ message: 'could not delete action', err }));
+  });
+
   module.exports = router;
