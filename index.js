e14812dacca5d19245bd115b2bac710d8e5419bb
MEBonham <programmer@mckaybonham.com>
2018-11-16 10:13:34 -0700

    POST projects implemented

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ const port = 9000;
 
 server.use(express.json());
 
-
+server.post("/api/projects", (req, res) => {
+    let project = req.body;
+    if (project.name) {
+        project = {
+            description: "",
+            completed: false,
+            ...project
+        };
+        db("projects")
+            .insert(project)
+            .then(idReturned => {
+                res.status(201).json(idReturned);
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "Cannot post project to database.",
+                    error: err
+                });
+            });
+    } else {
+        res.status(400).json({ error: "Please provide a valid name of the project." });
+    }
+});
 
 server.listen(port, () => console.log(`\n== Port ${port} Running ==\n`));
