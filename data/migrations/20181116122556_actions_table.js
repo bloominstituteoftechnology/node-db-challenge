f5d8bd5d93025f757eb04887022503026838cab7
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-11-16 14:30:26 -0500

    post and get endpoints

--- data/migrations/20181116122556_actions_table.js
+++ data/migrations/20181116122556_actions_table.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("actions", function(tbl) {
+    tbl.increments();
+    tbl.string("description");
+    tbl.string("notes");
+    tbl
+      .integer("project")
+      .unsigned()
+      .references("id")
+      .inTable("actions");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("actions");
 };
--- index.js
+++ index.js
@@ -7,4 +7,60 @@ const server = express();
 const db = knex(knexConfig.development);
 server.use(express.json());
 
+//========================================================================== Sanity Check <----
+server.get("/", (req, res) => {
+  res.json({ api: "running" });
+});
+//========================================================================== Post <----
+server.post("/projects", (req, res) => {
+  const project = req.body;
+  console.log(project);
+  db("projects")
+    .insert(project)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => res.status(500).json({ message: "error creating project" }));
+});
+
+server.post("/actions", (req, res) => {
+  const action = req.body;
+  console.log(action);
+  db("actions")
+    .insert(action)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => res.status(500).json({ message: "error creating project" }));
+});
+
+//========================================================================== get <----
+server.get("/projects", (req, res) => {
+  db("projects")
+    .then(project => res.status(200).json(project))
+    .catch(err => res.status(500).json(err));
+});
+
+server.get("/actions", (req, res) => {
+  db("actions")
+    .then(action => res.status(200).json(action))
+    .catch(err => res.status(500).json(err));
+});
+
+server.get("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  db("projects")
+    .where({ id })
+    .first()
+    .then(project => {
+      db("actions")
+        .where({ project: id })
+        .then(actions => {
+          project.actions = actions;
+          res.status(200).json(project);
+        });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
 server.listen(9000, () => console.log("running on port 9000"));
