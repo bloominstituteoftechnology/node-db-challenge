176e8e4cca2ee98e0a1678163e89f35f15f30a3a
Drewsup123c <drewsup123c@gmail.com>
2018-11-16 09:41:57 -0700

    finished actions and projects table

--- data/migrations/20181116093510_actions.js
+++ data/migrations/20181116093510_actions.js
@@ -1,18 +1,21 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('projects',function(tbl){
+    return knex.schema.createTable('actions',function(tbl){
         //primary key (Id)
         tbl.increments();
         //name
         tbl.string('name', 128).unique();
         //description
         tbl.string('description',255);
+        //Notes
+        tbl.string('notes').nullable()
         //completed bool
         tbl.boolean('is_completed');
+        //Project key / foriegn key
+        tbl.integer('project_id').unsigned().references('id').inTable('projects');
         tbl.timestamps(true,true);
     })
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('projects')
+    return knex.schema.dropTableIfExists('actions')
 };
-
