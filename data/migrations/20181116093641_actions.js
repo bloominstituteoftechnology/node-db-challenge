83489fb76c54a2f97c7ca3bbff40a0e3acfcce69
Brad Mortensen <b.mort86@gmail.com>
2018-11-16 09:46:57 -0700

    Creates tables

--- data/migrations/20181116093641_actions.js
+++ data/migrations/20181116093641_actions.js
@@ -1,17 +1,17 @@
 
 exports.up = function (knex, Promise) {
   //makes changes to the database
-  return knex.schema.createTable('projects', function (tbl) {
+  return knex.schema.createTable('actions', function (tbl) {
     //primary key
     tbl.increments();
     //other fields
-    tbl.string('name', 120);
     tbl.string('description', 240);
-    tbl.boolean('completed').defaultTo(false)
-    tbl.timestamps(true, true);
+    tbl.string('notes', 120);
+    tbl.boolean('completed').defaultTo(false);
+    tbl.text('projectId').references('projects.id')
   })
 };
 
 exports.down = function (knex, Promise) {
-  return knex.schema.dropTableIfExists('projects');
+  return knex.schema.dropTableIfExists('actions');
 };
