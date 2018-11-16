16885f75c18d9237fcd14f9a3ae2fff3f4bfc868
jsmnglng <jsmn.glng@gmail.com>
2018-11-16 08:22:58 -0800

    Added migrations

---
+++ data/migrations/20181116081948_create_project_tbl.js
@@ -0,0 +1,12 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('project', function (table) {
+    table.increments();
+    table.string('name', 128).notNullable();
+    table.string('description').notNullable();
+    table.boolean('completed');
+  });
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists('project');
+};
---
+++ data/migrations/20181116082054_create_action_tbl.js
@@ -0,0 +1,18 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('action', function (table) {
+    table.increments();
+    table.string('description');
+    table.string('notes');
+    table.boolean('completed');
+    table
+      .integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('project');
+  });
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists('action');
+};
