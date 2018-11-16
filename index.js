10fb3053c7bc77e98bcfe7915c2fede226bccf79
sean-one <basement365@gmail.com>
2018-11-16 09:11:22 -0800

    created get route for all projects

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const server = express();
 
 server.use(express.json());
 
-
+server.get('/api/projects', async (req, res) => {
+    try {
+        const projects = await db('projects');
+        res.status(200).json(projects)
+    } catch (error) {
+        res.status(500).json({ errorMessage: 'something went wrong' })
+    }
+})
 
 const port = 8250
 server.listen(port, console.log(`\n=== we are listening... on ${port} ===\n`));
