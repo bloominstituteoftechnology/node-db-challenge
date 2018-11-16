7956a57b419ce61dddcc02bb93a0bccbcd2d50c1
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-11-16 11:23:20 -0800

    display project by id w/ actions

--- routes/projectsRoute.js
+++ routes/projectsRoute.js
@@ -13,7 +13,30 @@ router.post("/", (req, res) => {
 });
 
 router.get("/", (req, res) => {
-  db("projects").then(projects => res.status(200).json(projects));
+  db("projects")
+    .then(projects => res.status(200).json(projects))
+    .catch(err => res.status(500).json(err));
+});
+
+router.get("/:id", async (req, res) => {
+  const {id} = req.params;
+
+  const query1 = await db("projects").where({id});
+  const query2 = await db("actions as a").where("a.project_id", id);
+  //   console.log("query 1", query1);
+  //   console.log("query 2", query2);
+
+  const response = {
+    ...query1[0],
+    actions: query2
+  };
+
+  //   console.log(response);
+
+  db("projects")
+    .where({id})
+    .then(projects => res.status(200).json(response))
+    .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
