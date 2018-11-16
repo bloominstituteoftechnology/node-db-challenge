e238b8b1295c6f43c4e95258819499b46befaa40
Gannon <gkleblanc@gmail.com>
2018-11-16 11:39:35 -0500

    added migrations and code to index.js

---
+++ data/migrations/20181116113651_actions_table.js
@@ -0,0 +1,26 @@
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('actions', function (actions) {
+        actions
+            .increments()
+            .primary()
+
+        actions
+            .integer('projectId')
+            .references('projects.id');
+
+        actions
+            .text('name')
+            .notNullable();
+
+        actions
+            .text('description')
+            .notNullable();
+
+        actions
+            .boolean('completed')
+            .defaultTo(false);
+    })
+};
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('actions');
+};
---
+++ index.js
@@ -0,0 +1,22 @@
+//dependecies
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const server = express();
+
+//setting up knex
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+//routes
+
+//calling server and routes
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+
+//port
+const port = 7000
+server.listen(port, function () {
+    console.log('Loud and Clear')
+})
