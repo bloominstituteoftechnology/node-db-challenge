04a656ab8944540af1eb49aad348bada62aaac1a
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-11-16 11:52:24 -0700

    project actions

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ server.post('/actions', (req, res) => {
     db('actions')
         .insert(action)
         .then(id => {
-            res.status(201).json(id);   //id????
+            res.status(201).json(id);   
         })
         .catch(err => {
             res.status(500).json({ message: 'Error Inserting' });
@@ -34,9 +34,60 @@ server.get('/projects/:id', (req, res) => {
     const { id } = req.params;
     db('projects')
         .where({ id })
+        .first('id', 'name', 'description', 'completed')
         .then(project => {
-            
+            db('actions')
+                .where({ project_id: id })
+                .then(actions => {
+                    project.actions = actions;
+                    res.status(200).json(project);
+                })
+                .catch(err => {
+                    res.status(500).json({ message: 'Error getting project' });
+                })
         })
-})
+        .catch(err => {
+            res.status(500).json({ message: 'Error getting project' });
+        })
+});
+
+server.get('/projects', (req, res) => {
+    db('projects')
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Error getting project'});
+        })
+});
+
+server.delete('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    db('projects')
+        .where({ id })
+        .del()
+        .then(count => {
+            res.status(200).json(count);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+server.put('projects/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    db('projects')
+        .where({ id })
+        .update(changes)
+        .then(count => {
+            res.status(200).json(count);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+
 
 server.listen(8000, () => console.log('Server is listening on port 8000'));
