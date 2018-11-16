2edbd28eeb54ac7130796cae39bdd121e459f1b5
Matthew Wright <matthewbwright@gmail.com>
2018-11-16 10:29:14 -0700

    adds POST for projects

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express');
-const knex = require('knex');
-const knexConfig = require('./knexfile.js');
-const db = knex(knexConfig.development);
 const server = express();
 
 server.use(express.json());
 
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
+
 //sanity check
 server.get('/', (req, res) => {
   res.json({ api: 'are you ready to be productive?' });
+});
+
+//POST project
+server.post('/api/projects', (req, res) => {
+  const project = req.body;
+  db('projects')
+    .insert(project)
+    .returning('id')
+    .then(id => res.status(201).json(id))
+    .catch(err =>
+      res
+        .status(400)
+        .json({ message: 'Your project could not be added.', error: err })
+    );
 });
 
 const port = 7000;
