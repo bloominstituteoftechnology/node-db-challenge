beeff6153c91b038524529d2cdd64ed45b2a93ca
Michael Checo <checomichael2@gmail.com>
2018-11-16 10:05:39 -0500

    dependencies, root file, and knex configurations

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const knex = require('knex');
+
+const server = express();
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development)
+
+server.use(express.json())
+
+
+
+
+const port = 8686;
+server.listen(port, () => console.log(`\n==^_^== Listening on http://localhost:${port} ==^_^==\n`))
