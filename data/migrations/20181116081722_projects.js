aae9077959af4ec6804c474797f86ce0f40c70a1
kyranmccann <kyran.mccann@gmail.com>
2018-11-16 09:48:45 -0800

    seeds and migrations

--- data/migrations/20181116081722_projects.js
+++ data/migrations/20181116081722_projects.js
@@ -2,7 +2,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('projects', function(projects) {
     projects.increments().primary();
-
     projects.text('name').notNullable();
     projects.text('description').notNullable();
     projects.boolean('completed').defaultTo(false);
---
+++ data/seeds/01_projects.js
@@ -0,0 +1,12 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('projects').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('projects').insert([
+        {name: 'APOD API front end', description: 'Create a front end for the APOD API'},
+        {name: 'GraphQL Practice', description: 'Follow the tutorial for a GraphQL project'},
+        {name: 'SacHacks Prep', description: 'Prepare for the hackathon'}
+      ]);
+    });
+};
