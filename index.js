0aa978ec63fe42855912a429207b0dbc800047d4
Brandon Strellis <bstrellis@gmail.com>
2018-11-16 12:30:55 -0500

    MVP endpoints (POST for actions and projects, GET for project by id) working.

--- index.js
+++ index.js
@@ -12,7 +12,54 @@ server.get('/', (req, res) => {
 });
 
 server.post('/api/projects', async (req, res) => {
-    
+    const newProject = req.body;
+    try {
+        const returned = await db('projects').insert(newProject);
+        res.status(200).json(returned);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+server.post('/api/actions', async (req, res) => {
+    const newAction = req.body;
+    try {
+        const returned = await db('actions').insert(newAction);
+        res.status(200).json(returned);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+server.get('/api/projects', async (req, res) => {
+    try {
+        const projects = await db('projects');
+        res.status(200).json(projects);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+server.get('/api/actions', async (req, res) => {
+    try {
+        const actions = await db('actions');
+        res.status(200).json(actions);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+server.get('/api/projects/:id', async (req, res) => {
+    const project_id = req.params.id;
+    try {
+        const projectArr = await db('projects').where({ id: project_id });
+        const project = projectArr[0];
+        const actionsForProj = await db('actions').where({ project_id: project_id });
+        project.actions = actionsForProj;
+        res.status(200).json(project);
+    } catch(err) {
+        res.status(500).json(err);
+    }
 });
 
 const port = 9000;
