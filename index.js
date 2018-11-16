ef12973da15c471a41f9af1e115295676041a23f
Yanrong Xu <xuyanrong@icloud.com>
2018-11-16 11:44:20 -0500

    added listening port

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ const server = express()
 
 server.use(express.json())
 server.use(helmet()) 
+
+const port = 9000;
+server.listen(port, function(){
+    console.log(`\n===APP listening on port ${port} ===\n`)
+})
