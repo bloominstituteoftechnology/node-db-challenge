6d889d296f1860eef391410b5308fb532b2aac89
Yanrong Xu <xuyanrong@icloud.com>
2018-11-16 12:00:52 -0500

    able to get all project

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ server.get('/', (req, res) => {
     res.send('WOW it is working')
 });
 
+// get all project
+server.get('/api/projects', (req, res) => {
+    db('project')
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => {
+            res.status(500).json({message: err})
+        })
+});
+
 const port = 9000;
 server.listen(port, function(){
     console.log(`\n===APP listening on port ${port} ===\n`)
