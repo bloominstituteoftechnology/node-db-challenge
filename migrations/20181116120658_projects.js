7b58c9cebc963ef9a9a3ccd9bbdb6ac4d8c94d62
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-16 12:23:47 -0500

    created projects and actions tables

--- migrations/20181116120658_projects.js
+++ migrations/20181116120658_projects.js
@@ -1,8 +1,12 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('projects', tbl => {
+        tbl.increments();
+        tbl.string('name', 128).notNullable();
+        tbl.text('description').notNullable();
+        tbl.boolean('completed').defaultTo(false);
+    });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('projects');
 };
--- migrations/20181116120717_actions.js
+++ migrations/20181116120717_actions.js
@@ -1,8 +1,19 @@
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('actions', tbl => {
+        tbl.increments();
+        tbl.string('description', 128).notNullable();
+        tbl.text('notes').notNullable();
+        tbl.boolean('completed').defaultTo(false);
 
-exports.up = function(knex, Promise) {
-  
+        tbl
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects');
+    });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('actions');
 };
