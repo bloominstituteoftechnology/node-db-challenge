8a4bdd0d186d9ffde1bd51c2776354f2db9be74b
Elvis Ibarra <ielvisd@gmail.com>
2018-11-16 09:23:22 -0800

    Server is running

---
+++ index.js
@@ -0,0 +1,21 @@
+const server = require('./api/server.js');
+// const projectsRouter = require('./projects/projectsRouter.js');
+// const actionsRouter = require('./actions/actionsRouter');
+
+const port = 9000;
+server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
+
+// //PROJECTS METHODS
+// server.use('/api/projects/', projectsRouter);
+// server.get('/api/projects/:id', projectsRouter);
+// server.get('/api/projects/actions/:id', projectsRouter);
+// server.post('/api/projects', projectsRouter);
+// server.delete('/api/projects/:id', projectsRouter);
+// server.put('/api/projects/:id', projectsRouter);
+
+// // ACTIONS METHODS
+// server.use('/api/actions/', actionsRouter);
+// server.get('/api/actions/:id', actionsRouter);
+// server.post('/api/actions/', actionsRouter);
+// server.delete('/api/actions/:id', actionsRouter);
+// server.put('/api/actions/:id', actionsRouter);
