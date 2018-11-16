35ec8d2376b00c08e689d1b8079a1b622e58e023
KHollobaugh <khollobaugh@gmail.com>
2018-11-16 11:38:55 -0500

    server is running

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+server.get('/', (req, res) => {
+    res.json({ api: 'server is running!'});
+})
+
+const port = 4000;
+server.listen(port, function() {
+    console.log(`\n API is listening on http://localhost:${port}`)
+})
