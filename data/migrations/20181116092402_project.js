95efa6697fa7763d1c4d645393ff775d2b207427
Yusuf Nafey <yusufnafey@yahoo.com>
2018-11-16 12:44:12 -0800

    updated

--- data/migrations/20181116092402_project.js
+++ data/migrations/20181116092402_project.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('project', table => {
-        table.increments()
+        table.increments('id')
         table.string('name', 128)
         table.string('description', 500)
         table.boolean('completed')
--- data/migrations/20181116092409_action.js
+++ data/migrations/20181116092409_action.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
-};
+    return knex.schema.createTable('action', table => {
+        table.increments('id')
+        table.string('description', 128)
+        table.string('notes', 500)
+        table.boolean('completed')
+        table.integer('projectID').unsigned().references('id').inTable('project')
+    }) 
+}
 
 exports.down = function(knex, Promise) {
-  
-};
+    return knex.schema.dropIfTableExists('action')
+}
