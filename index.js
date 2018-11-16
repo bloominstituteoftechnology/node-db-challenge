71b7f66d3d739c0eeddb6f5b90e861cac176b4dc
Matthew Wright <matthewbwright@gmail.com>
2018-11-16 10:42:18 -0700

    adds POST for actions, adds GET project by id

--- index.js
+++ index.js
@@ -26,6 +26,43 @@ server.post('/api/projects', (req, res) => {
     );
 });
 
+//POST actions
+server.post('/api/actions', (req, res) => {
+  const action = req.body;
+  db('actions')
+    .insert(action)
+    .returning('id')
+    .then(id => res.status(201).json(id))
+    .catch(err =>
+      res
+        .status(400)
+        .json({ message: 'Your project could not be added.', error: err })
+    );
+});
+
+//Get project by id
+server.get('/api/projects/:id', (req, res) => {
+  const { id } = req.params;
+  db('projects')
+    .where({ id })
+    .then(project => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({
+          message: 'The project with the specified ID does not exist.',
+          error: err
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'The cohort information could not be retrieved.',
+        error: err
+      });
+    });
+});
+
 const port = 7000;
 server.listen(port, function() {
   console.log(`\n=== Web API listening on http://localhost:${port} ===\n`);
