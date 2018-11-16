2929da1592351f182182aee2902040d1ebe920ea
MEBonham <programmer@mckaybonham.com>
2018-11-16 09:42:36 -0700

    Created index.js with boiler-plate stuff

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const knex = require("knex");
+
+const knexConfig = require("./knexfile.js");
+
+const db = knex(knexConfig.development);
+
+const server = express();
+
+const port = 9000;
+
+server.use(express.json());
+
+
+
+server.listen(port, () => console.log(`\n== Port ${port} Running ==\n`));
