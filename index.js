0a40ea906b2a2b3c01ca33ec2fd765eae5ba6282
Yanrong Xu <xuyanrong@icloud.com>
2018-11-16 15:38:07 -0500

    fix the problem that do not display the action with project

--- index.js
+++ index.js
@@ -46,10 +46,10 @@ server.post('/api/projects', (req, res) => {
 // get all action by project
 server.get('/api/projects/:id/actions', (req, res) => {
     const { id } = req.params;
-    db('action')
-        .where({project_id: id})
-        .then(actions => {
-            res.status(200).json(actions)
+    db('project')
+        .where({id})
+        .then(project => {
+            res.status(200).json(project)
         })
         .catch(err => {
             res.status(500).json({message: err})
@@ -59,11 +59,17 @@ server.get('/api/projects/:id/actions', (req, res) => {
 // get project by id
 server.get('/api/projects/:id', (req, res) => {
     const { id } = req.params;
-    db('project')
+    return db('project')
         .where({id})
+        .first()
         .then(project => {
             if (project) {
-                res.status(200).json(project)
+                db('action')
+                    .where({project_id: id})
+                    .then(action => {
+                        project.action = action
+                        res.status(200).json(project)
+                    })
             } else {
                 res.status(404).json({message: 'Project non found'})
             }
