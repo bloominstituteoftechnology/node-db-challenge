ef3c9a3af3d7e974efb1d7d2b2014d8d504e8261
Stublink <chrisrey95@gmail.com>
2018-11-16 11:39:38 -0500

    :sparkels: projects and actions migrations

--- data/migrations/20181116112016_projects.js
+++ data/migrations/20181116112016_projects.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('projects', tbl => {
+    tbl.increments();
+    tbl.string('name', 256);
+    tbl.string('description', 256);
+    tbl.boolean('completed', true);
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('projects');
 };
--- data/migrations/20181116112049_actions.js
+++ data/migrations/20181116112049_actions.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('actions', tbl => {
+      tbl.increments();
+      tbl.string('description', 256);
+      tbl.string('note', 256);
+      tbl.boolean('completed', false);
+
+      tbl.integer('project_id')
+        .unsigned()
+        .references('id')
+        .inTable('projects');
+    
+
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('actions');
 };
