81fd08b1debf076840de106b828f410f8ee718f4
Brandon Strellis <bstrellis@gmail.com>
2018-11-16 13:01:17 -0500

    Refactored json of POST endpoints for user-friendliness. Refactored GET by project id to screen for valid id.

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ server.use(express.json());
 
 const db = knex(knexConfig.development);
 
+// ROOT ENDPOINT
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'At root' });
 });
 
+// API ENDPOINTS
 server.post('/api/projects', async (req, res) => {
     const newProject = req.body;
     try {
-        const returned = await db('projects').insert(newProject);
-        res.status(200).json(returned);
+        const returnedArr = await db('projects').insert(newProject);
+        res.status(200).json({ project_id: returnedArr[0]});
     } catch(err) {
         res.status(500).json(err);
     }
@@ -24,8 +26,8 @@ server.post('/api/projects', async (req, res) => {
 server.post('/api/actions', async (req, res) => {
     const newAction = req.body;
     try {
-        const returned = await db('actions').insert(newAction);
-        res.status(200).json(returned);
+        const returnedArr = await db('actions').insert(newAction);
+        res.status(200).json({ action_id: returnedArr[0]});
     } catch(err) {
         res.status(500).json(err);
     }
@@ -53,14 +55,19 @@ server.get('/api/projects/:id', async (req, res) => {
     const project_id = req.params.id;
     try {
         const projectArr = await db('projects').where({ id: project_id });
-        const project = projectArr[0];
-        const actionsForProj = await db('actions').where({ project_id: project_id });
-        project.actions = actionsForProj;
-        res.status(200).json(project);
+        if (projectArr.length !== 0) {
+            const project = projectArr[0];
+            const actionsForProj = await db('actions').where({ project_id: project_id });
+            project.actions = actionsForProj;
+            res.status(200).json(project);
+        } else {
+            res.status(404).json({ message: 'Invalid project id'});
+        }
     } catch(err) {
         res.status(500).json(err);
     }
 });
 
+// SERVER SETTINGS
 const port = 9000;
 server.listen(port, () => console.log(`\nServer up on port ${port}\n`));
