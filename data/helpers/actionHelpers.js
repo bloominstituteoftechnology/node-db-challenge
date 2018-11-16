525fae017153d9ce8362b8d836e42528f6a1e274
kyranmccann <kyran.mccann@gmail.com>
2018-11-16 08:39:27 -0800

    setting up helpers

---
+++ data/helpers/actionHelpers.js
@@ -0,0 +1,25 @@
+const knex = require('knex');
+const knexConfig = require('../../knexfile.js');
+const db = knex(knexConfig.development);
+const mappers = require('./mappers');
+
+module.exports = {
+  get: function(id) {
+
+    if (id) {
+      return db('actions')
+        .where('id', id)
+        .first()
+        .then(action => mappers.actionToBody(action));
+    }
+
+    return db('actions').then(actions => {
+      return actions.map(action => mappers.actionToBody(action));
+    });
+  },
+  insert: function(action) {
+    return db('actions')
+      .insert(action)
+      .then(([id]) => this.get(id));
+  },
+};
