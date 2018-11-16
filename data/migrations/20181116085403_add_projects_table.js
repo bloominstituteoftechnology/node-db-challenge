868935f7d4c68b3f9324febbe7d0f2354c1dfe6d
Elvis Ibarra <ielvisd@gmail.com>
2018-11-16 09:02:06 -0800

    Projects table migrated

---
+++ data/migrations/20181116085403_add_projects_table.js
@@ -0,0 +1,24 @@
+exports.up = function(knex, Promise) {
+  // makes the changes to the database
+  return knex.schema.createTable('projects', function(tbl) {
+    // make changes to the table using the tbl object passed as a parameter
+
+    // primary key
+    tbl.increments(); // generate an id field and make it auto-increment and the primary key
+
+    // other fields
+    tbl
+      .string('name', 128)
+      .notNullable()
+      .unique();
+    tbl.text('description').notNullable();
+    tbl.boolean('completed').defaultTo(false);
+
+    tbl.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  // undo the changes to the database (it's called rolling back changes)
+  return knex.schema.dropTableIfExists('projects');
+};
