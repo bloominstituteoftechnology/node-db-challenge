bab33aebed0cf9d034179d98de08c916240dc35c
Matthew Wright <matthewbwright@gmail.com>
2018-11-16 09:51:45 -0700

    adds migrations

--- data/migrations/20181116094031_projects.js
+++ data/migrations/20181116094031_projects.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('projects', tbl => {
+    tbl.increments();
+    tbl
+      .string('name')
+      .notNullable()
+      .unique();
+    tbl.string('description').notNullable();
+    tbl.boolean('complete');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTablieIfExists('projects');
 };
--- data/migrations/20181116094050_actions.js
+++ data/migrations/20181116094050_actions.js
@@ -1,8 +1,17 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('actions', tbl => {
+    tbl.increments();
+    tbl.string('action description');
+    tbl.string('action notes');
+    tbl.boolean('complete');
+    tbl
+      .integer('project_id')
+      .unsigned()
+      .references('id')
+      .inTable('projects');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('actions');
 };
