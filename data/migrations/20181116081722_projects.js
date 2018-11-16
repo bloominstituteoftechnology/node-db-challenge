3303c9d0e388ef7ccc4bba498ea566ecaf3766df
kyranmccann <kyran.mccann@gmail.com>
2018-11-16 08:19:47 -0800

    projects migration

---
+++ data/migrations/20181116081722_projects.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('projects', function(projects) {
+    projects.increments().primary();
+
+    projects.text('name').notNullable();
+    projects.text('description').notNullable();
+    projects.boolean('completed').defaultTo(false);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('projects')
+};
