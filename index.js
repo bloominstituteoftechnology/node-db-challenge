898fd78ac73750f9264a92b2af07c174fdd6b0bd
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-11-16 09:10:47 -0800

    server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const server = express();
+const PORT = 9000;
+
+// const projectsRoute = require("/routes/projectsRoute");
+// const actionsRoute = require("/routes/actionsRoute");
+
+server.use(express.json());
+// server.use("api/projects", projectsRoute);
+// server.use("api/actions", actionsRoute);
+
+server.listen(PORT, () => console.log(`\n== PORT ${PORT} is Def Live ==\n`));
