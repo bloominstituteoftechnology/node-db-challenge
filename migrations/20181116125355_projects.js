ae6dae26f9b3d586dcbd2e5a019404f0e8aebcce
unknown <quan.nguyen199712@gmail.com>
2018-11-16 12:57:39 -0500

    initial commit

---
+++ migrations/20181116125355_projects.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('projects', projects=>{
+      projects.increments();
+      projects.string('name',255).notNullable();
+      projects.string('description', 500).notNullable();
+      projects.boolean('completed');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  
+};
