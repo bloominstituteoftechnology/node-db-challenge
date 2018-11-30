bbe1c6ba5008a87c78a2f1c2d4499571170c52d5
mikaelacurrier <mikaela.currier@gmail.com>
2018-11-30 12:01:12 -0500

    added helmet and morgan middleware

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const morgan = require('morgan');
+const helmet = require('helmet');
+
 const projectRoutes = require('./routes/projectRoutes.js');
 const actionRouters = require('./routes/actionRoutes');
 
 const server = express();
 
+//Middleware
 server.use(express.json());
+server.use(morgan('dev'));
+server.use(helmet());
 
 // Endpoints
 server.use('/api/projects', projectRoutes);
