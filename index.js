80d63e7820d7803c1aa788849c343927cd73881e
Jake Thomas <jmaxt12@gmail.com>
2018-11-30 10:10:38 -0700

    Posts and Gets work on both actions and projects

--- index.js
+++ index.js
@@ -9,6 +9,95 @@ const server = express();
 
 server.use(express.json());
 
+//------------PROJECT----------------
+
+// GET
+
+server.get("/projects", (req, res) => {
+    db("projects")
+      .then(projects => {
+          res.status(200).json(projects)
+      })
+      .catch(err => {
+          res.status(500).json(err)
+      });
+  });
+
+  // GET BY ID
+
+server.get("/projects/:id", (req, res) => {
+    const { id } = req.params;
+    db("projects")
+      .where({ id: id})
+      .then(projects => {
+          res.status(200).json(projects)
+      })
+      .catch(err => {
+          res.status(500).json(err)
+      });
+  });
+
+// POST
+
+server.post("/projects", (req, res) => {
+    const projects = req.body;
+
+    db("projects")
+      .insert(projects)
+      .into("projects")
+      .then(ids => {
+          res.status(201).json({ids})
+      })
+      .catch(err => {
+          res.status(500).json({ error: err })
+      });
+  });
+
+//------------ACTIONS----------------
+
+// GET
+
+server.get("/actions", (req, res) => {
+    db("actions")
+      .then(actions => {
+          res.status(200).json(actions)
+      })
+      .catch(err => {
+          res.status(500).json(err)
+      });
+  });
+
+  // GET BY ID
+
+server.get("/actions/:id", (req, res) => {
+    const { id } = req.params;
+    db("actions")
+      .where({ id: id})
+      .then(actions => {
+          res.status(200).json(actions)
+      })
+      .catch(err => {
+          res.status(500).json(err)
+      });
+  });
+
+// POST
+
+server.post("/actions", (req, res) => {
+    const actions = req.body;
+
+    db("actions")
+      .insert(actions)
+      .then(ids => {
+          res.status(201).json(ids)
+      })
+      .catch(err => {
+          res.status(500).json({ error: err })
+      });
+  });
+
+
+
 
 
 
