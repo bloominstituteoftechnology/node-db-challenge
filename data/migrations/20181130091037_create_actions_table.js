ca21f270738691504786158d259bc52c36263e1b
Phil Hart <phil@Phils-iMac.local>
2018-11-30 09:23:37 -0700

    First migration built

---
+++ data/migrations/20181130091037_create_actions_table.js
@@ -0,0 +1,19 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("actions", tbl => {
+      tbl.increments();
+      tbl.string("description", 120).notNullable();
+      tbl.string("notes", 1000).notNullable();
+      tbl.boolean("completed").defaultTo(false);
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects");
+     
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("actions");
+  };
