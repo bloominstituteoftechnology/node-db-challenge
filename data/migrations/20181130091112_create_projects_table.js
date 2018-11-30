58b5781792af5e3a246ce8a8d07176ccb2ce2018
Phil Hart <phil@Phils-iMac.local>
2018-11-30 09:27:25 -0700

    Second migration built

--- data/migrations/20181130091112_create_projects_table.js
+++ data/migrations/20181130091112_create_projects_table.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable("projects", tbl => {
+      tbl.increments();
+      tbl.string("name", 120).notNullable();
+      tbl.string("description", 300).notNullable();
+      tbl.boolean("completed").defaultTo(false);
+    });
+  };
   
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("projects");
+  };
