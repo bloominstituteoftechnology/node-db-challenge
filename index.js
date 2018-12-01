134e0c8c74d74f4466518db7f7fd2dfca3503390
Orlando Castillo <omaddoc@gmail.com>
2018-12-01 17:43:42 -0500

    finished

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const server = express();
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'running' });
+});
+
+server.post('/api/projects', (req, res) => {
+  const project = req.body;
+  db('projects')
+    .insert(project)
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'error with your insert.',
+        err
+      });
+    });
+});
+
+server.post('/api/actions', (req, res) => {
+  const action = req.body;
+  db('actions')
+    .insert(action)
+    .into('actions')
+    .then(action => {
+      res.status(201).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'There was an error', err });
+    });
+});
+
+server.get('/api/projects', (req, res) => {
+  db('projects')
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'error', err });
+    });
+});
+
+server.get('/api/projects/:id', (req, res) => {
+  const { id } = req.params;
+  db('projects')
+    .where({ id })
+    .first()
+    .then(project => {
+      db('actions')
+        .where({ pro_id: id })
+        .then(actions => {
+          project.actions = actions;
+          res.status(200).json(project);
+        });
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'error', err });
+    });
+});
+
+server.listen(9000, () => console.log('server is live'));
---
+++ migrations/20181201100024_actions.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('actions', function(table) {
+    table.increments();
+    table.string('description', 255).notNullable();
+    table.string('notes').notNullable();
+    table.boolean('completed').defaultTo(false);
+    table
+      .integer('pro_id')
+      .unsigned()
+      .references('id')
+      .inTable('projects');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('actions');
+};
