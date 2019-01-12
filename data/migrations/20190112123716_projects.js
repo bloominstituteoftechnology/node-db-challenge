66f9691b7750980e928d32504c90fe0a25eea15d
Jennifer King <jenbellpgh@gmail.com>
2019-01-12 12:51:11 -0500

    Schemas completed for 'projects' and 'actions'

--- data/migrations/20190112123716_projects.js
+++ data/migrations/20190112123716_projects.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('projects', table => {
+    table.increments();
+    table.string('name').notNullable();
+    table.text('desc');
+    table.boolean('completed').notNullable().defaultTo(0);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('projects');
 };
--- data/migrations/20190112123733_actions.js
+++ data/migrations/20190112123733_actions.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('actions', table => {
+    table.increments();
+    table.text('desc').notNullable();
+    table.text('notes');
+    table.boolean('completed').notNullable().defaultTo(0);
+    table.integer('proj_id').unsigned();
+    table.foreign('proj_id').references('id').on('projects');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('actions');
 };
