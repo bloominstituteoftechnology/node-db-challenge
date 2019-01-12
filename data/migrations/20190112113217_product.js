69a3e322d18c0e5842cf53aaac70ae1bc73b3955
BrendonKootz <brendonkootz@gmail.com>
2019-01-12 11:58:42 -0600

    Migrations Built

--- data/migrations/20190112113217_product.js
+++ data/migrations/20190112113217_product.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+    // Product schema
+    return knex.schema.createTable('product', table => {
+        table.increments()
+        table.string('name');
+        table.string('description');
+        table.boolean('product_finished');
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('product');
 };
--- data/migrations/20190112113251_actions.js
+++ data/migrations/20190112113251_actions.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+    // Actions schema
+    return knex.schema.createTable('actions', table => {
+        table.increments();
+        table.string('action_desc');
+        table.string('action_notes');
+        table.boolean('action_complete');
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('actions');
 };
