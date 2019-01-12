a35f0157f54aa7c375e192c7fa8d3a11b5b1456b
Kendra Williams <kmonique.williams@gmail.com>
2019-01-12 13:07:39 -0500

    helper methods created, project endpoints added to index.js

--- data/migrations/20190112121528_projects_table.js
+++ data/migrations/20190112121528_projects_table.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("projects", table => {
       table.increments();
       table.string("project_name").notNullable();
-      table.string("decription").notNullable();
+      table.string("description").notNullable();
       table.boolean("completed");
   })
 };
--- index.js
+++ index.js
@@ -5,8 +5,35 @@ const dbConfig = require("./knexfile.js");
 const server = express();
 
 const db = knex(dbConfig.development);
+const helper = require("./helper.js");
+
 const PORT = 6000;
+
+//add project
+server.post("/api/projects", (req, res) => {
+    const project = req.body;
+    project.project_name && project.description
+    ? helper.addProject(project)
+        .then(projectId => res.status(201).json(projectId))
+        .catch(err => res.status(401).json({err: "error adding project"}))
+    : res.status(400).json({err: "please provide project_name and description"}) 
+});
+
+//add action
+server.post("/api/actions", (req, res) => {
+    
+});
+
+//get project by id
+server.get("/api/project/:id", (req, res)=> {
+    const {id} = req.params;
+    helper.getProject(id)
+        .then(project => {
+            project.length > 0 ? res.json(project) : res.status(400).json({err: `the project with an id of ${id} does not exist`})
+        })
+        .catch(err => res.status(500).json({err: "error retreiving project"}));
+})
 
 server.listen(PORT, () => {
     `server is up and running on ${PORT}`
-})
+});
