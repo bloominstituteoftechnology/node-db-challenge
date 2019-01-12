d8d5f7cecf9b518e5349851d73095678bdb5d188
Colin Dismuke <colin.dismuke@gmail.com>
2019-01-12 12:05:54 -0600

    Added GET and POST endpoints for projects and actions

--- index.js
+++ index.js
@@ -16,6 +16,57 @@ server.get("/", (req, res) => {
   res.send(" ✅ Getting Things Done™️ ✅ ");
 });
 
+// Project endpoints
+server.get("/api/projects", (req, res) => {
+  helper
+    .getProjects()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.post("/api/projects", (req, res) => {
+  const newProject = req.body;
+
+  helper
+    .addProject(newProject)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(err => {
+      console.log(newProject);
+      res.status(500).json({ message: "Could not add new project." });
+    });
+});
+
+// Action endpoints
+server.get("/api/actions", (req, res) => {
+  helper
+    .getActions()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.post("/api/actions", (req, res) => {
+  const newAction = req.body;
+
+  helper
+    .addAction(newAction)
+    .then(action => {
+      res.status(201).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Could not add new action." });
+    });
+});
+
 server.listen(PORT, () => {
   console.log(`\n===== API Listening on http://localhost:${PORT} =====\n`);
   console.log(`===== Updated on ${date} =====\n`);
