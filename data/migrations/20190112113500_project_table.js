f8b97d4d875e5b91e595183207c6f681275f94e9
Denise Escobar <descobar1490@gmail.com>
2019-01-12 12:08:13 -0600

    Added the migration for the project table

---
+++ data/migrations/20190112113500_project_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('project', table =>{
+        table.increments('id');
+        table.string('name').notNullable();
+        table.string('description').notNullable();
+        table.boolean('finished').notNullable();
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('boxes');
+};
