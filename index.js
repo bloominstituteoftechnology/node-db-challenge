442394842be24e5f94e2f302b1783f472097f5ff
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-01-12 15:55:32 -0500

    made changes to the index for readability

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const db = require('./data/dbconfig');
 const cors = require('cors');
+const projectDB = require('./data/helpers/projectModel')
+const actionDB = require('./data/helpers/actionModel')
 
 const server = express()
 const PORT = process.env.PORT || 6050;
@@ -14,7 +15,7 @@ server.use(
 server.post('/api/projects', (req, res) => {
   const newProject = req.body;
 
-  return db('projects').insert(newProject)
+    projectDB.post(newProject)
     .then( project => {
       res.status(201).json({message: `succesfully created project`})
     }).catch( err => {
@@ -24,13 +25,36 @@ server.post('/api/projects', (req, res) => {
 
 server.post('/api/projects/:id/actions', (req, res) => {
   const { id } = req.params;
-  const newAction = req.body;
+  const action = req.body;
+  const newAction = {project_id: id, ...action}
 
-  return db('actions').insert({project_id: id, ...newAction})
+    actionDB.postAction(newAction)
     .then(action => {
       res.status(201).json({message: 'sucessfully created a new action for project'})
     }).catch( err => {
       res.status(500).json({message: 'unable to create action'})
+    })
+})
+
+server.get('/api/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  projectDB.get(id)
+  .then( project => {
+      res.status(200).json(project)
+  }).catch( err => {
+    res.status(400).json({message: "cannot get project, please try again"})
+  })
+})
+
+server.get('/api/actions/:id', (req, res) => {
+  const { id } = req.params;
+
+  projectDB.getProjectActions(id)
+    .then( actions => {
+      res.status(200).json(actions)
+    }).catch( err => {
+      res.json({message: 'unable to get the actions for this project'})
     })
 })
 
