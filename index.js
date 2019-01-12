d686f7c36e858c76268fb3e4f56118515fa3a033
Andres Rivera <andres.rivera.toro@gmail.com>
2019-01-12 12:59:16 -0500

    Add POST method and endpoint for projects and actions

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ const db = knex(dbConfig.development);
 
 server.use(express.json());
 
+server.post('/api/projects', (req, res) => {
+  const project = req.body;
+  db('projects')
+    .insert(project)
+    .then((id) => {
+      res.status(201).json(id);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Failed to insert project' });
+    });
+});
+
+server.post('/api/actions', (req, res) => {
+  const action = req.body;
+  db('actions')
+    .insert(action)
+    .then((id) => {
+      res.status(201).json(id);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Failed to insert action' });
+    });
+});
+
 server.listen(port, () => {
   console.log(`\n*** Web API listening on http://localhost:${port} ***\n`);
 });
