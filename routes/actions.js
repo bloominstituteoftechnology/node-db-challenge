11d58a3dfa222de0ae76ba65e0adbf8439474896
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-12 16:24:20 -0500

    defined POST endpoint to create new actions

--- routes/actions.js
+++ routes/actions.js
@@ -3,6 +3,21 @@ const db = require('../data/dbConfig');
 
 const router = express.Router();
 
+// POST /api/actions
+router.post('/', (req, res) => {
+  const action = req.body;
+  db('actions').insert(action)
+    .then(idInfo => {
+      db('actions').get(idInfo.id)
+        .then(action => {
+          res.status(201).json(idInfo);
+        });
+    }).catch(err => {
+      res.status(500)
+        .json({err: 'failed to insert action into db'});
+    });
+});
+
 // GET /api/actions
 router.get('/', (req, res) => {
   db('actions').then(rows => {
