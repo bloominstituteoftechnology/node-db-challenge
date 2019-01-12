3d42e2f00f8b959d854660f0a826dfe36c171ae3
cferrell2 <cferrell12182@gmail.com>
2019-01-12 12:34:13 -0500

    create migration for project table

--- migrations/20190112122517_project.js
+++ migrations/20190112122517_project.js
@@ -1,7 +1,10 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('project', function(tbl){
-    
+    tbl.increments();
+    tbl.string('project_name', 255).nonNullable();
+    tbl.string('description', 255);
+    tbl.boolean('completed');
   });
 
 };
