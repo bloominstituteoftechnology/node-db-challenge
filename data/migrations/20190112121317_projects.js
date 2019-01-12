651bec58a624b5646dadc8c4e0044a9e07ed5b15
Joseph Thompson <joe.thompson838@gmail.com>
2019-01-12 12:23:20 -0500

    created setup for a projects table in migrations

--- data/migrations/20190112121317_projects.js
+++ data/migrations/20190112121317_projects.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("projects", table => {
+    table.increments();
+    table.string("name").notNullable();
+    table.string("description").notNullable();
+    table.integer("completed").defaultTo(0);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("projects");
 };
