1b1d9c59c83eb5a7d0b73aced95424865331d625
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-12 15:10:10 -0500

    updated seeds and reseeded db

--- data/seeds/01_actions.js
+++ data/seeds/01_actions.js
@@ -8,7 +8,7 @@ exports.seed = function(knex, Promise) {
         {id: 1, description: 'bootstrap app', notes: 'set up with express, react and redux', complete: 0, project_id: 1},
         {id: 2, description: 'build presentational frontend', notes: 'build landing page, post list, and post form', complete: 0, project_id: 1},
         {id: 3, description: 'define user routes', notes: 'set up CRUD routes', complete: 0, project_id: 1},
-        {id: 4, description: 'bootstrap app', notes: 'set up with express, react and redux'', complete: 0, project_id: 2},
+        {id: 4, description: 'bootstrap app', notes: 'set up with express, react and redux', complete: 0, project_id: 2},
         {id: 5, description: 'build presentational frontend', notes: 'build landing page, notes list, and notes form', complete: 0, project_id: 2},
         {id: 6, description: 'build wireframe', notes: 'define basic layout based on clients needs', complete: 0, project_id: 3}
       ]);
--- data/seeds/01_projects.js
+++ data/seeds/01_projects.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('projects').insert([
-        {id: 1, name: 'blog app', description: '', complete: 0},
-        {id: 2, name: 'notes app', description: '', complete: 0},
-        {id: 3, name: 'OilBay website', description: '', complete: 0}
+        {id: 1, name: 'blog app', description: 'personal blog chronicaling my exploration of creative coding', complete: 0},
+        {id: 2, name: 'notes app', description: 'notes taking app for younger sister', complete: 0},
+        {id: 3, name: 'OilBay website', description: 'band website for friends looking to not use wix or squarespace', complete: 0}
       ]);
     });
 };
