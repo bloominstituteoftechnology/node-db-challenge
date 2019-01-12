2dcba93e362f5938c38c8c304b95bf464097642c
Levi Appenfelder <levi.appenfelder@gmail.com>
2019-01-12 15:26:03 -0700

    Migrations Have Been Run, sqlite3 File Created

--- data/migrations/20190112120032_actions.js
+++ data/migrations/20190112120032_actions.js
@@ -2,10 +2,10 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('actions', table => {
         table.increments();
-        table.integer('project_id').unsigned().unique();
+        table.integer('project_id').unsigned();
         table.foreign('project_id').references('id').on('projects')
         table.string('action_description').notNullable();
-        table.string('action_note').notNullable();
+        table.string('action_note');
         table.boolean('action_complete').notNullable();
     })
 };
