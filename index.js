3dcf39952ad10513945074cbb99c320c102f029e
Emmanium <pradoe.ep@gmail.com>
2019-01-12 11:09:15 -0700

    created MVP endpoints in index.js

--- index.js
+++ index.js
@@ -12,17 +12,38 @@ server.use(express.json());
 
 //POST for adding projects
 server.post('/api/projects', (req, res) => {
-
+  const project = req.body;
+  db('projects').insert(project)
+    .then(ids => {
+      res.status(201).json(ids) //201 = created
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to create project' })
+    })
 })
 
 //POST for adding actions
 server.post('/api/actions', (req, res) => {
-  
+  const action = req.body;
+  db('actions').insert(action)
+    .then(ids => {
+      res.status(201).json(ids)
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to create action' })
+    })
 })
 
 //GET for retrieving a project by its ID
 server.get('/api/projects/:id', (req, res) => {
-  
+  const { id } = req.params;
+  db('projects').where('id', id)
+    .then(rows => {
+      res.json(rows);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to find project' })
+    })
 })
 
 const PORT = 3000;
