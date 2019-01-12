de9a062119c64e385f20ecefa33cd4bb79f0824a
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-01-12 12:11:57 -0500

    Create first migration

---
+++ migrations/20190112120813_project.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('projects',    table   =>  {
+      table.increments();
+      table.string('project_name').notNullable();
+      table.string('description');
+      table.boolean('complete');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('projects');
+};
