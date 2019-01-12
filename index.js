629d6055604632e6efcf6f9b1a2e1be2776b4cab
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-01-12 12:01:36 -0600

    Endpoints not working properly

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ server.use(express.json());
 
 const PORT = 4040;
 
+server.post('/projects', (req, res) => {
+  const { project } = req.body;
+  db('projects')
+    .insert({ project })
+    .then(ids => res.status(201).json(ids[0]))
+    .catch(err => res.status(500).json({ err }));
+});
+
+server.post('./actions', (req, res) => {
+  const { action } = req.body;
+  db('actions')
+    .insert({ action })
+    .then(ids => res.status(201).json(ids[0]))
+    .catch(err => res.status(500).json({ err }));
+});
+
 server.listen(PORT, () =>
   console.log(`Server is up and running in port: ${PORT}`)
 );
