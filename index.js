1a133a307d11ff9aa93c5c62ec6c29b30862ae23
Robert Billington <chillingtonbillington@gmail.com>
2019-01-12 10:41:23 -0600

    Updated GET projects by id to include accompanying actions

--- index.js
+++ index.js
@@ -22,9 +22,21 @@ server.get('/projects', (req, res) =>{
 
 server.get('/projects/:id', (req, res) =>{
     const {id} = req.params;
-    db('projects').where('id', id).then(rows =>{
-        res.json(rows);
-    })
+    db('projects').where('id', id)
+        .first()
+        .then(project =>{
+            if(project){
+                db('actions').where('project_id', id)
+                    .then(actions =>{
+                        project.actions =actions;
+                        res.status(200).json(project)
+                    })
+            }else{
+                res.status(400).json({err : 'Failed to find project'})
+            }
+        })
+        
+   
     .catch(err =>{
         res.status(500).json({err: 'Faile to find project with specified ID'})
     })
@@ -63,11 +75,32 @@ server.put('/projects/:id', (req, res) =>{
     })
 })
 
-//post for actions
-//INSERT INTO actions (action_description, notes, completed, project_id) VALUES (w, x, y, z)
+//Actions CRUD operations below
+server.get('/actions', (req, res) =>{
+    db('actions').then(actions =>{
+        res.json(actions);
+    })
+    .catch(err =>{
+        res.status(500).json({err: 'Failed to get actions'})
+    })
+})
 
 //get for project by id
 //SELECT * FROM projects WHERE id = 1
+server.get('/actions/:id', (req, res) =>{
+    const {id} = req.params;
+    db('actions').where('id', id).then(rows =>{
+        res.status(201).json(rows);
+    })
+    .catch(err =>{
+        res.status(500).json({err: 'Failed to find action with specified ID'})
+    })
+})
+
+//post for actions
+//INSERT INTO actions (action_description, notes, completed, project_id) VALUES (w, x, y, z)
+
+
 
 server.listen(port, () =>{
     console.log(`Server is up and running on port ${port}`)
