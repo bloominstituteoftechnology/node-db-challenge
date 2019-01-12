7c78070658e2dc813498747a134d7aa32d139131
cferrell2 <cferrell12182@gmail.com>
2019-01-12 12:37:59 -0500

    create migration for actions table

---
+++ migrations/20190112122517_projects.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('projects', function(tbl){
+    tbl.increments();
+    tbl.string('project_name', 255).nonNullable();
+    tbl.string('proj_description', 255);
+    tbl.boolean('proj_completed');
+  });
+
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('projects');
+};
---
+++ migrations/20190112122534_actions.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('actions', function(tbl){
+      tbl.increments();
+      tbl.string('action_description', 255).nonNullable();
+      tbl.string('action_notes', 255);
+      tbl.boolean('action_completed');
+      tbl.integer('project_id')
+        .unsigned().references('id').inTable('projects');
+    });
+  
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('actions');
+  }
