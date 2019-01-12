7c32b4dfed64a5ca1ff8c8e2af68b1c5f8dc5a37
Matthew Meitl <darksamus6@gmail.com>
2019-01-12 10:27:14 -0600

    added initial post and get

--- index.js
+++ index.js
@@ -7,6 +7,34 @@ const PORT = 4000;
 
 server.use(express.json());
 
+server.get('/api/cohorts/:id', (req, res) => {
+    const {id} = req.params
+    db('cohorts').where({id}).then(id => {
+      res.status(201).json(id); 
+    })
+    .catch(err => { res.status(500).json({err: "there was an error"})
+  })
+})
+
+
+server.post('/api/projects/:id', (req, res) => {
+    const content = req.body
+    db('projects').insert(content).then( id => {
+      res.status(200).json(id)
+    })
+    .catch(err => { res.status(500).json({err: "there was an error"})
+  })
+})
+
+server.post('/api/actions', (req, res) => {
+    const content = req.body
+    db('actions').insert(content).then( id => {
+      res.status(200).json(id)
+    })
+    .catch(err => { res.status(500).json({err: "there was an error"})
+  })
+})
+
 
 server.listen(PORT, () => {
     console.log("server is running in port " + PORT)
