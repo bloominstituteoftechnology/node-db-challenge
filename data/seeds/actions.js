25d09e3823d7643a88d0cd29a8fb9e16967dfd81
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-01-12 15:40:07 -0600

    added truncate to seeds/actions file

--- data/seeds/actions.js
+++ data/seeds/actions.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('actions').del()
+  return knex('actions').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('actions').insert([
