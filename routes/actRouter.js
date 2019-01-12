46b9441c70c94a4718e52286f98f37bac2200a2c
Jennifer King <jenbellpgh@gmail.com>
2019-01-12 13:44:06 -0500

    Completed POST for adding new actions

--- routes/actRouter.js
+++ routes/actRouter.js
@@ -7,6 +7,27 @@ const db = knex(dbConfig.development);
 
 /* ---------- Routes ---------- */
 
+// -----
+// - POST for adding actions
+// - INSERT INTO actions ('desc','notes','completed', 'proj_id') VALUES ('','',0,0);
+// ----- 
+router.post('/', (req,res) => {
+  const actsData = req.body;
+
+  // Check for empty fields, leaving "notes" as optional:
+  if( actsData.desc && actsData.proj_id ) {
+    db('actions').insert(actsData)
+      .then( (newId) => {
+        res.status(201).json(newId);
+      })
+      .catch( (err) => {
+        res.status(500).json({ error: "Could not add new action." });
+      });
+    // end-db
+  } else {
+    res.status(400).json({ error: "Please provide a description and project id for the action." });
+  }
+});
 
 
 /* ---------- Export ---------- */
