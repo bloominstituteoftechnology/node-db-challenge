dc7281e9846bd49730a2ee244fbf90dc1f5677a6
Javier Alavrez <javieralvarez82@live.com>
2019-01-12 13:53:19 -0800

    completed table config working on server and end points

--- data/migrations/20190112112210_projects.js
+++ data/migrations/20190112112210_projects.js
@@ -1,8 +1,9 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('projects', table => {
-      table.increments('id');
-      table.string('project_name').primary().notNullable().unique();
+      table.increments();
+      table.integer('project_id').unsigned().primary()
+      table.string('project_name').notNullable().unique();
       table.text('proj_desc').notNullable();
       table.boolean('completed').notNullable()
 
--- data/migrations/20190112112242_actions.js
+++ data/migrations/20190112112242_actions.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return  knex.schema.createTable('actions', table => {
+      table.increments();
+      table.foreign('action_id').references('project_id').on('projects')
+      table.text('action_desc').notNullable()
+      table.text('action_notes')
+      table.boolean('action_completed');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExist('actions')
 };
