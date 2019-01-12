9d872d69c80f7b0dab2fda7f6118c2ccdc884394
TheGameFreak720 <luis.alvarez@commithub.com>
2019-01-12 13:44:46 -0500

    get request does return the projects and joins the table but not in the format asked

--- data/helpers/projectDb.js
+++ data/helpers/projectDb.js
@@ -4,6 +4,22 @@ const dbConfig = require('../../knexfile');
 const db = knex(dbConfig.development);
 
 module.exports = {
+    get: function(id) {
+        return db('project')
+        .join('action', 'project.id', '=', 'action.project_id')
+        .where('project.id', id)
+        .select(
+            'project.id', 
+            'project.name',
+            'project.description',
+            'project.completed',
+            'action.id as action_id',
+            'action.description as action_description',
+            'action.notes',
+            'action.completed as action_completed'
+        );
+    },
+
     add: function(project) {
         return db('project').insert(project);
     }
--- routes/project.js
+++ routes/project.js
@@ -3,6 +3,18 @@ const router = express.Router();
 
 const projectDB = require('../data/helpers/projectDb');
 
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+
+    projectDB.get(id)
+        .then(rows => {
+            res.json(rows);
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: 'Failed to get project' });
+        });
+});
+
 router.post('/', (req, res) => {
     const project = req.body;
 
