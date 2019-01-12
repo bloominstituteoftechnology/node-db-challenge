56f8632dcbd894ba98b3be1a737f84f360b730dc
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-01-12 13:48:25 -0500

    still debugging

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 const PORT = process.env.PORT || 5000;
 server.use(express.json());
-const db = ('/data/db.js');
+const db = ('data/db.js');
 
 //POST for adding project
 
