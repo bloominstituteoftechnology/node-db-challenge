65a7e5c4918e8e1695e7e6333d9d5d8bd19ba81e
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-12 13:18:07 -0500

    defined actions table schema

--- data/migrations/20190112130306_actions.js
+++ data/migrations/20190112130306_actions.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('actions', table => {
+    table.increments();
+    table.string('description').notNullable();
+    table.integer('project_id').unsigned().references('id').inTable('projects').unique();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('actions');
 };
