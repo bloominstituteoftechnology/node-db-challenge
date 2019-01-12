892053f0ea3bd51944fd131d19a17f1864d121c3
Phinehas <kohler.nai@gmail.com>
2019-01-12 12:38:04 -0700

    built database starting on API

--- data/migrations/20190112121636_projects.js
+++ data/migrations/20190112121636_projects.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('projects', table => {
+      table.increments();
+      table.string('project_name').notNullable();
+      table.string('proj_description').notNullable();
+      table.boolean('proj_completed').notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('projects');
 };
--- data/migrations/20190112121646_actions.js
+++ data/migrations/20190112121646_actions.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('actions', table => {
+      table.increments();
+      table.string('action_description').notNullable();
+      table.string('notes');
+      table.boolean('action_completed').notNullable();
+      table.integer('proj_ID').unsigned();
+      table.foreign('proj_ID').references('id').on('projects');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('actions');
 };
