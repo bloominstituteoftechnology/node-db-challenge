50569c732469cd64890b341f4644d4ec39718ccc
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2019-01-12 11:33:44 -0600

    fixed syntax error in actions_table migration

--- data/migrations/20190112112512_actions_table.js
+++ data/migrations/20190112112512_actions_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('actions', tbl => {
     tbl.increments();
     tbl.integer('project_id').unsigned();
-    tbl.foreign('project_id').references(projects.id);
+    tbl.foreign('project_id').references('projects.id');
     tbl.string('action_description').notNullable();
     tbl.string('additional_information');
     tbl.boolean('completed').notNullable();
