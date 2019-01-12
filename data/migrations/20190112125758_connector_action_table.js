60ba4240addad3f3d0952a6dfe006ec2b00ac7b9
Alex Martin <atonymartin20@yahoo.com>
2019-01-12 13:39:46 -0600

    Adding routers

--- data/migrations/20190112125758_connector_action_table.js
+++ data/migrations/20190112125758_connector_action_table.js
@@ -1,9 +1,9 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('actions', table => {
+  return knex.schema.createTable('connector_actions', table => {
     table.increments();
     table.integer('action_id').notNullable().unsigned();
-    table.foreign('action_id').references('id')on('actions');
+    table.foreign('action_id').references('id').on('actions');
     table.integer('connector_id').notNullable().unsigned();
     table.foreign('connector_id').references('id').on('connector');
   })
