79804d2b3477f10374ba4d50bfaf063f036ea230
Alex Martin <atonymartin20@yahoo.com>
2019-01-12 11:49:56 -0600

    created endpoints for post and get in api/projects

--- index.js
+++ index.js
@@ -7,7 +7,46 @@ const PORT = 4500;
 
 server.use(express.json());
 
-//beginngin of /api/
+//beginning of /api/projects endpoints
+
+//POST /api/projects
+server.post('/api/projects', (req, res) => {
+    const project = req.body;
+    db('projects').insert(project)
+        .then(id => {
+            res.status(201).json(id);
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: 'Failed to inserkt project' });
+        });
+});
+
+//GET /api/projects
+server.get('/api/projects', (req, res) => {
+    db('projects')
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: 'Failed to find projects' });
+        });
+});
+
+//GET BY ID /api/projects/:id
+server.get('/api/projects/:id', (req, res) => {
+    const { id } = req.params;
+    db('projects').where('id', id)
+        .then(project => {
+            res.status(200).json(project);
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: 'Failed to find project with that id.' });
+        });
+});
+
+// PUT
+
+// DELETE
 server.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
 })
