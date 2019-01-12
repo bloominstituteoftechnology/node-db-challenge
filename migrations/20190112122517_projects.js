390fc06de5095d252bf1c9aa024238cb19ee864f
cferrell2 <cferrell12182@gmail.com>
2019-01-12 12:42:22 -0500

    run migrations creating database with projects and actions tables.  Verify within DB Browser for SQLITE that database and tables were created correctly.

--- migrations/20190112122517_projects.js
+++ migrations/20190112122517_projects.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('projects', function(tbl){
     tbl.increments();
-    tbl.string('project_name', 255).nonNullable();
+    tbl.string('project_name', 255).notNullable();
     tbl.string('proj_description', 255);
     tbl.boolean('proj_completed');
   });
