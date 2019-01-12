132c1c8fd899d0e2955745b8abfe3b955e2aec6c
shannonhealy <sahealy525@gmail.com>
2019-01-12 12:29:29 -0500

    created actions migration

---
+++ data/migrations/20190112121751_actions_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("actions", table => {
+        table.increments("id").primary();
+        table.string("description").notNullable();
+        table.string("notes");
+        table.boolean("completed");
+        table.integer("project_id").references("projects.id");
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("actions");
+};
