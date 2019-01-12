329f3673291b401c251aff9841dab2ab0108f722
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-01-12 10:35:05 -0700

    Added migration file to create schema for projects table

--- data/migrations/20190112102650_projects_table.js
+++ data/migrations/20190112102650_projects_table.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('projects_table', table => {
+        table.increments();
+        table.string('project_name');
+        table.string('project_description');
+        table.boolean('project_completed');
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('projects_table');
 };
