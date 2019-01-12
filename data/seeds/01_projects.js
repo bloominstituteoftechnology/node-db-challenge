5fa9ff7b7ea40e99cdc6d8804498696bd8acc91c
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-12 14:05:53 -0500

    refactored to have seed truncate

--- data/seeds/01_projects.js
+++ data/seeds/01_projects.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('table_name').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
