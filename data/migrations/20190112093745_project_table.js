5bbdca89c4f5d3af11b1734affc7d75475444f18
Freekeyboy <jjhutton81@gmail.com>
2019-01-12 10:44:11 -0800

    Completed migration files for project and action tables... I think. Moving on to join table.

--- data/migrations/20190112093745_project_table.js
+++ data/migrations/20190112093745_project_table.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('projects', tbl => {
+      tbl.increments();
+      tbl.integer().notNullable().unsigned();
+      tbl.string('project_name', 255).unique();
+      tbl.string('project_description', 255);
+      tbl.boolean('project_completed').notNullable().defaultTo(false);
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('project');
 };
--- data/migrations/20190112093830_action_table.js
+++ data/migrations/20190112093830_action_table.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.scheme.createTable('action', tbl => {
+      tbl.increments();
+      tbl.integer().notNullable().unsigned();
+      tbl.string('description', 500);
+      tbl.string('notes', 500);
+      tbl.boolean('project_completed').notNullable().defaultTo(false);
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('action');
 };
