8519506c88a73e4910973e041eebe41f02fcfb6f
Colin Dismuke <colin.dismuke@gmail.com>
2019-01-12 11:30:00 -0600

    Added columns to  project and action tables

--- data/migrations/20190112112210_schema_table.js
+++ data/migrations/20190112112210_schema_table.js
@@ -1,3 +1,27 @@
-exports.up = function(knex, Promise) {};
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable("projects", project => {
+      project.increments().primary();
+      project.text("name", 255).notNullable();
+      project.text("project_description", 255).notNullable();
+      project.boolean("project_is_complete");
+    })
+    .createTable("actions", action => {
+      action.increments();
+      action.text("action_description", 255).notNullable();
+      action.text("action_notes", 255).notNullable();
+      action.boolean("action_is_complete");
+      action
+        .integer("projectId")
+        .notNullable()
+        .unsigned()
+        .references("projects.id");
+    });
+};
 
-exports.down = function(knex, Promise) {};
+exports.down = function(knex, Promise) {
+  // Rollback
+  return Promise.all(["projects", "actions"]).map(table =>
+    knex.schema.dropTableIfExists(table)
+  );
+};
