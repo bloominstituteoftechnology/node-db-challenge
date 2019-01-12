e18ea13628b51f8badd5f7eb275e1c14ce67eec0
jbrock1981 <jbrock1981@gmail.com>
2019-01-12 11:31:28 -0600

    completed projects migration

---
+++ data/migrations/20190112112701_projects.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('projects', table => {
+        table.increments('id');
+        table.string('name').notNullable();
+        table.string('description').notNullable();
+        table.boolean('completed').defaultTo(false);
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('projects');
+};
