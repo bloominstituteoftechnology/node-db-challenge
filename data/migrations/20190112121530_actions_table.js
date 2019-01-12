a23e7bf211f3e0a8cffea2b21c781d11ce71999f
Ben <bennybooftw@gmail.com>
2019-01-12 12:28:29 -0500

    Setup actions migration table

--- data/migrations/20190112121530_actions_table.js
+++ data/migrations/20190112121530_actions_table.js
@@ -1,6 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("actions", table => {
+      table.increments();
+      table.string("description", 280).notNullable();
+      table.string("notes", 280).notNullable();
+      table.boolean("completed").defaultTo(false);
+      table.integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+  })
 };
 
 exports.down = function(knex, Promise) {
