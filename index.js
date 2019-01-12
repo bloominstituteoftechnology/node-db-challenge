3a907c0d01499c18bd5ac8d789221c5d09ae9a10
Chris Roth <c.roth.lambda@gmail.com>
2019-01-12 13:48:59 -0600

    Sprint-Challenge-RDBMS MVP Complete: -all end points created

--- index.js
+++ index.js
@@ -7,15 +7,47 @@ const knex = require('knex');
 const dbConfig = require('./knexfile');
 const db = knex(dbConfig.development);
 
-const PORT = 5750
+const PORT = 5750;
 
 server.use(
      express.json(),
-     morgan.dev
+     morgan('dev')
     );
 
+server.post('/projects', (req, res)=>{
+    const newProject = req.body;
+    db('projects').insert(newProject)
+        .then(project=>{
+            res.status(201).json(project)
+        })
+        .catch(err=>{
+            res.status(500).json({err: "we had a problem adding your data"})
+        })
 
-    
+})
+server.post('/actions', (req, res)=>{
+    const newAction = req.body;
+    db('actions').insert(newAction)
+        .then(action=>{
+            res.status(201).json(action)
+        })
+        .catch(err=>{
+            res.status(500).json({err: "we had a problem adding your data"})
+        })
+})
+
+server.get('/projects/:id', (req, res)=>{
+    const {id} = req.params
+    db('projects').join()
+        .then(projects=>{
+            res.json(projects);
+        })
+        .catch(err=>{
+            res.status(500).json({err:'trouble wth grabbing all these projects'})
+        })
+});
+
+
 server.listen(PORT, ()=>{
     console.log(`Server is listenig on port:${PORT}`)
 })
