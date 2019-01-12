d621fcea58ad7975cac9507d46e111dc984d3218
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-12 13:17:47 -0500

    defined projects table schema

--- data/migrations/20190112130249_projects.js
+++ data/migrations/20190112130249_projects.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('projects', table => {
+    table.increments();
+    table.string('name').notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('projects');
 };
