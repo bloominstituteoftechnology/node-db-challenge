c7e5d5bc0579705f37ea5c8290af7b076f195222
Andres Rivera <andres.rivera.toro@gmail.com>
2019-01-12 12:39:44 -0500

    Add knex migrations and database

---
+++ data/migrations/20190112123155_actions.js
@@ -0,0 +1,17 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('actions', (table) => {
+    table.increments();
+    table.string('description').notNullable();
+    table.string('notes');
+    table.boolean('completed');
+    table
+      .integer('project_id')
+      .unsigned()
+      .references('id')
+      .inTable('projects');
+  });
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists('actions');
+};
