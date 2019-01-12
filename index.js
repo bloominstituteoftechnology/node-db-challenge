3a7b024aa2aa08af8818657ad2b784204baf489e
Mitchell Robles <mitchellr92@live.com>
2019-01-12 10:24:23 -0800

    Finished required endpoints

--- index.js
+++ index.js
@@ -10,15 +10,36 @@ const PORT = 0112
 server.use(express.json());
 
 server.post('/project', (req, res) => {
-
+    const project = req.body;
+    db('project').insert(project)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(err => {
+            res.status(500).json({ err: 'Failed to insert project' })
+        });
 });
 
 server.post('/action', (req, res) => {
-
+    const action = req.body;
+    db('action').insert(action)
+        .then(action => {
+            res.status(201).json(action);
+        })
+        .catch(err => {
+            res.status(500).json({ err: 'Failed to insert action' })
+        });
 });
 
-server.get('/porject/:id', (req, res) => {
-
+server.get('/project/:id', (req, res) => {
+    const { id } = req.params;
+    db('project').where('id', id)
+        .then(project => {
+            res.status(200).json(porject);
+        })
+        .catch(err => {
+            res.status(500).json({ err: 'Failed to get project' })
+        })
 });
 
 server.listen(PORT, () => {
