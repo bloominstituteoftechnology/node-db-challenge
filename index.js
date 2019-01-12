2f1801060dd325b7bb1eef39a5310f4c2c3babb0
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-01-12 13:58:36 -0600

    added some error handling

--- index.js
+++ index.js
@@ -8,32 +8,44 @@ const PORT = 4040;
 
 server.post('/projects', (req, res) => {
   const project = req.body;
-  db('projects')
-    .insert(project)
-    .then(ids => res.status(201).json(ids[0]))
-    .catch(err => res.status(500).json({ err }));
+  !project
+    ? res
+        .status(401)
+        .json({ error: 'Check the sumbitted information and try again' })
+    : db('projects')
+        .insert(project)
+        .then(ids => res.status(201).json(ids[0]))
+        .catch(err => res.status(500).json({ err }));
 });
 
 server.post('/actions', (req, res) => {
   const action = req.body;
-  db('actions')
-    .insert(action)
-    .then(ids => res.status(201).json(ids[0]))
-    .catch(err => res.status(500).json({ err }));
+  !action
+    ? res
+        .status(401)
+        .json({ error: 'Check the submitted information and try again.' })
+    : db('actions')
+        .insert(action)
+        .then(ids => res.status(201).json(ids[0]))
+        .catch(err => res.status(500).json({ err }));
 });
 
 server.get('/projects/:id', (req, res) => {
   const { id } = req.params;
   db('projects')
     .where('id', id)
-    .then(result => {
-      db('actions')
-        .where('project_id', id)
-        .then(actions => {
-          result[0].actions = actions;
-          res.status(200).json(result[0]);
-        });
-    })
+    .then(result =>
+      !result
+        ? res
+            .status(404)
+            .json({ error: 'There is no project with the specified ID' })
+        : db('actions')
+            .where('project_id', id)
+            .then(actions => {
+              result[0].actions = actions;
+              res.status(200).json(result[0]);
+            })
+    )
     .catch(err => res.status(500).json({ err }));
 });
 
