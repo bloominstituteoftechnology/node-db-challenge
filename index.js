fa6b7c598c20c2db8bd9ed9172c948157943163d
hraya <hraya@lssu.edu>
2019-01-12 14:01:48 -0600

    Completed Endpoints
    Testing endpoints the POST work find
    
    the GET is not working properly
    
    i have two test GET endpoints

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-cosnt knex = require('knex');
+const knex = require('knex');
 
 const db_config = require('./knexfile.js');
 
@@ -22,15 +22,47 @@ server.post('/api/projects',(req, res)=>{
 
 server.post('/api/actions',(req, res)=>{
     const actions = req.body;
-    if(!actions.name || !actions.description){res.status(400).json({message:"Please provide a description for this action. "})}
+    if(!actions.action_description){res.status(400).json({message:"Please provide a description for this action. "})}
     db('action').insert(actions)
     .then(action_post =>{
         res.status(201).json(action_post)
     }).catch(err=>{
+        console.log(err)
         res.status(404).json({error:"There was an error while saving the action to the database"})
     })
 } )
 
+server.get('/api/projects/:id', (req, res)=>{
+    const { id } = req.params;
+    db('projects').join('action','project_id','projects.id')
+    .where({id:id})
+    .then(projects =>{
+        if(projects.length > 0){
+            res.json(projects)
+        }else{
+            res.status(404).json({message:'The project with the specified id does not exist'})
+        }
+    }).catch(err =>{
+        console.log(err)
+        res.status(500).json({error:"Trouble getting the project"})
+    })
+})
+
+//Just for testing purposes
+// server.get('/api/projects', (req, res) => {
+//     db('projects').leftJoin('action', 'project_id', 'project.id')
+//     .then(pInfo => {
+//       res.send(pInfo);
+//     });
+//   });
+
+
+//   server.get('/api/actions', (req, res) => {
+//     db('action')
+//     .then(aInfo => {
+//       res.send(aInfo);
+//     });
+//   });
 
 server.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}!`)
