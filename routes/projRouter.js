1df53a21027d4b2957231129514fcce5d889fdf9
Jennifer King <jenbellpgh@gmail.com>
2019-01-12 13:36:30 -0500

    Completed POST endpoint for projects

--- routes/projRouter.js
+++ routes/projRouter.js
@@ -7,6 +7,27 @@ const db = knex(dbConfig.development);
 
 /* ---------- Routes ---------- */
 
+// -----
+// - POST for adding projects
+// - INSERT INTO projects ('name','desc','completed') VALUES ('','',0);
+// ----- 
+router.post('/', (req,res) => {
+  const projData = req.body;
+
+  // Check for empty fields:
+  if( projData.name && projData.desc ) {
+    db('projects').insert(projData)
+      .then( (newId) => {
+        res.status(201).json(newId);
+      })
+      .catch( (err) => {
+        res.status(500).json({ error: "Could not add new project." });
+      });
+    // end-db
+  } else {
+    res.status(400).json({ error: "Please provide a name and description for the project." });
+  }
+});
 
 
 /* ---------- Export ---------- */
