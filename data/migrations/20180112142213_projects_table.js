f757ea806fab952d97eb2df5af9044f1466e5173
Joe Mercado <jryel1970@gmail.com>
2019-01-12 19:49:35 -0500

    next commit. finishing up project

---
+++ data/migrations/20180112142213_projects_table.js
@@ -0,0 +1,18 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('projects', function(tbl) {
+		tbl.increments();
+
+		tbl
+		  .string('name', 128).notNullable();
+
+		tbl
+		  .string('description', 3000);
+
+		tbl
+		  .boolean('completed').defaultTo(false);
+	})
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('projects');
+};
