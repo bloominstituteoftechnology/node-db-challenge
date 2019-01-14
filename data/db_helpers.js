9847a1e4cadaa72e8a8d47ba5da9255341f84bd1
Javier Alavrez <javieralvarez82@live.com>
2019-01-13 23:10:56 -0800

    working on helper functions

---
+++ data/db_helpers.js
@@ -0,0 +1,13 @@
+const knex = require('knex')
+
+const dbConfig = require('../knexfile.js');
+
+const db = knex(dbConfig.development);
+
+module.exports = {
+
+    
+
+
+
+}
--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const PORT = 4000;
 
 server.use(express.json());
 
+
+//Post Projects
+
 server.post('/api/projects', (req, res) => {
   const project = req.body
   console.log(project)
@@ -20,8 +23,30 @@ server.post('/api/projects', (req, res) => {
   })
 })
 
+//Post Actions
+
+server.post('/api/projects/actions', (req, res) => {
+  const actions = req.body
+  console.log(db("actions"))
+  db("actions").leftJoin(actions).then(act => {
+      res.status(201)
+      .json(act)
+  })
+  .catch(err => { message: `could not post project error ${err}`
+  })
+})
+
 server.get('/api/projects', (req, res) => {
   db("projects").then(proj => {
+    res.status(200)
+    .send(proj)
+  })
+})
+
+
+server.get('/api/projects/actions', (req, res) => {
+  console.log(db("actions"))
+  db("actions").then(proj => {
     res.status(200)
     .send(proj)
   })
