7af009d8441f781032ba9faae7f2d4131b1ec520
Dennis Orbison <orbison.dennis@gmail.com>
2019-01-18 08:33:19 -0800

    set up helpers not sure if working yet

--- data/helper/actionsModel.js
+++ data/helper/actionsModel.js
@@ -1,2 +1,9 @@
-const knex = require('knex');
-const knexConfig = require('../../knexfile.js');
+const db = require('../dbConfig.js');
+
+module.exports = {
+  createAction({description, notes, completed = false}){
+    
+    let query = db('actions');
+    return query.insert({description, notes, completed});
+  },
+}
--- data/helper/projectsModel.js
+++ data/helper/projectsModel.js
@@ -1,2 +1,27 @@
-const knex = require('knex');
-const knexConfig = require('../../knexfile.js');
+const db = require('../dbConfig.js');
+
+module.exports = {
+
+  getProjects() {
+
+    let query = db('projects');
+    return query;
+  },
+
+  getProject(id) {
+
+    let project = db('projects').where({id})
+    let actions = db('actions').where({project_id: id})
+
+    return Promise.all([project, actions]).then(results => {
+      const [project, actions] = results;
+      return {project, actions: {...actions}};
+    })
+  },
+
+  createProject({name, description, completed = false}){
+    
+    let query = db('projects');
+    return query.insert({name, description, completed});
+  },
+}
