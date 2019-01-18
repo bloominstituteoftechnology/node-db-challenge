4abebe72c50007a20a476deb085a2836e6c8f2b8
Jarrad Miller <jm020801@gmail.com>
2019-01-18 12:42:06 -0700

    fix

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./planning.sqlite3"
+      filename: "./projects.db3"
     },
     useNullAsDefault: true,
     migrations: {
