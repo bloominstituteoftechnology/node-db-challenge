b6a9cd4891a64a6072145e466e647fa874d40023
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-01-26 14:22:16 -0500

    Writing out endpoints, will try and test in postman.

--- data/routes/actionsRouter.js
+++ data/routes/actionsRouter.js
@@ -3,11 +3,23 @@ const router = express.Router()
 const actsDB = require('../helpers/actionsDB')
 
 router.get('/', (req, res) => {
+ actsDB.pull()
+  .then(() => {
 
+  })
+  .catch(() => {
+
+  })
 })
 
 router.get('/:id', (req, res) => {
+ actsDB.pullById(id)
+  .then(() => {
 
+  })
+  .catch(() => {
+
+  })
 })
 
 router.post('/', (req, res) => {
--- data/routes/projectRouter.js
+++ data/routes/projectRouter.js
@@ -17,11 +17,37 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
  const { id } = req.params 
- projDB.pullById
+ projDB.pullById(id)
+  .then((project) => {
+   res
+    .json(project)
+  })
+  .catch(() => {
+   res
+    .status(500)
+    .json({error: "There was an error getting project from DB."})
+  })
 })
 
 router.post('/', (req, res) => {
-
+ const project = req.body 
+ if (project.name && project.description) {
+  projDB.place(project)
+   .then(() => {
+    res
+     .status(201)
+     .json(project)
+   })
+   .catch(() => {
+    res
+     .status(500)
+     .json({error: "There was an error "})
+   })
+ }
+ else {
+  res
+   .json({error: "Name and description required for project."})
+ }
 })
 
 router.put('/', (req, res) => [
