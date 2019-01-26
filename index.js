c640dc30d140d09456f44fc9c5a6c23711c2f5be
cocoitali <cmburatto@gmail.com>
2019-01-26 10:17:53 -0800

    FIX : added catch, modified Get by ID

--- index.js
+++ index.js
@@ -9,14 +9,28 @@ server.use(express.json())
 
 server.get('/api/projects/:id/actions', (req, res) => {
   // const { id } = req.params
-  db('projects').innerJoin('actions', 'projects.id', 'project_id')
+  db('projects')
+    .innerJoin('actions', 'projects.id', 'project_id')
     .then(projectInfo => {
-      res.send(projectInfo)
+      if (projectInfo) {
+        res.json(projectInfo)
+      } else {
+        res
+          .status(404)
+          .json({
+            message: 'There are no actions that exist within this project.'
+          })
+      }
+    })
+    .catch(() => {
+      res
+        .status(404)
+        .json({ error: 'Info about this action could not be retrieved.' })
     })
 })
 
 server.post('/api/projects', (req, res) => {
-    const project = req.body
+  const project = req.body
   if (project.name && project.description && project.is_complete) {
     db('projects')
       .insert(project)
@@ -29,17 +43,15 @@ server.post('/api/projects', (req, res) => {
           .json({ error: 'Failed to insert project into database' })
       })
   } else {
-    res
-      .status(400)
-      .json({
-        error:
-          'Please provide a name and description for the project as well as whether or not it has been completed.'
-      })
+    res.status(400).json({
+      error:
+        'Please provide a name and description for the project as well as whether or not it has been completed.'
+    })
   }
 })
 
 server.post('/api/actions', (req, res) => {
-    const action = req.body
+  const action = req.body
   if (action.name && action.description && action.is_completed) {
     db('actions')
       .insert(action)
@@ -50,12 +62,10 @@ server.post('/api/actions', (req, res) => {
         res.status(500).json({ error: 'Failed to insert action into database' })
       })
   } else {
-    res
-      .status(400)
-      .json({
-        error:
-          'Please provide a name and description for the action and whether or not it has been completed.'
-      })
+    res.status(400).json({
+      error:
+        'Please provide a name and description for the action and whether or not it has been completed.'
+    })
   }
 })
 
