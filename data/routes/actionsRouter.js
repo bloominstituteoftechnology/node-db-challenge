6b2624d70dcabb31dd742399b7995ca64ad3a0d9
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-01-26 13:29:15 -0500

    Writing endpoints out for projects and actions.

--- data/routes/actionsRouter.js
+++ data/routes/actionsRouter.js
@@ -1,6 +1,24 @@
 const express = require("express")
 const router = express.Router() 
 
+router.get('/', (req, res) => {
 
+})
+
+router.get('/:id', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.put('/:id', (req, res) => {
+
+})
+
+router.delete('/', (req, res) => {
+
+})
 
 module.exports = router 
--- data/routes/projectRouter.js
+++ data/routes/projectRouter.js
@@ -1,8 +1,25 @@
 const express = require("express")
 const router = express.Router() 
 
+router.get('/', (req, res) => {
 
+})
 
+router.get('/:id', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.put('/', (req, res) => [
+
+])
+
+router.delete('/', (req, res) => {
+
+})
 
 module.exports = router 
 
