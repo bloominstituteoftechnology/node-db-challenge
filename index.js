fe1f5c9dd439703e6fff787add7174f6b5ab18e1
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-01-29 17:34:52 -0600

    created files necessary for express servers using routes

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const server = express()
+server.use(express.json())
+
+//Routes
+const projectsRoute = require('./data/routes/projectsRoute')
+const actionsRoutes = require('./data/routes/actionsRoutes')
+
+server.use('/api/projects', projectsRoute)
+server.use('/api/actions', actionsRoutes)
+
+//Listening
+
+const port = 3300;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
