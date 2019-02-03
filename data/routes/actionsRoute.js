fc3ec64cae8101e5d1203530738dee721a4168be
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-02-02 18:45:19 -0600

    fff

--- data/routes/actionsRoute.js
+++ data/routes/actionsRoute.js
@@ -7,10 +7,9 @@ const db = require('../helpers/db')
 router.post('/', async (req, res) => {
   const action = req.body;
   if (action.action_description && action.project_id) {
-      db.getProject()
-      .then(projects => {
-        projects.forEach(project => {
-          if(project.id === action.project_id){
+      db.getProject(action.project_id)
+      .then(project => {
+          if(project[0]) {
             db.addAction(action)
               .then(id => {
                 res
@@ -23,9 +22,15 @@ router.post('/', async (req, res) => {
                   .json({message: 'failed to add action'})
               })
           } else {
-
+            res
+              .status(500)
+              .json({message: 'No project under that project_id'})
           }
         })
+      .catch(err => {
+        res
+          .status(500)
+          .json({message: 'failed to check projects'})
       })
   } else {
       res
