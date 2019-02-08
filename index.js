39d290591cc64f477454ed29739224949d9e1930
jbierly1 <james.bierly@gmail.com>
2019-02-07 20:34:21 -0500

    fixed relative path bug

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express=require('express');
-const server=express();
-const knex=require('knex');
-const dbConfig=require('./database/knexfile.js');
-const dbr=require('./database/database/modelProject.js');
-const db=knex(dbConfig.development);
-const PORT=process.env.PORT || 8000;
+const express = require('express');
+const server = express();
+const knex = require('knex');
+const dbConfig = require('./knexfile.js');
+const dbr = require('./database/database/modelProject.js');
+const db = knex(dbConfig.development);
+const PORT = process.env.PORT || 8000;
 
 server.use(express.json());
 
@@ -16,30 +16,34 @@ server.use(express.json());
 //     console.log(result);
 //   })
 
-
 server.post('/projects/post', (req, res) => {
-    dbr.addProject(req.body).then(id=>res.status(200).json(id)).catch(err=>res.status(500).json(err))
-})
+	dbr
+		.addProject(req.body)
+		.then(id => res.status(200).json(id))
+		.catch(err => res.status(500).json(err));
+});
 server.post('/actions/post', (req, res) => {
-    dbr.addAction(req.body).then(id=>res.status(200).json(id))
-})
+	dbr.addAction(req.body).then(id => res.status(200).json(id));
+});
 
-
+//
 
 server.get('/projects/:id', (req, res) => {
-    dbr.getProject(req.params.id).then(project=>dbr.getActions(req.params.id).then(actions=>res.status(200).json({
-        "id": project[0].id,
-        "name": project[0].name,
-        "description": project[0].description,
-        "isDone": project[0].isDone,
-        "actions":actions
-        
-      })))
-    
-})
+	dbr.getProject(req.params.id).then(project =>
+		dbr.getActions(req.params.id).then(actions =>
+			res.status(200).json({
+				id: project[0].id,
+				name: project[0].name,
+				description: project[0].description,
+				isDone: project[0].isDone,
+				actions: actions
+			})
+		)
+	);
+});
 
-server.get('/actions/:id',(req,res)=>{
-    dbr.getActions(req.params.id).then(actions=>res.status(200).json(actions))
-})
+server.get('/actions/:id', (req, res) => {
+	dbr.getActions(req.params.id).then(actions => res.status(200).json(actions));
+});
 
 server.listen(PORT, () => console.log(`API running on port ${PORT}`));
