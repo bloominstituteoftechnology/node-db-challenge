215f9c4ff42a1c0ee221a98a3a224f1246d43365
erscriber <erscriber@gmail.com>
2019-02-12 11:43:22 -0500

    Added dummy data

---
+++ data/action_data.js
@@ -0,0 +1,40 @@
+module.exports = [
+  { action_description: "action 1-1", notes: "yay notes!", project_id: 1 },
+  { action_description: "action 2-1", notes: "more notes", project_id: 1 },
+  { action_description: "action 3-1", notes: "all the notes", project_id: 1 },
+  {
+    action_description: "action 1-2",
+    notes: "Puerto Rico",
+    project_id: 2
+  },
+  { action_description: "action 3-2", notes: "too many notes", project_id: 2 },
+  {
+    action_description: "action 1-3",
+    notes: "Cuba",
+    project_id: 3
+  },
+  {
+    action_description: "action 2-3",
+    notes: "Dominican Republic",
+    project_id: 3
+  },
+  {
+    action_description: "action 3-3",
+    notes: "Honduras",
+    project_id: 3
+  },
+  { action_description: "action 1-4", notes: "even more notes", project_id: 4 },
+  { action_description: "action 2-4", notes: "........", project_id: 4 },
+  { action_description: "action 3-4", notes: "woohoo notes!", project_id: 4 },
+  {
+    action_description: "action 1-5",
+    notes: "Panama",
+    project_id: 5
+  },
+  { action_description: "action 2-5", notes: "almost done.", project_id: 5 },
+  {
+    action_description: "action 3-5",
+    notes: "Belize",
+    project_id: 5
+  }
+];
---
+++ data/project_data.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { name: "test project 1", description: "testing database 1" },
+  { name: "test project 2", description: "testing database 2" },
+  { name: "test project 3", description: "testing database 3" },
+  { name: "test project 4", description: "testing database 4" },
+  { name: "TP5", description: "Countries of my family origin" }
+];
