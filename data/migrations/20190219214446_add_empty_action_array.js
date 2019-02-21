00b0314dbfe5eee21466b7dd8584d7c372865b75
Jesse Theisen <theiseja@gmail.com>
2019-02-20 21:10:08 -0600

    MVP: knex test successful, created database file

--- data/migrations/20190219214446_add_empty_action_array.js
+++ data/migrations/20190219214446_add_empty_action_array.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.table('projects', table => {
-      table.speficType('actions', 'jsonb[]')
+      table.specificType('actions', 'jsonb[]')
   })
 };
 
