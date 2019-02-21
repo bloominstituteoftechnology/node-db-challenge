8baddb6eb1ca246dde608a8ff62b65a46e24bbd0
Nathan Thomas <nwthomas@me.com>
2019-02-20 21:10:59 -0800

    Create seeding/migrations files and basic data file directory structure

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.get("/", (req, res) => {
+  res.send("Working!");
+});
+
+module.exports = server;
