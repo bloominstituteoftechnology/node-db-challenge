3ee2b8ada5b279f5903d044341760a959787e3fe
Jesse Theisen <theiseja@gmail.com>
2019-02-20 20:46:36 -0600

    MVP: Created migrations & seeds. Running knex now to test

--- data/migrations/20190219214446_add_empty_action_array.js
+++ data/migrations/20190219214446_add_empty_action_array.js
@@ -1,8 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.table('projects', table => {
+      table.speficType('actions', 'jsonb[]')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('projects')
 };
--- data/seeds/actions.js
+++ data/seeds/actions.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('actions').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('actions').insert([
+        {todo_description: 'study js', notes: 'Start studying JS methods more', is_completed: false},
+        {todo_description: 'Cook dinner', notes: 'Cook dinner without setting off smoke alarm and ruining it', is_completed: true},
+        {todo_description: 'Work on RDBMS Sprint challenge for class', notes: 'Finish building the database and writing the routes', is_completed: false}
       ]);
     });
 };
