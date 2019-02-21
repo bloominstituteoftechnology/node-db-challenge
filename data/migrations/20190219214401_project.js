4f3e01859ce062bcfb6401b5d2fb5a38f9174af5
Jesse Theisen <theiseja@gmail.com>
2019-02-20 20:32:46 -0600

    MVP: Started building database with knex

--- data/migrations/20190219214401_project.js
+++ data/migrations/20190219214401_project.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('projects', table => {
+      table.increments('id')
+      table.string('name', 128).notNullable()
+      table.string('description').notNullable()
+      table.boolean('is_complete').defaultTo(false)
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('projects')
 };
--- data/migrations/20190219214421_action.js
+++ data/migrations/20190219214421_action.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('actions', table => {
+      table.increments('id')
+      table.string('todo_description', 128).notNullable()
+      table.text('notes').notNullable()
+      table.boolean('is_completed').defaultTo(false)
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('actions')
 };
