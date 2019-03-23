c17b00456f7bbf5fb4c4178f15c48f440b1621c9
Oliver Abreu <oliverabreu@Olivers-MacBook.attlocal.net>
2019-03-23 13:59:53 -0400

    This commit should initialize 'index.js' and allow nomdeon to update server automatically on save

---
+++ index.js
@@ -0,0 +1,27 @@
+// IMPORT DEPENDENCIES
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+
+// IMPORT KNEX CONFIGURATION
+const knexConfig = {
+client: 'sqlite3',
+connection: {
+    filename: './data/rolex.db3',
+},
+useNullAsDefault: true, // needed for sqlite
+};
+
+// DEFINE DATABASE
+const dbConfig = require("./knexfile.js")
+const db = knex(dbConfig.development);
+
+// DEFINE SERVER
+const server = express();
+
+// APPLY MIDDLEWARE
+server.use(helmet());
+server.use(express.json());
+
+const port = process.env.PORT || 9090;
+server.listen(port, () => console.log(`\nrunning on ${port}\n`));
