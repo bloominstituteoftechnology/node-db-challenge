4b4f98c22a62dead3f4cdb4a15202593415e0e8e
Kseniya Platonava <ksu2008@gmail.com>
2019-03-22 22:46:01 -0400

    Add addProject And Endpoint Post The Unique Project

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-//GET
+//GET ALL Projects
 
 server.get('/api/projects', (req, res) => {
   db.getProjects()
@@ -19,6 +19,15 @@ server.get('/api/projects', (req, res) => {
     .catch(err => res.status(500).json(err));
   });
 
+  //POST for adding projects
+
+  server.post('/api/project', async (req, res) => {
+    db.addProject(req.body)
+        .then(project => {
+          res.status(201).json(project);
+        })
+    .catch(err => res.status(500).json(err));
+  });
 
 const port = process.env.PORT || 9000;
 server.listen(port, () => console.log(`\nrunning on ${port}\n`));
