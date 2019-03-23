9e2ae4284a7676eb3fd93bef91481ae5fc477f82
Jesse Theisen <theiseja@gmail.com>
2019-03-23 11:11:02 -0500

    Cleaned up code a bit, finishing up stretch goals on this one

--- data/Routes/actionRoutes.js
+++ data/Routes/actionRoutes.js
@@ -1,94 +1,94 @@
-const express = require('express')
-const knex = require('knex')
-const knexConfig = require('../../knexfile')
-const DB = knex(knexConfig.development)
-const router = express.Router()
+const express = require("express");
+const knex = require("knex");
+const knexConfig = require("../../knexfile");
+const DB = knex(knexConfig.development);
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    DB('actions')
+router.get("/", (req, res) => {
+  DB("actions")
     .then(actions => {
-        res.json(actions)
+      res.json(actions);
     })
     .catch(() => {
-        res.status(500).json({error: 'Actions cannot be retrieved from the DB.'})
-    })
-})
+      res
+        .status(500)
+        .json({ error: "Actions cannot be retrieved from the DB." });
+    });
+});
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params
-    DB('actions')
-      .where('actions.id', id)
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  DB("actions")
+    .where("actions.id", id)
+    .then(project => {
+      const thisProject = project[0];
+      DB("actions")
+        .select(
+          "actions.id",
+          "actions.description",
+          "actions.notes",
+          "actions.is_complete",
+          "actions.project_id"
+        )
+        .where("actions.project_id", id)
+        .then(actions => {
+          if (!thisProject) {
+            res.status(404).json({ err: "invalid project id" });
+          } else {
+            res.json({
+              id: thisProject.id,
+              name: thisProject.name,
+              description: thisProject.description,
+              is_complete: thisProject.is_complete,
+              actions: actions
+            });
+          }
+        });
+    })
+    .catch(() => {
+      res
+        .status(404)
+        .json({ error: "Info about this project could not be retrieved." });
+    });
+});
+
+router.post("/", (req, res) => {
+  const action = req.body;
+  if (
+    action.description &&
+    action.project_id &&
+    action.notes &&
+    typeof action.is_complete === "boolean"
+  ) {
+    DB("projects")
+      .where({ id: action.project_id })
+      .first()
       .then(project => {
-        const thisProject = project[0]
-        DB('actions')
-          .select(
-            'actions.id',
-            'actions.description',
-            'actions.notes',
-            'actions.is_complete',
-            'actions.project_id'
-          )
-          .where('actions.project_id', id)
-          .then(actions => {
-            if (!thisProject) {
-              res.status(404).json({ err: 'invalid project id' })
-            } else {
-              res.json({
-                id: thisProject.id,
-                name: thisProject.name,
-                description: thisProject.description,
-                is_complete: thisProject.is_complete,
-                actions: actions
-              })
-            }
-          })
+        if (!project) {
+          res.status(404).json({ err: "invalid project id" });
+        } else {
+          DB("actions")
+            .insert(action)
+            .then(id => {
+              if (id[0]) {
+                DB("actions")
+                  .where("actions.id", id[0])
+                  .then(action => {
+                    res.status(201).json(action);
+                  });
+              }
+            })
+            .catch(() => {
+              res.status(500).json({ err: "Error creating action" });
+            });
+        }
       })
       .catch(() => {
-        res
-          .status(404)
-          .json({ error: 'Info about this project could not be retrieved.' })
-      })
-  })
-  
+        res.status(500).json({ err: "Failed to insert action" });
+      });
+  } else {
+    res.status(404).json({ message: "Provide all fields" });
+  }
+});
 
-router.post('/', (req, res) => {
-    const action = req.body
-    if (
-      action.description &&
-      action.project_id &&
-      action.notes &&
-      typeof action.is_complete === 'boolean'
-    ) {
-      DB('projects')
-        .where({ id: action.project_id })
-        .first()
-        .then(project => {
-          if (!project) {
-            res.status(404).json({ err: 'invalid project id' })
-          } else {
-            DB('actions')
-              .insert(action)
-              .then(id => {
-                if (id[0]) {
-                  DB('actions')
-                    .where('actions.id', id[0])
-                    .then(action => {
-                      res.status(201).json(action)
-                    })
-                }
-              })
-              .catch(() => {
-                res.status(500).json({ err: 'Error creating action' })
-              })
-          }
-        })
-        .catch(() => {
-          res.status(500).json({ err: 'Failed to insert action' })
-        })
-    } else {
-      res.status(404).json({ message: 'Provide all fields' })
-    }
-  })
-
-
-module.exports = router
+module.exports = router;
--- data/Routes/projectRoutes.js
+++ data/Routes/projectRoutes.js
@@ -1,72 +1,82 @@
-const express = require('express')
-const knex = require('knex')
-const knexConfig = require('../../knexfile')
-const DB = knex(knexConfig.development)
-const router = express.Router()
+const express = require("express");
+const knex = require("knex");
+const knexConfig = require("../../knexfile");
+const DB = knex(knexConfig.development);
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    DB('projects')
+router.get("/", (req, res) => {
+  DB("projects")
     .then(projects => {
-        res.json(projects)
+      res.json(projects);
     })
     .catch(() => {
-        res.status(500).json({error: 'Projects cannot be retrieved from the DB.'})
-    })
-})
+      res
+        .status(500)
+        .json({ error: "Projects cannot be retrieved from the DB." });
+    });
+});
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params
-    DB('projects')
-      .where('projects.id', id)
-      .then(project => {
-        const thisProject = project[0]
-        DB('actions')
-          .select(
-            'actions.id',
-            'actions.description',
-            'actions.notes',
-            'actions.is_complete',
-            'actions.project_id'
-          )
-          .where('actions.project_id', id)
-          .then(actions => {
-            if (!thisProject) {
-              res.status(404).json({ err: 'A project with that ID could not be found in the DB.' })
-            } else {
-              res.json({
-                id: thisProject.id,
-                name: thisProject.name,
-                description: thisProject.description,
-                is_complete: thisProject.is_complete,
-                actions: actions
-              })
-            }
-          })
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  DB("projects")
+    .where("projects.id", id)
+    .then(project => {
+      const thisProject = project[0];
+      DB("actions")
+        .select(
+          "actions.id",
+          "actions.description",
+          "actions.notes",
+          "actions.is_complete",
+          "actions.project_id"
+        )
+        .where("actions.project_id", id)
+        .then(actions => {
+          if (!thisProject) {
+            res
+              .status(404)
+              .json({
+                err: "A project with that ID could not be found in the DB."
+              });
+          } else {
+            res.json({
+              id: thisProject.id,
+              name: thisProject.name,
+              description: thisProject.description,
+              is_complete: thisProject.is_complete,
+              actions: actions
+            });
+          }
+        });
+    })
+    .catch(() => {
+      res
+        .status(404)
+        .json({ error: "Info about this project could not be retrieved." });
+    });
+});
+
+router.post("/", (req, res) => {
+  const project = req.body;
+  if (project.name && project.description && project.is_complete) {
+    DB("projects")
+      .insert(project)
+      .then(id => {
+        res.status(201).json({ id: id[0] });
       })
       .catch(() => {
         res
-          .status(404)
-          .json({ error: 'Info about this project could not be retrieved.' })
-      })
-  })
+          .status(500)
+          .json({ error: "Failed to insert the project into the DB." });
+      });
+  } else {
+    res
+      .status(400)
+      .json({
+        error:
+          "Please provide a name, description and if project is completed or not."
+      });
+  }
+});
 
-  router.post('/', (req, res) => {
-      const project = req.body
-      if(project.name && project.description && project.is_complete) {
-          DB('projects')
-          .insert(project)
-          .then(id => {
-              res.status(201).json({ id: id[0]})
-          })
-          .catch(() => {
-              res.status(500).json({error: 'Failed to insert the project into the DB.'})
-          })
-      } else {
-          res.status(400).json({error: 'Please provide a name, description and if project is completed or not.'})
-      }
-  })
-  
-  
-
-
-module.exports = router
+module.exports = router;
