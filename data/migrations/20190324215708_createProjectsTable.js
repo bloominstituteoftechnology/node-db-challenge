9da3a04a7f7d170ba5310b9458f29c2a99446f6c
kammagik <kammagik@yahoo.com>
2019-03-24 22:02:59 -0400

    migrations created

---
+++ data/migrations/20190324215708_createProjectsTable.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('Projects', table => {
+        table.increments();
+        table.string('name').unique().notNullable();
+        table.string('description',200).notNullable();
+        table.boolean('completed');
+})
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('Projects');
+};
