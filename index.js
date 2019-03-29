e235c622e008083958c74fba016b27670bc605e8
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-03-29 12:59:43 -0400

    finish post projects ep

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = express(); //server
 const db = knex(knexConfig.development); //db
 
 server.use(helmet(), express.json()); //midware
-
+///////////////////////////////////////////////////////////////////////
 server.post('/api/projects', (request, response) => {
     let { name, description, complete } = request.body; // decon
     if ( !name || name.length < 4 ) {
@@ -20,6 +20,17 @@ server.post('/api/projects', (request, response) => {
     if ( !complete ) {
         complete = false;
     }
+    const newProject = { name, description, complete }; // construct obj
+    db.insert(newProject) // db prom
+    .into('project')
+    .then( created => {
+        if ( !created || created.length < 1 ) {
+            return response.status(400).json({errorMessage: "Project not created"})
+        }
+        response.status(201).json(created[0]);
+    })
+    .catch( error => response.status(500).json(error) );
+});
 
 
 const port = 4000;
