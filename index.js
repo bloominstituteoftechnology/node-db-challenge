f2f72336a646ae4776b6d236cde8db6dac472c22
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-03-29 12:51:45 -0400

    post projects endpoint

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ const server = express(); //server
 const db = knex(knexConfig.development); //db
 
 server.use(helmet(), express.json()); //midware
+
+server.post('/api/projects', (request, response) => {
+    let { name, description, complete } = request.body; // decon
+    if ( !name || name.length < 4 ) {
+        return response.status(400).json({ errorMessage: "Project name must be at least 4 characters." })
+    }
+    if ( !description ) {
+        description = "";
+    }
+    if ( !complete ) {
+        complete = false;
+    }
+
+
+const port = 4000;
+ server.listen(port, () => console.log(`server rolling on port ${port}`));
