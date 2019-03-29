a45a11d1dc52e8318a32d176254ff3eae9ebe16d
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-03-29 12:26:19 -0400

    knex init

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,44 @@
+// Update with your config settings.
+
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/projects.db3'
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
     },
     pool: {
-    afterCreate: (conn, cb) =>
-       conn.run('PRAGMA foreign_keys = ON', cb) },
+      min: 2,
+      max: 10
+    },
     migrations: {
-      directory: './data/migrations'
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
     },
-    seeds: {
-      directory: './data/seeds'
+    pool: {
+      min: 2,
+      max: 10
     },
-    useNullAsDefault: true
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
+
 };
