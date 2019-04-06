3b095fe2f692e5cfe6a8bd8dbc521bbbd6a5edbf
Eiosx <eiosxenzo@gmail.com>
2019-04-05 23:37:45 -0400

    Finished MVP. Answered Essay Questions.

---
+++ Answers.md
@@ -0,0 +1,22 @@
+## Self-Study/Essay Questions
+
+Demonstrate your understanding of this week's concepts by answering the following free-form questions. Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your project manager.
+
+1. Explain the difference between `RDBMS` and `SQL`.
+
+Relational Database Managements Systems are just a collection of data stored in a table format.
+
+String Query Language is the syntax used to manage databases and their data.
+
+2. Why do tables need a `primary key`?
+
+To make it easier to identify records and allow for relationships between tables. The primary key allows records between two tables to be more easily linked.
+
+3. What is the name given to a table column that references the primary key on another table.
+
+Foreign key
+
+4. What do we need in order to have a _many to many_ relationship between two tables.
+
+A third table that hosts foreign keys to both tables
+
--- index.js
+++ index.js
@@ -19,47 +19,66 @@ server.post('/projects', (req, res) => {
     const project = req.body;
 
     db.insert(project)
-    .into('projects')
-    .then(response => {
-        res.status(201).json(response);
-    })
-    .catch(err => {
-        res.status(500).json({
-            errorMessage: 'Could not insert project'
+        .into('projects')
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({
+                errorMessage: 'Could not insert project'
+            });
         });
-    });
 });
 
 server.post('/actions', (req, res) => {
     const action = req.body;
 
     db.insert(action)
-    .into('actions')
-    .then(response => {
-        res.status(201).json(response);
-    })
-    .catch(err => {
-        res.status(500).json({
-            errorMessage: 'Could not insert action'
+        .into('actions')
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({
+                errorMessage: 'Could not insert action'
+            });
         });
-    });
 });
 
 server.get('/projects/:id', (req, res) => {
     const { id } = req.params;
 
-    db('projects')
-      .where({id})
-      .then(response => {
-          res.json(response)
-      })
-      .catch(err => {
-          res.status(500).json({
-              errorMessage: `Could not get project with id:${id}`
-          });
-      });
+    db('actions')
+        .where({ project_id: id })
+        .then(actions => {
+            let actionsCopy = actions;
+            for (let i = 0; i < actionsCopy.length; i++) {
+                if (actionsCopy[i].completed) {
+                    actionsCopy[i].completed = true;
+                } else {
+                    actionsCopy[i].completed = false;
+                }
+            }
+            db('projects')
+                .where({ id })
+                .then(response => {
+                    let projectCopy = response[0];
+                    if (projectCopy.completed) {
+                        projectCopy.completed = true;
+                    } else {
+                        projectCopy.completed = false;
+                    }
+                    let projectWithActions = { ...projectCopy, actions: actionsCopy }
+                    res.json(projectWithActions)
+                });
+        })
+        .catch(err => {
+            res.status(500).json({
+                errorMessage: "There was an error when trying to find the project."
+            });
+        });
 });
 
-server.listen(PORT, function(){
+server.listen(PORT, function () {
     console.log(`\n API Listening on http://localhost:${PORT}\n`);
 });
