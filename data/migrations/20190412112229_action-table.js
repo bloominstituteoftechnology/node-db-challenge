c5243ad6e716ac6353a179c0d7939cbd9b35a835
Marc Thiongo <marcthuo@gmail.com>
2019-04-12 17:57:26 -0400

    migrations

--- data/migrations/20190412112229_action-table.js
+++ data/migrations/20190412112229_action-table.js
@@ -1,7 +1,8 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('actions', function(tbl) {
         
-      tbl.increments();
+      tbl.increments()
+      .unique()
   
       tbl
         .string('description')
--- data/migrations/20190412112313_project-table.js
+++ data/migrations/20190412112313_project-table.js
@@ -2,7 +2,8 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('projects', function(tbl) {
         
-      tbl.increments();
+      tbl.increments()
+      .unique()
   
       tbl
         .string('name', 128)
