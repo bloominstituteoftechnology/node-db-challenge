317c20b896a8ab2cad895db4db3dd9a3bf39c639
Marc Thiongo <marcthuo@gmail.com>
2019-04-12 11:53:00 -0400

    server up, secondary setup complete

---
+++ action/action-router.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const knex = require('knex');
+
+const knexConfig = {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+        filename: './data/lambda.sqlite3'
+    }
+}
+
+const db = knex(knexConfig);
+
+//check
+router.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+router.post
+
+module.exports = router;
