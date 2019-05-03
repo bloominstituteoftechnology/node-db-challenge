7be6d971fd8fe904a30b65f7d15b15f33b11642d
Alexnadria Supriana <alexa9410@gmail.com>
2019-05-03 13:14:29 -0400

    Create server.js

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const actionRouter = require("../action/action-router.js");
+const projectRouter = require("../project/project-router.js");
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+server.use("/api/action", actionRouter);
+server.use("/api/project", projectRouter);
+
+//check
+server.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+module.exports = server;
