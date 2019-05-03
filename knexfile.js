71523a00cd4a8caa7b194a2998cc328c88ce7b43
Alexnadria Supriana <alexa9410@gmail.com>
2019-05-03 13:59:22 -0400

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,43 @@
+// Update with your config settings.
+
 module.exports = {
   development: {
     client: "sqlite3",
-    useNullAsDefault: true,
     connection: {
-      filename: "./data/lambda.sqlite3"
+      filename: "./dev.sqlite3"
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
+      min: 2,
+      max: 10
     },
     migrations: {
-      directory: "./data/migrations"
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
     }
   }
 };
