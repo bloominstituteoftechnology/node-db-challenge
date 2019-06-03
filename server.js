162cd34c7890653697b18c0f1c39ae1df1ce0a4d
raberin <rmvaberin@gmail.com>
2019-06-02 18:45:14 -0700

    Added index.js and server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const projectsRouter = require("./data/projects/projects-router.js");
+const actionsRouter = require("./data/actions/actions-router.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/api/projects", projectsRouter);
+server.use("/api/actions", actionsRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ hello: "World!" });
+});
+
+module.exports = server;
