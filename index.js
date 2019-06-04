cf8ab828691bf0c3c361fb6318838add6e8e269a
RodBromberg <rod.bromberg@yahoo.com>
2019-06-04 00:14:33 -0400

    mvp

--- index.js
+++ index.js
@@ -33,75 +33,106 @@ server.get('/api/projects', async(req, res) => {
 
 
 server.get('/api/projects/:id', async(req, res) => {
-            const { id } = req.params
-            const count = await db('projects')
-                .where({ id })
-                .first()
-                .then(project => {
-                        if (project) {
-                            db('action')
-                                .where({ project_id: id })
+    try {
+        const project = await db('projects')
+            .where({ id: req.params.id })
+            .first()
+            .then(project => {
+                if (project) {
+                    db('action')
+                        .where({ project_id: req.params.id })
+                        .then(action => {
+                            project.action = action
+                            res.status(200).json(project)
                         })
-                })
-
-
-
-
-
-
-        // create project
-        server.post('/api/projects', async(req, res) => {
-            try {
-                const [id] = await db('projects').insert(req.body);
-
-                const project = await db('projects')
-                    .where({ id })
-                    .first();
-
-                res.status(201).json(project);
-            } catch (error) {
-                const message = errors[error.errno] || 'We ran into an error';
-                res.status(500).json({ message, error });
-            }
-        });
-
-        server.put('/api/projects/:id', async(req, res) => {
-            try {
-                const count = await db('projects')
-                    .where({ id: req.params.id })
-                    .update(req.body);
-
-                if (count > 0) {
-                    const project = await db('projects')
-                        .where({ id: req.params.id })
-                        .first();
-
-                    res.status(200).json(project);
+                        .catch(err => {
+                            res.status(500).json({ err })
+                        })
                 } else {
-                    res.status(404).json({ message: 'Records not found' });
+                    res.status(404).json({ message: 'User Not FOund' })
                 }
-            } catch (error) {}
-        });
-
-
-        server.delete('/api/projects/:id', async(req, res) => {
-            try {
-                const count = await db('projects')
-                    .where({ id: req.params.id })
-                    .del();
-                res.status(200).json({ message: 'Deleted Succesfully' })
-                if (count > 0) {
-                    res.status(204).end();
-                } else {
-                    res.status(404).json({ message: 'Records not found' });
-                }
-            } catch (error) {
-                res.status(500).json({ message: 'Not Able to fulfill request' })
-            }
-        });
+            })
+    } catch (err) {
+        res.status(500).json({ err });
+    }
+})
 
 
 
 
 
-        const port = process.env.PORT || 5000; server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+
+// create project
+server.post('/api/projects', async(req, res) => {
+    try {
+        const [id] = await db('projects').insert(req.body);
+
+        const project = await db('projects')
+            .where({ id })
+            .first();
+
+        res.status(201).json(project);
+    } catch (error) {
+        const message = errors[error.errno] || 'We ran into an error';
+        res.status(500).json({ message, error });
+    }
+});
+
+server.put('/api/projects/:id', async(req, res) => {
+    try {
+        const count = await db('projects')
+            .where({ id: req.params.id })
+            .update(req.body);
+
+        if (count > 0) {
+            const project = await db('projects')
+                .where({ id: req.params.id })
+                .first();
+
+            res.status(200).json(project);
+        } else {
+            res.status(404).json({ message: 'Records not found' });
+        }
+    } catch (error) {}
+});
+
+
+server.delete('/api/projects/:id', async(req, res) => {
+    try {
+        const count = await db('projects')
+            .where({ id: req.params.id })
+            .del();
+        res.status(200).json({ message: 'Deleted Succesfully' })
+        if (count > 0) {
+            res.status(204).end();
+        } else {
+            res.status(404).json({ message: 'Records not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Not Able to fulfill request' })
+    }
+});
+
+
+server.post('/api/actions', async(req, res) => {
+    try {
+        const [id] = await db('action').insert(req.body);
+
+        const project = await db('action')
+            .where({ id })
+            .first();
+
+        res.status(201).json(project);
+    } catch (error) {
+        const message = errors[error.errno] || 'We ran into an error';
+        res.status(500).json({ message, error });
+    }
+});
+
+
+
+
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
