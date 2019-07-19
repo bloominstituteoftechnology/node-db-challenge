2fbc56887fd13f7610d40b360dbe857f98fe0711
Michael Hart <michael@mhart.dev>
2019-07-19 11:18:17 -0400

    Table built: projects

---
+++ data/migrations/20190719110559_001-create-projects-table.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('projects', tbl => {
+      tbl.increments();
+
+      tbl
+        .string('name', 64)
+        .notNullable();
+      
+      tbl
+        .string('description', 255);
+      
+      tbl
+        .boolean('completed')
+        .defaultTo(false);
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('projects');
+};
