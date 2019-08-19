0af4b03ec68ebb8e89fa0384f427e36b363c1889
Ihab Ali <ihab@live.com>
2019-08-19 00:39:33 -0500

    configured migration file and created db by running 'knex migrate:latest'

--- data/migrations/20190819003459_create-projects-table.js
+++ data/migrations/20190819003459_create-projects-table.js
@@ -1,8 +1,58 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("projects", tbl => {
+      tbl.increments();
+      tbl
+        .text("name")
+        .notNullable()
+        .unique();
+      tbl.text("description");
+      tbl.boolean("completed").defaultTo(false);
+    })
+    .createTable("tasks", tbl => {
+      tbl.increments();
+      tbl.text("description").notNullable();
+      tbl.text("notes");
+      tbl.boolean("completed").defaultTo(false);
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .references("projects.id")
+        .notNullable()
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("resources", tbl => {
+      tbl.increments();
+      tbl
+        .text("name", 128)
+        .notNullable()
+        .unique();
+      tbl.text("description");
+    })
+    .createTable("projects_resources", tbl => {
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("projects.id")
+        .onDelete("CASCADE")
+        .onDelete("CASCADE");
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("resourses.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl.primary(["project_id", "resource_id"]);
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("projects_resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
 };
