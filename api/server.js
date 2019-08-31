a0cddc09201026d9d112b273d24e09054d7af3e1
Tyler Turnipseed <tyler@techturnip.us>
2019-08-31 13:02:27 -0400

    created projects model and projects routing

--- api/server.js
+++ api/server.js
@@ -1,10 +1,23 @@
 // bring in express
+// --------------------------------------------|
 const express = require('express')
-// bring in router
+
+// bring in routers
+// --------------------------------------------|
+const ProjectsRouter = require('../projects/projects-router.js')
+
 // initialize express server
+// --------------------------------------------|
 const server = express()
+
 // define global middlewares
+// --------------------------------------------|
 server.use(express.json())
+
 // define routes
+// --------------------------------------------|
+server.use('/api/projects', ProjectsRouter)
+
 // export server
+// --------------------------------------------|
 module.exports = server
---
+++ projects/projects-model.js
@@ -0,0 +1,21 @@
+// DATABASE HELPER METHODS - Projects =========|
+// --------------------------------------------|
+const db = require('./data/db-config.js')
+// --------------------------------------------|
+const find = () => db('projects')
+// --------------------------------------------|
+const findById = id =>
+  db('projects')
+    .where({ id })
+    .first()
+// --------------------------------------------|
+const add = project =>
+  db('projects')
+    .insert(project)
+    .then(ids => findById(ids[0]))
+// --------------------------------------------|
+module.exports = {
+  find,
+  findById,
+  add
+}
