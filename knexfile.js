89c3602e96cecdb6ec2450f02803bde1f010e5da
AllHailMegatron <ahmadrthomp@gmail.com>
2019-08-30 20:36:15 -0400

    Added data & knexfile

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/projects.db3'
+    }
+  },
+
+
+};
--- projects/helper-model.js
+++ projects/helper-model.js
@@ -1,4 +1,4 @@
-const db = require('../data/db.config');
+const db = require('../data/db-config.js');
 
 module.exports = {
     find,
