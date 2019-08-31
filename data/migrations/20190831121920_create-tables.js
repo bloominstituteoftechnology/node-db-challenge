6b3cd2003a2bd529597099b7c38eaaabd0e4366d
Tyler Turnipseed <tyler@techturnip.us>
2019-08-31 12:46:19 -0400

    define db schema

---
+++ data/migrations/20190831121920_create-tables.js
@@ -0,0 +1,50 @@
+exports.up = function(knex) {
+  return (
+    knex.schema
+      // a project can have multiple tasks and multiple resources
+      .createTable('projects', tbl => {
+        tbl.increments()
+        tbl.string('name').notNullable()
+        tbl.string('desc')
+        tbl.boolean('completed')
+      })
+      // a resource can be used in multiple projects
+      .createTable('resources', tbl => {
+        tbl.increments()
+        tbl.string('name').notNullable()
+        tbl.string('desc')
+      })
+      // a task belongs only to one project
+      .createTable('tasks', tbl => {
+        tbl.increments()
+        tbl.string('desc').notNullable()
+        tbl.string('notes')
+        tbl.boolean('completed')
+        // foreign key
+        tbl
+          .integer('proj_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('projects')
+      })
+      // intermediary table
+      .createTable('projects_resources', tbl => {
+        tbl
+          .integer('proj_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('projects')
+        tbl
+          .integer('res_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('resources')
+        tbl.primary(['proj_id', 'animal_id'])
+      })
+  )
+}
+
+exports.down = function(knex) {}
