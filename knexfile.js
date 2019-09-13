54779d181be7099a57ea6ae1b4b76e9e440426cc
jose restrepo <arturo.restrepo@gmail.com>
2019-09-13 13:46:27 -0500

    MVP

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,21 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
+    useNullAsDefault: true, // needed for sqlite
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: "./data/projects.db3"
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: "./data/migrations"
     },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    // sqlite will not enforce Foreign Keys by default
     pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done); // turns on the FK enforcement
+      }
     }
   }
-
 };
