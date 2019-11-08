7665cdd547440cb63dad9962492a3477cda70006
Emily_Richard <richard.emily.e@gmail.com>
2019-11-08 13:43:17 -0500

    all CRUD operations functional except for PUT

--- projects/model.js
+++ projects/model.js
@@ -34,11 +34,17 @@ function getTaskById(id) {
 
 function getTasksByProject(id) {
     return db('tasks as t')
-    .where({ id })
+    .where({ project_id: id })
     .join('projects as p', 'p.id', 't.project_id')
     .select('t.id', 't.description', 't.notes', 't.completed');
 }
 
+function getResourcesByProject(id) {
+    return db('projects-resources as pr')
+    .where({ project_id: id })
+    .join('resources as r', 'r.id', 'pr.resource_id')
+    .select('r.id', 'r.name', 'r.description')
+}
 
 function addProject(project) {
     return db('projects')
@@ -64,6 +70,45 @@ function addTask(task) {
     });
 }
 
+function deleteProject(id) {
+    return db('projects')
+    .where({id})
+    .del();
+}
+
+function deleteResource(id) {
+    return db('resources')
+    .where({id})
+    .del()
+}
+
+function deleteTask(id) {
+    return db('tasks')
+    .where({id})
+    .del()
+}
+
+function updateProject(update, id) {
+    return db('projects')
+    .where({ id })
+    .update(update)
+    .then((ids) => ids);
+}
+
+function updateResource(update, id) {
+    return db('resources')
+    .where({ id })
+    .update(update)
+    .then(count => count);
+}
+
+function updateTask(update, id) {
+    return db('tasks')
+    .where({ id })
+    .update(update)
+    .then(count => count);
+}
+
 module.exports = {
     getProjects,
     getResources,
@@ -72,7 +117,14 @@ module.exports = {
     getResourceById,
     getTaskById,
     getTasksByProject,
+    getResourcesByProject,
     addProject,
     addResource,
-    addTask
+    addTask,
+    deleteProject,
+    deleteResource,
+    deleteTask,
+    updateProject,
+    updateResource,
+    updateTask
 }
--- projects/router.js
+++ projects/router.js
@@ -57,38 +57,123 @@ router.get('/:id/tasks', validateProjectId, (req, res) => {
     const id = req.params.id;
     db.getTasksByProject(id)
     .then(tasks => {
+        tasks.length === 0 && res.status(200).json("Provided project ID has no associated tasks.")
         res.status(200).json(tasks);
     })
     .catch(err => res.status(500).json({ message: 'Unable to fetch tasks for project by ID.', error: err }));
 })
 
-router.post('/', (req, res) => {
+router.get('/:id/resources', validateProjectId, (req, res) => {
+    const id = req.params.id;
+    db.getResourcesByProject(id)
+    .then(resources => {
+        resources.length === 0 && res.status(200).json("Provided project ID has no associated resources.")
+        res.status(200).json(resources);
+    })
+    .catch(err => res.status(500).json({ message: 'Unable to fetch resources for project by ID.', error: err }));
+})
+
+router.post('/', validateProject, (req, res) => {
     const project = req.body;
     db.addProject(project)
     .then(response => {
-        res.status(200).json(response);
+        res.status(201).json(response);
     })
     .catch(err => res.status(500).json({ error: err }));
 })
 
-router.post('/resources', (req, res) => {
+router.post('/resources', validateResource, (req, res) => {
     const resource = req.body;
     db.addResource(resource)
     .then(response => {
-        res.status(200).json(response);
+        res.status(201).json(response);
     })
     .catch(err => res.status(500).json({ error: err }));
 })
 
-router.post('/tasks', (req, res) => {
+router.post('/tasks', validateTask, (req, res) => {
     const task = req.body;
     db.addTask(task)
     .then(response => {
-        res.status(200).json(response);
+        res.status(201).json(response);
     })
     .catch(err => res.status(500).json({ error: err }));
 })
 
+router.delete('/:id', validateProjectId, (req, res) => {
+    const id = req.params.id;
+    let count = 0;
+
+    db.getTasksByProject(id)
+    .then(tasks => {
+        tasks.forEach(task => {
+            const id = task.id;
+            db.deleteTask(id)
+            .then(response => {
+                console.log(`Task ${id} deleted`, response);
+                count += response;
+            })
+        })
+    })
+    .catch(err => res.status(500).json({ message: "Unable to delete tasks associated with project", error: err }));
+
+    db.deleteProject(id)
+    .then(response => {
+        count += response;
+        console.log(`Project ${id} deleted.`, response)
+        console.log(`${count} records deleted.`)
+        res.status(202).json(`${count} records deleted.`);
+    })
+    .catch(err => res.status(500).json({ error: err }));
+})
+
+router.delete('/resources/:id', validateResourceId, (req, res) => {
+    const id = req.params.id;
+    db.deleteResource(id)
+    .then(response => {
+        res.status(202).json(`${response} records deleted.`);
+    })
+    .catch(err => res.status(500).json({ error: err }));
+})
+
+router.delete('/tasks/:id', validateTaskId, (req, res) => {
+    const id = req.params.id;
+    db.deleteTask(id)
+    .then(response => {
+        res.status(202).json(`${response} records deleted.`);
+    })
+    .catch(err => res.status(500).json({ error: err }));
+})
+
+router.put('/project/:id', validateProjectId, validateProject, (req, res) => {
+    const id = req.params.id;
+    const update = req.body;
+    db.updateProject(update, id)
+    .then(response => {
+        res.status(200).json(`${response} records updated.`);
+    })
+    .catch(err => res.status(500).json({ error: err }));
+})
+
+router.put('/project/resources/:id', validateResourceId, validateResource, (req, res) => {
+    const id = req.params.id;
+    const update = req.body;
+    db.updateResource(update, id)
+    .then(response => {
+        res.status(200).json(`${response} records updated.`);
+    })
+    .catch(err => res.status(500).json({ error: err }));
+})
+
+router.put('/project/tasks/:id', validateTaskId, validateTask, (req, res) => {
+    const id = req.params.id;
+    const update = req.body;
+    db.updateTask(update, id)
+    .then(response => {
+        res.status(200).json(`${response} records updated.`);
+    })
+    .catch(err => res.status(500).json({ error: err }));
+})
 
 //MIDDLEWARE
 
@@ -120,6 +205,28 @@ function validateTaskId (req, res, next) {
         next();
     })
     .catch(err => res.status(500).json({ error: "Task with provided ID not found in database." }));
+}
+
+function validateProject (req, res, next) {
+    const project = req.body;
+    project.length === 0 && res.status(400).json('Please provide a project to add.')
+    !project.name && res.status(400).json('Please provide a name for the project to add.')
+    next();
+}
+
+function validateResource (req, res, next) {
+    const resource = req.body;
+    resource.length === 0 && res.status(400).json('Please provide a resource to add.')
+    !resource.name && res.status(400).json('Please provide a name for the resource to add.')
+    next();
+}
+
+function validateTask (req, res, next) {
+    const task = req.body;
+    task.length === 0 && res.status(400).json('Please provide a task to add.')
+    !task.project_id && res.status(400).json('Task must be associated with a project_id.')
+    !task.description && res.status(400).json('Please provide a description for the task to add.')
+    next();
 }
 
 module.exports = router;
