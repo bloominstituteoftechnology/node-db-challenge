bcaf722748b070562e57c3e654c3373d989b35d3
Chazaray Robinson <chaarayrobinson@Chazarays-MacBook-Pro.local>
2019-11-08 13:20:13 -0500

    project-model

---
+++ project/project-model.js
@@ -0,0 +1,41 @@
+const db = require('../data/db.config.js');
+
+module.exports = {
+    get,
+    getById,
+    add,
+    getTasks,
+    addTask
+};
+
+function get() {
+    return db('projects');
+}
+
+function getById(id) {
+    return db('projects').where({ id });
+}
+
+function add(project) {
+    return db('projects').insert(project);
+}
+
+function getTasks(id) {
+    return db('projects')
+        .innerJoin('tasks', 'tasks.project_id', 'projects.id')
+        .select(
+            'projects.project_name',
+            'projects.project_descripition',
+            'tasks.task_description',
+            'tasks.task_notes',
+            'tasks.completed',
+            'taks.projects_id'
+        )
+        .where({ project_id: id });
+}
+
+function addTask(id, task) {
+    return db('tasks')
+        .where({ project_id: id })
+        .insert(task);
+}
