705b94d270d630eaf6005bff16caac16f3adac0b
Dominick Bruno <dbruno@unseen.is>
2019-11-23 13:04:57 -0500

    All hail the internet! But I commented the parts I googled to show I understand it, I just wouldn't have thought to do it

--- helpers/projModel.js
+++ helpers/projModel.js
@@ -3,8 +3,17 @@ const db = require('../data/db-config');
 
 // Return all Projects
 function find() {
-    return db('project');
-  }
+  let query = db('project as p');
+
+  // code found on interwebs, commented to show understanding
+  // returns the array of projects which are then mapped over with intToBoolean for conversion
+  return query.then(projects => {
+      return projects.map(project =>
+          project = { ...project, complete: intToBoolean(project.complete) }
+
+      );
+  });
+}
 
 // Included to provide "under the hood" functionality so Add returns an object not an id
 function findById(id) {
@@ -24,9 +33,15 @@ function remove(id) {
     return db('project').where({ id }).del();
 }
 
+function intToBoolean(int) {
+
+  return int === 1 ? true : false;
+}
+
 module.exports = {
     find,
     findById,
     add,
-    remove
+    remove,
+    intToBoolean
 };
--- helpers/taskModel.js
+++ helpers/taskModel.js
@@ -3,8 +3,30 @@ const db = require('../data/db-config');
 
 // Return all Tasks
 function find() {
-  return db('task');
-  }
+ /* return db('task');
+  } */
+  let query = db('project')
+  // Join task and project on id row
+        .join('task', 'project.id', '=', 'task.project_id')
+  // Select fields needed, renamed to avoid conflicts & for readability
+        .select(
+            'project.name as project_name',
+            'project.description as project_description',
+            'task.description as task_description',
+            'task.notes as task_notes',
+            'task.completed as task_completed'
+        );
+  // Map over array of tasks to return list w/ bool converted
+    return query.then(tasks => {
+        return tasks.map(task =>
+            task = {
+                ...task, task_completed: intToBoolean(task.complete),
+
+            }
+
+        );
+    });
+}
 
 // Included to provide "under the hood" functionality so Add returns an object not an id
 function findById(id) {
@@ -24,9 +46,15 @@ function remove(id) {
     return db('task').where({ id }).del();
 }
 
+function intToBoolean(int) {
+
+  return int === 1 ? true : false;
+}
+
 module.exports = {
     find,
     findById,
     add,
-    remove
+    remove,
+    intToBoolean
 };
