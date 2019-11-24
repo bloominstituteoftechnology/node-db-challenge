a1555aab42e502fbeac1939ed75588e1b3ae9608
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2019-11-24 17:43:27 -0500

    added seeds for the project_resource table

---
+++ seeds/project_resource.js
@@ -0,0 +1,13 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex("projects_resources")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex("projects_resources").insert([
+        { project_id: 1, resource_id: 1 },
+        { project_id: 2, resource_id: 1 },
+        { project_id: 2, resource_id: 2 }
+      ]);
+    });
+};
