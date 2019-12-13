4cd45296cb393edaa942d3fe4916211d520b04b6
sooshe <sooshe@sooshebot.com>
2019-12-13 10:31:10 -0600

    add knexfile with knex init and update migrations

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/project-info.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        // this was in a couple projects of mine, which is why it's here
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
+  },
+};
