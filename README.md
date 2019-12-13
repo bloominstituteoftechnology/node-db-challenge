ad0987491c25ada7e0ee840c8142b22d2a7ecc43
juarezfrench <markedfrench@gmail.com>
2019-12-13 13:06:23 -0600

    Executed migration, built seeds, started on router.

--- README.md
+++ README.md
@@ -98,25 +98,25 @@ The API should return:
 
 A `project` is what needs to be done. We want to store the following data about a `project`:
 
-- [ ] a unique Id.
-- [ ] a name. This column is required.
-- [ ] a description.
-- [ ] a boolean that indicates if the project has been completed. This column cannot be NULL, the default value should be `false`.
+- [x] a unique Id.
+- [x] a name. This column is required.
+- [x] a description.
+- [x] a boolean that indicates if the project has been completed. This column cannot be NULL, the default value should be `false`.
 
 A `resource` is anything needed to complete a project, some examples are: a person, a tool, a meeting room or a software license. We want to store the following data about a `resource`:
 
-- [ ] a unique Id.
-- [ ] a name. This column is required.
-- [ ] a description.
+- [x] a unique Id.
+- [x] a name. This column is required.
+- [x] a description.
 
 The database should not allow resources with duplicate names.
 
 An `task` one of the steps needed to complete the project. We want to store the following data about an `task`.
 
-- [ ] a unique id.
-- [ ] a description of what needs to be done. This column is required.
-- [ ] a notes column to add additional information.
-- [ ] a boolean that indicates if the task has been completed. This column cannot be NULL, the default value should be `false`.
+- [x] a unique id.
+- [x] a description of what needs to be done. This column is required.
+- [x] a notes column to add additional information.
+- [x] a boolean that indicates if the task has been completed. This column cannot be NULL, the default value should be `false`.
 
 ## Stretch Problem
 
--- data/migrations/20191213114210_projects.js
+++ data/migrations/20191213114210_projects.js
@@ -1,8 +1,89 @@
-
 exports.up = function(knex) {
-  
+  return (
+    knex.schema
+
+      ///// projects table ///
+      .createTable("projects", tbl => {
+        tbl.increments();
+        tbl
+          .string("name", 128)
+          .notNullable()
+          .unique();
+        tbl
+          .string("description", 255)
+        tbl
+          .boolean("completed")
+          .notNullable()
+          .defaultTo(0);
+      })
+
+      /// resources table ///
+      .createTable("resources", tbl => {
+        tbl.increments();
+        tbl
+          .string("name", 128)
+          .notNullable()
+          .unique();
+        tbl.string("description", 255);
+        tbl
+          .integer("project_id")
+          .unsigned()
+          .unique()
+          .notNullable()
+          .references("id")
+          .inTable("projects")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+      })
+
+      /// tasks table///
+      .createTable("tasks", tbl => {
+        tbl.increments();
+       tbl
+       .string("description", 255)
+        .notNullable();
+        
+        tbl.string("notes", 255);
+
+        tbl
+          .boolean("completed")
+          .notNullable()
+          .defaultTo(0);
+
+        tbl
+          .integer("project_id")
+          .unsigned()
+          .notNullable()
+          .defaultTo(0)
+          .references("id")
+          .inTable("projects")
+          .onDelete("CASCADE")
+          .onUpdate("RESTRICT");
+      })
+  );
+  // .createTable("resources", tbl => {
+  //   tbl.increments();
+
+  //   tbl
+
+  //   tbl.unique(["project_id", "resource_id"]);
+  //   tbl
+  //     .integer("project_id")
+  //     .unsigned()
+  //     .notNullable()
+  //     .references("id")
+  //     .inTable("projects")
+  //     .onDelete("CASCADE")
+  //     .onUpdate("CASCADE");
+  // });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("project-resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
 };
+
+exports.down = function(knex) {};
