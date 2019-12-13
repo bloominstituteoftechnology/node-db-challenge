7be9be38dbbc408650c0f8955ddf758e334d5b5d
Ale <alearanibar@gmail.com>
2019-12-13 08:49:27 -0800

    knex updated

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const helmet = require('helmet')
+
+const tasksRouter = require('../tasksFolder/tasks-router')
+
+const server = express()
+
+server.get('/', (req, res) => {
+    res.json({ message: `we don't need anymore build weeks....lol`})
+})
+
+server.use(helmet())
+server.use(express.json())
+
+server.use('/api/tasks', tasksRouter)
+
+module.exports = server
