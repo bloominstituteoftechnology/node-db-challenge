9f858ed905ebdbae38ec6a753766a8dbf1e82619
spettigrew <sara.pettigrew5@gmail.com>
2020-01-10 20:33:16 -0700

    Routers added

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const helmet = require("helmet");
 
-const projectRouter = require('');
+const projectRouter = require('./projects/project-router');
+const taskRouter = require('./tasks/task-router');
+const resourceRouter = require('./resources/resource-router');
+const proresRouter = require('./projects-resources/prores-router');
 
 require('dotenv').config()
 
 const server = express();
 
 server.use(express.json());
+
 server.use('/api/projects', projectRouter);
+server.use('/api/tasks', taskRouter);
+server.use('/api/resources', resourceRouter);
+server.use('/api/projects-resources', proresRouter);
 
 server.use((err, req, res, next) => {
     console.log(err)
