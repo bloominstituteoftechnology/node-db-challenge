0984cce0e6a9228001c793bd802eb26695d37311
Amy Barba <Amybarba70@gmail.com>
2020-01-11 23:41:04 -0800

    corrected index file added structure for routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server.js');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
     console.log(`Server is Listening on port ${PORT}...`);
