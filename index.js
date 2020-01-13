0400bc796c4083d90d05a37652c6ba0b97613b9f
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-01-12 17:51:37 -0800

    routes established, database schema built out, devdependencies

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const port = process.env.PORT || 4004;
 server.listen(port, () => {
     console.log('\n* Server started http://localhost:4004 *\n')
 });
-
+ 
 //should run dependencies for db and middleware 
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ server.use(express.json());
 server.use('/api/projects', projectRouter);
 server.use('/api/resources', resourceRouter);
 server.use('/api/tasks', taskRouter);
-
+ 
 
 server.get("/", (req, res) => {
     res.send(`<h1> Server: started on port 4004....</h2>`);
