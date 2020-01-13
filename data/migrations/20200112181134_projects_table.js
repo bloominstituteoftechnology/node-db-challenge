56bd5441655e50b92c3aa99b920384388267a34b
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-01-12 18:28:25 -0800

    seed knex error conflict

--- data/migrations/20200112181134_projects_table.js
+++ data/migrations/20200112181134_projects_table.js
@@ -1,8 +1,64 @@
+// many to many sql knex db create and export for schema
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('project_resources', tbl => {
+            tbl.increments();
+            tbl.string('name')
+                .notNullable();
+            tbl.string('project_description')
+                .notNullable()
+            tbl.boolean('completed')
+                .defaultTo(0);
+        })
+        .createTable('resources', tbl => {
+            tbl.increments();
+            tbl.string('resource_name')
+                .notNullable()
+                .unique();
+            tbl.string('resource_description')
+        })
+        .createTable('tasks', tbl => {
+            tbl.increments();
+            tbl.string('task_description')
+                .notNullable();
+            tbl.string('task_notes')
+                .notNullable()
 
-exports.up = function(knex) {
-  
+            tbl.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('projects')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+            tbl.boolean('completed')
+                .defaultTo(0);
+        })
+        .createTable('project_resources', tbl => {
+            tbl.increments();
+            tbl.integer('resource_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('resources')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE');
+            tbl.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('projects')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE');
+        })
 };
 
-exports.down = function(knex) {
-  
+
+exports.down = function (knex) {
+    return knex.schema
+        .dropTableIfExists('projects')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('projects_resources')
 };
+
