785c1bdbe720447fa0c7ba6c9cffd98d26a076f3
spettigrew <sara.pettigrew5@gmail.com>
2020-01-14 17:08:55 -0700

    Removed 'completed' column

--- data/seeds/002-task.js
+++ data/seeds/002-task.js
@@ -1,8 +1,8 @@
 
 exports.seed = async (knex) => {
   await knex('task').insert([
-    { id: 1, description: 'Dust all hard surfaces and window blinds', notes: "Dust inside vacuum afterwards with dirty duster.", completed: "false", project_id: 1 },
-    { id: 2, description: 'Vacuum all floors and stairs', notes: "Empty canister when finished and dust with dirty duster.", completed: "false", project_id: 2 },
-    { id: 3, description: 'Mop all hard floors', notes: "Use 1 gallon of hot water to 1 cup of distilled white vinegar", completed: "false", project_id: 3 }
+    { id: 1, description: 'Dust all hard surfaces and window blinds', notes: "Dust inside vacuum afterwards with dirty duster.", completed: false, project_id: 1 },
+    { id: 2, description: 'Vacuum all floors and stairs', notes: "Empty canister when finished and dust with dirty duster.", completed: false, project_id: 2 },
+    { id: 3, description: 'Mop all hard floors', notes: "Use 1 gallon of hot water to 1 cup of distilled white vinegar", completed: false, project_id: 3 }
       ])
 }
