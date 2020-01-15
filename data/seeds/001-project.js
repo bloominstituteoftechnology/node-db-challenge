5a8589734e07e64bfdd23557ada1aab23c4daa65
spettigrew <sara.pettigrew5@gmail.com>
2020-01-14 17:07:42 -0700

    Removed 'completed' column

--- data/seeds/001-project.js
+++ data/seeds/001-project.js
@@ -1,8 +1,8 @@
 
 exports.seed = async (knex) => {
  await knex('project').insert([
-        { id: 1, name: 'Dust', description: "Use Swiffer Dusters", completed: "false" },
-        { id: 2, name: 'Vacuum', description: "Use a Shark or a Dyson Vacuum", completed: "false" },
-        { id: 3, name: 'Mop', description: "Use a Libman Cloth Mop", completed: "false" },
+        { id: 1, name: 'Dust', description: "Use Swiffer Dusters" },
+        { id: 2, name: 'Vacuum', description: "Use a Shark or a Dyson Vacuum" },
+        { id: 3, name: 'Mop', description: "Use a Libman Cloth Mop" },
       ])
 }
