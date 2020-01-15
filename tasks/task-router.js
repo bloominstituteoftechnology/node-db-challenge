5c500f56313a7319940a45283a227ff887664c6f
spettigrew <sara.pettigrew5@gmail.com>
2020-01-14 17:10:27 -0700

    Switched parameters to match model

--- tasks/task-router.js
+++ tasks/task-router.js
@@ -47,7 +47,7 @@ router.post("/", async (req, res, next) => {
 router.put("/:id", async (req, res, next) => {
     try {
         const { id } = req.params //returns an object
-        const task = await taskModel.update(id, req.body)
+        const task = await Tasks.update(req.body, id)
 
         if (task) {
             res.json(task)
