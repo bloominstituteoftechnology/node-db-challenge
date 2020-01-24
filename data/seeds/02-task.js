61cb6d370f4e1d094b88495518e5af2fb8dba11e
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-24 09:32:55 +0000

    Task seed fleshed out

--- data/seeds/02-task.js
+++ data/seeds/02-task.js
@@ -1,13 +1,26 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('tasks').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('tasks').insert([{
+          task_description: "watch a video",
+          task_notes: "watch the first ten minutes",
+          completed: 0,
+          project_id: 2
+        },
+        {
+          task_description: "watch another video",
+          task_notes: "watch the first video before this one",
+          completed: 0,
+          project_id: 3 
+        },
+        {
+          task_description: "watch yet another video",
+          task_notes: "are you bored yet",
+          completed: 0,
+          project_id: 1
+        }
       ]);
     });
 };
