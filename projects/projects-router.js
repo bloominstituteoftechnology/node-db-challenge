d4f6be8856e5c740a9751174f4e6889d7e60fde4
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-24 10:35:43 +0000

    get all projects is working

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -3,4 +3,13 @@ const db = require('../data/db-config')
 const Projects = require ('./projects-model')
 const router = express.Router()
 
+router.get('/', async (req, res, next) => {
+    try {
+        const allProjects = await Projects.getAll()
+        res.json(allProjects)
+    } catch (error) {
+        next(error)
+    }
+})
+
 module.exports = router
