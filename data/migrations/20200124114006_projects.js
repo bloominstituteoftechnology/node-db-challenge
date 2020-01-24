ad5fc986965d15eb47598cffa19ee5b73bf52797
wchamber01 <williamchambers@gmail.com>
2020-01-24 12:01:25 -0500

    created migration file and built tables

---
+++ data/migrations/20200124114006_projects.js
@@ -0,0 +1,61 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("projects", tbl => {
+      tbl.increments();
+      tbl.string("name").notNullable();
+      tbl.text("description");
+      tbl
+        .boolean("completed")
+        .default()
+        .notNullable();
+    })
+    .createTable("tasks", tbl => {
+      tbl.increments();
+      tbl.text("description").notNullable();
+      tbl.string("notes");
+      tbl
+        .boolean("completed")
+        .default()
+        .notNullable();
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("project_details", tbl => {
+      tbl.increments();
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("resources")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("resources", tbl => {
+      tbl.increments();
+      tbl.string("name").notNullable();
+      tbl.text("description");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("resources")
+    .dropTableIfExists("project_details")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("projects");
+};
