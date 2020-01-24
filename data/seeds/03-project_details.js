5d72921beb1169a7a5c614cc621ccca7bfc22a78
wchamber01 <williamchambers@gmail.com>
2020-01-24 12:48:27 -0500

    built project_details seed file

--- data/seeds/03-project_details.js
+++ data/seeds/03-project_details.js
@@ -7,7 +7,38 @@ exports.seed = function(knex) {
       return knex("table_name").insert([
         {
           id: 1,
-          colName: "rowValue1"
+          project_id: 1,
+          resource_id: 1
+        },
+        {
+          id: 2,
+          project_id: 1,
+          resource_id: 2
+        },
+        {
+          id: 3,
+          project_id: 2,
+          resource_id: 1
+        },
+        {
+          id: 4,
+          project_id: 2,
+          resource_id: 2
+        },
+        {
+          id: 5,
+          project_id: 3,
+          resource_id: 1
+        },
+        {
+          id: 6,
+          project_id: 3,
+          resource_id: 3
+        },
+        {
+          id: 7,
+          project_id: 3,
+          resource_id: 4
         }
       ]);
     });
