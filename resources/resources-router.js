97cfb8d3446f7267258fd11bdbca5986eb9ed96c
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-24 11:17:37 +0000

    post request working for res

--- resources/resources-router.js
+++ resources/resources-router.js
@@ -22,6 +22,15 @@ router.get('/:id', async (req, res, next) => {
     }
 })
 
-router.post
+router.post('/', async (req, res, next) => {
+    try {
+
+        const id = await Resources.add(req.body)
+        res.status(200).json(await Resources.getById(id))
+
+    } catch (errpr) {
+        next (error)
+    }
+} ) 
 
 module.exports = router
