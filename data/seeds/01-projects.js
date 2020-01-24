c2674182369ee5ef64341141ff1ada7c1a763177
wchamber01 <williamchambers@gmail.com>
2020-01-24 12:18:25 -0500

    built projects seed file

--- data/seeds/01-projects.js
+++ data/seeds/01-projects.js
@@ -1,13 +1,29 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("projects")
+    .del()
+    .then(() => {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("projects").insert([
+        {
+          id: 1,
+          name: "Complete Lambda School",
+          description:
+            "Pass all remaining sprints and build weeks. Get through Labs, CS and finally Lambda X.",
+          completed: "false"
+        },
+        {
+          id: 2,
+          name: "Get a job",
+          description: "Must...find...work...",
+          completed: "false"
+        },
+        {
+          id: 3,
+          name: "Go back to Alaska",
+          description: "Take another vacation to Alaska.",
+          completed: "false"
+        }
       ]);
     });
 };
