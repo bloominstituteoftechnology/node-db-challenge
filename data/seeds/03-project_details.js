42c1be9f15c85751af36112640fbcd90ef6caa88
wchamber01 <williamchambers@gmail.com>
2020-01-24 12:42:12 -0500

    built resources seed file

--- data/seeds/03-project_details.js
+++ data/seeds/03-project_details.js
@@ -1,13 +1,14 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("table_name")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("table_name").insert([
+        {
+          id: 1,
+          colName: "rowValue1"
+        }
       ]);
     });
 };
--- data/seeds/04-resources.js
+++ data/seeds/04-resources.js
@@ -1,13 +1,30 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("resources")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("resources").insert([
+        {
+          id: 1,
+          name: "computer",
+          description: "preferably a new mac"
+        },
+        {
+          id: 2,
+          name: "internet",
+          description: "the faster the better"
+        },
+        {
+          id: 3,
+          name: "camping gear",
+          description: "should be lightweight"
+        },
+        {
+          id: 4,
+          name: "money",
+          description: "can always use more of this"
+        }
       ]);
     });
 };
