38eec685f3bcfd8d383f8094933dbb810e0346bb
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-24 13:07:58 +0000

    Fixed typos in task model undefined id

--- tasks/task-model.js
+++ tasks/task-model.js
@@ -13,8 +13,8 @@ function getAll() {
             "tasks.completed", "projects.proj_name", "projects.proj_description")
 }
 
-function getById() {
-    return db('task')
+function getById(id) {
+    return db('tasks')
         .join("projects", "projects.id", "tasks.project_id")
         .select("tasks.id", "tasks.task_description", "tasks.task_notes",
             "tasks.completed", "projects.proj_name", "projects.proj_description")
