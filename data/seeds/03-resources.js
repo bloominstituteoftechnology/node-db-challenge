1549cd8fb33b1e6d06cc83ac895b329d85aea7f5
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-24 09:55:18 +0000

    Resources seed

--- data/seeds/03-resources.js
+++ data/seeds/03-resources.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('resources').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('resources').insert([
+
+        {resource_name: 'blah', resource_description: 'dkjd'},
+        {resource_name: 'bla', resource_description: 'dkj'},
+        {resource_name: 'bl', resource_description: 'dk'},
+        {resource_name: 'ah', resource_description: 'kj'},
       ]);
     });
 };
