457e00c7c6f2187502fdeb625de83e32482b1024
wesley.moody <wgm3190@gmail.com>
2020-02-21 10:17:25 -0600

    index.js and dependencies

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const server = require("./server.js");
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`\n** API running on port: ${PORT} **\n`);
+});
