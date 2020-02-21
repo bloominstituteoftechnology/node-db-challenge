6ca841f4c3c8179aab888129c87de4da8f644099
wesley.moody <wgm3190@gmail.com>
2020-02-21 11:05:51 -0600

    dbconfig and port 2222

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const server = require("./server.js");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 2222;
 
 server.listen(PORT, () => {
   console.log(`\n** API running on port: ${PORT} **\n`);
