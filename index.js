bb6892579fe32482ad69bee5476661e10490fe11
wesley.moody <wgm3190@gmail.com>
2020-02-21 11:06:37 -0600

    typo

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require("./server.js");
 const PORT = process.env.PORT || 2222;
 
 server.listen(PORT, () => {
-  console.log(`\n** API running on port: ${PORT} **\n`);
+  console.log(`\n** Server running on port: ${PORT} **\n`);
 });
