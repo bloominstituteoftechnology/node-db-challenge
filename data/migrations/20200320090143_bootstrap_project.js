92918bf50e8e24204a90bb89ced5b5624d86762e
johnkirtley <kirtleyj16@gmail.com>
2020-03-20 09:16:18 -0700

    Table schema complete

--- data/migrations/20200320090143_bootstrap_project.js
+++ data/migrations/20200320090143_bootstrap_project.js
@@ -1,8 +1,50 @@
-
 exports.up = function(knex) {
-  
+	return knex.schema
+		.createTable('projects', tbl => {
+			tbl.increments();
+
+			tbl.string('name').notNullable();
+			tbl.string('description').notNullable();
+			tbl.boolean('completed');
+		})
+
+		.createTable('tasks', tbl => {
+			tbl.increments();
+			tbl.string('description').notNullable();
+			tbl.string('notes');
+			tbl.boolean('completed');
+			tbl
+				.integer('project_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('projects')
+				.onUpdate('CASCADE')
+				.onDelete('CASCADE');
+		})
+
+		.createTable('resources', tbl => {
+			tbl.increments();
+			tbl
+				.string('name')
+				.notNullable()
+				.unique();
+			tbl.string('description');
+			tbl
+				.integer('project_id')
+				.unsigned()
+				.notNullable()
+				.unique()
+				.references('id')
+				.inTable('projects')
+				.onUpdate('CASCADE')
+				.onDelete('CASCADE');
+		});
 };
 
 exports.down = function(knex) {
-  
+	return knex.schema
+		.dropTableIfExists('resources')
+		.dropTableIfExists('tasks')
+		.dropTableIfExists('projects');
 };
