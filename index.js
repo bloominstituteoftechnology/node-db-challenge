92ea756d3caa1f22446e7ac21747522026ed7016
Your name <a.m.verdine@gmail.com>
2020-05-03 21:41:10 -0400

    Build routers for each table

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const projectsRouter = require("./Routers/projects-router");
+const tasksRouter = require("./Routers/tasks-router");
+const resourcesRouter = require("./Routers/resources-router");
 
 const server = express();
 
 server.use(express.json());
 const port = process.env.PORT || 8000;
+
+server.use("/projects", projectsRouter);
+server.use("/tasks", tasksRouter);
+server.use("/resources", resourcesRouter);
 
 server.get("/", (req, res) => {
   res.status(200).send(`<h1>Aaron's Node DB Sprint Challenge</h1>`);
