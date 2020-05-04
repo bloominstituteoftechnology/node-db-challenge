46f1d659563c8c054526f6f2b2c8a9660037dfdb
Your name <a.m.verdine@gmail.com>
2020-05-03 20:37:44 -0400

    Update knexfile

--- knexfile.js
+++ knexfile.js
@@ -7,5 +7,20 @@ module.exports = {
     connection: {
       filename: "./data/projects.db3",
     },
+
+    migrations: {
+      directory: "./data/migrations",
+    },
+
+    seeds: {
+      directory: "./data/seeds",
+    },
+
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      },
+    },
   },
 };
