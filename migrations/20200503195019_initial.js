95d765134ca06d385fd29822d417c715379ae309
Your name <a.m.verdine@gmail.com>
2020-05-03 20:27:00 -0400

    Build schema through migrations file

--- migrations/20200503195019_initial.js
+++ migrations/20200503195019_initial.js
@@ -1,8 +1,28 @@
+exports.up = async function (knex) {
+  await knex.schema.createTable("Projects", (table) => {
+    table.increments("id");
+    table.text("Name").notNull();
+    table.text("Description", 128);
+    table.boolean("Completed").notNull().defaultTo(false);
+  });
 
-exports.up = function(knex) {
-  
+  await knex.schema.createTable("Tasks", (table) => {
+    table.increments("id");
+    table.text("Description").notNull();
+    table.text("Notes");
+    table.integer("Project_ID").references("id").inTable("Projects");
+    table.boolean("Completed").notNull().defaultTo(false);
+  });
+
+  await knex.schema.createTable("Resources", (table) => {
+    table.increments("id");
+    table.text("Name").notNull();
+    table.text("Description");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("Resources");
+  await knex.schema.dropTableIfExists("Tasks");
+  await knex.schema.dropTableIfExists("Projects");
 };
