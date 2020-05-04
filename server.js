f616bd8da423d4b129b48e8677ac5e536dc3eb88
jacobvanwormer <sharpiejacob@gmail.com>
2020-05-03 17:25:58 -0700

    I made a booboo

--- server.js
+++ server.js
@@ -2,5 +2,17 @@ const express = require('express');
 const helmet = require('helmet');
 const db = require('./data/db-config.js');
 const server = express();
+const projectRouter = require('./schema/projects-router.js');
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Sprint!</h2>`);
+  });
+
+server.use(express.json()); // built-in middleware, no need to install it
+
+//endpoints
+server.use('/api/projects', projectRouter);
+
+
 
 module.exports = server; 
