bae9ef5810804f76fc5f244038d1e342fb4dfeac
Your name <a.m.verdine@gmail.com>
2020-05-03 21:49:21 -0400

    Write GET request endpoints for projects/tasks/resources routers

--- Routers/resources-router.js
+++ Routers/resources-router.js
@@ -3,4 +3,12 @@ const db = require("../data/db-config");
 
 const router = express.Router();
 
+router.get("/", async (req, res, next) => {
+  try {
+    res.json(await db("Resources"));
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
