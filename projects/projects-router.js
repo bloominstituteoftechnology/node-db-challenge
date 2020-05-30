7eb9f7f2cba165e9029b0550082b69e8727f9cd5
Veto Ramirez <vetorobert3@gmail.com>
2020-05-30 10:47:33 -0700

    created projects-model and router.

---
+++ projects/projects-router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const Projects = require('./projects-model.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  Projects.find()
+  .then(project => {
+    res.json(project);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get projects' });
+  });
+});
+
+module.exports = router;
